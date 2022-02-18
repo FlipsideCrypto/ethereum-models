ed111adba7cec8251a0a9ee19ed2223b93d0de2c
austinFlipside <austin@flipsidecrypto.com>
2022-02-17 20:11:00 -0500

    updated

--- macros/js_hextoint.sql
+++ macros/js_hextoint.sql
@@ -1,6 +1,6 @@
 {% macro js_hex_to_int() %}
     CREATE
-    OR REPLACE FUNCTION silver_ethereum_2022.js_hex_to_int (
+    OR REPLACE FUNCTION {{ target.schema }}.js_hex_to_int (
         s STRING
     ) returns DOUBLE LANGUAGE javascript AS 'if (S !== null) { yourNumber = parseInt(S, 16); } return yourNumber'
 {% endmacro %}

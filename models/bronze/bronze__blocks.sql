395910330e233a731dfa66b8eb27ba2e29d5063b
austinFlipside <austin@flipsidecrypto.com>
2022-04-05 16:58:18 -0400

    fix dupes

--- models/bronze/bronze__blocks.sql
+++ models/bronze/bronze__blocks.sql
@@ -17,3 +17,10 @@ FROM
         'prod',
         'ethereum_blocks'
     ) }}
+WHERE
+    CASE
+        WHEN block_id <= 14348123
+        AND block_id >= 14298399
+        AND ingested_at < '2022-03-20' THEN FALSE
+        ELSE TRUE
+    END = TRUE

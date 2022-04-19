31fed8bdadc0e1100f863a6e5d6580c6ce133cf5
Desmond Hui <desmond@flipsidecrypto.com>
2022-04-19 14:07:45 -0700

    add lower

--- models/silver/silver__nft_metadata_api_requests.sql
+++ models/silver/silver__nft_metadata_api_requests.sql
@@ -3,12 +3,12 @@
 ) }}
 
 SELECT
-    DISTINCT contract_address
+    DISTINCT lower(contract_address)
 FROM
     {{ ref('silver__seed_nft_metadata_upload') }}
 EXCEPT
 SELECT
-    DISTINCT contract_address
+    DISTINCT lower(contract_address)
 FROM
     {{ source(
         'ethereum_external',

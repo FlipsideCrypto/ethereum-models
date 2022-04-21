4cb4db9fac2e4d95822f6b17f0a934c618c52c86
Vahid-flipside <vahid@flipsidecrypto.com>
2022-04-21 17:28:30 -0400

    changing amount from hexa to int

--- models/Sushi/sushi__dim_distributor_reward_schedule.sql
+++ models/Sushi/sushi__dim_distributor_reward_schedule.sql
@@ -1,11 +1,13 @@
 {{ config(
-    materialized = 'table',
+    materialized = 'incremental',
     unique_key = 'address',
     ) }}
 
 select c.key as address, 
        c.value:index::number as index, 
-       c.value:amount::string as amount, 
+        silver.js_hex_to_int(
+        c.value :amount :: STRING
+    ) / pow(10,18) AS amount, 
        c.value:proof::array as proof
 from {{ source(
         'sushi_external',

b3f4818dfd9c2b23b2f4d036f95f702fe431c881
Vahid-flipside <vahid@flipsidecrypto.com>
2022-04-28 17:52:16 -0400

    timestamp to timestamp_ntz

--- models/Sushi/sushi__dim_distributor_reward_schedule.sql
+++ models/Sushi/sushi__dim_distributor_reward_schedule.sql
@@ -4,8 +4,8 @@
     ) }}
 
 select merkle_root,
-        case when merkle_root = '0x015e6c2cd1a4d6fa77aa1884c436b2435aae4beab5c9a091f18fd0c00dc7e577' then cast('2021-10-22 23:10:00' as timestamp)
-             when merkle_root = '0x2828440576d787582da1af59d0bc0f090e5add2680610dc2ffa53b5a6aa30b07' then cast('2021-10-12 21:47:00' as timestamp)               
+        case when merkle_root = '0x015e6c2cd1a4d6fa77aa1884c436b2435aae4beab5c9a091f18fd0c00dc7e577' then cast('2021-10-22 23:10:00' as timestamp_ntz)
+             when merkle_root = '0x2828440576d787582da1af59d0bc0f090e5add2680610dc2ffa53b5a6aa30b07' then cast('2021-10-12 21:47:00' as timestamp_ntz)               
              end as rewards_snapshot,
         c.key as address, 
         c.value:index::number as index, 

d82035701a2d576b2788960e199483ef9f749c0b
austinFlipside <austin@flipsidecrypto.com>
2022-05-04 15:42:06 -0400

    add status and sub traces

--- models/silver/traces/silver__traces.sql
+++ models/silver/traces/silver__traces.sql
@@ -22,13 +22,11 @@ AND block_id NOT IN (
         {{ this }}
 )
 {% endif %}
-
--- ORDER BY
---     ingested_at DESC
--- LIMIT
---     1800000
-),
-traces_txs AS (
+ORDER BY
+    ingested_at DESC
+LIMIT
+    1800000
+), traces_txs AS (
     SELECT
         *
     FROM
@@ -101,12 +99,15 @@ flattened_traces AS (
         DATA :time :: STRING AS TIME,
         DATA :to :: STRING AS to_address,
         DATA :type :: STRING AS TYPE,
-        silver.js_hex_to_int(
-            DATA :value :: STRING
-        ) / pow(
-            10,
-            18
-        ) AS eth_value,
+        CASE
+            WHEN DATA :type :: STRING = 'CALL' THEN silver.js_hex_to_int(
+                DATA :value :: STRING
+            ) / pow(
+                10,
+                18
+            )
+            ELSE 0
+        END AS eth_value,
         CASE
             WHEN id = '__' THEN CONCAT(
                 DATA :type :: STRING,
@@ -126,28 +127,84 @@ flattened_traces AS (
             '-',
             tx_hash,
             identifier
-        ) AS _call_id,*
+        ) AS _call_id,
+        SPLIT(
+            identifier,
+            '_'
+        ) AS id_split,
+        ARRAY_SLICE(id_split, 1, ARRAY_SIZE(id_split)) AS levels,
+        ARRAY_TO_STRING(
+            levels,
+            '_'
+        ) AS LEVEL,
+        CASE
+            WHEN ARRAY_SIZE(levels) = 1
+            AND levels [0] :: STRING = 'ORIGIN' THEN NULL
+            WHEN ARRAY_SIZE(levels) = 1 THEN 'ORIGIN'
+            ELSE ARRAY_TO_STRING(ARRAY_SLICE(levels, 0, ARRAY_SIZE(levels) -1), '_')END AS parent_level,
+            COUNT(parent_level) over (
+                PARTITION BY tx_hash,
+                parent_level
+            ) AS sub_traces,*
+            FROM
+                base_table
+        ),
+        group_sub_traces AS (
+            SELECT
+                tx_hash,
+                parent_level,
+                sub_traces
+            FROM
+                flattened_traces
+            GROUP BY
+                tx_hash,
+                parent_level,
+                sub_traces
+        ),
+        FINAL AS (
+            SELECT
+                flattened_traces.tx_hash AS tx_hash,
+                flattened_traces.block_number AS block_number,
+                flattened_traces.block_timestamp AS block_timestamp,
+                flattened_traces.from_address AS from_address,
+                flattened_traces.to_address AS to_address,
+                flattened_traces.eth_value AS eth_value,
+                flattened_traces.gas AS gas,
+                flattened_traces.gas_used AS gas_used,
+                flattened_traces.input AS input,
+                flattened_traces.output AS output,
+                flattened_traces.type AS TYPE,
+                flattened_traces.identifier AS identifier,
+                flattened_traces._call_id AS _call_id,
+                flattened_traces.ingested_at AS ingested_at,
+                flattened_traces.data AS DATA,
+                flattened_traces.tx_status AS tx_status,
+                group_sub_traces.sub_traces AS sub_traces
+            FROM
+                flattened_traces
+                LEFT OUTER JOIN group_sub_traces
+                ON flattened_traces.tx_hash = group_sub_traces.tx_hash
+                AND flattened_traces.level = group_sub_traces.parent_level
+        )
+    SELECT
+        tx_hash,
+        block_number,
+        block_timestamp,
+        from_address,
+        to_address,
+        eth_value,
+        gas,
+        gas_used,
+        input,
+        output,
+        TYPE,
+        identifier,
+        _call_id,
+        ingested_at,
+        DATA,
+        tx_status,
+        sub_traces
     FROM
-        base_table
-)
-SELECT
-    tx_hash,
-    block_number,
-    block_timestamp,
-    from_address,
-    to_address,
-    eth_value,
-    gas,
-    gas_used,
-    input,
-    output,
-    TYPE,
-    identifier,
-    _call_id,
-    ingested_at,
-    DATA,
-    tx_status
-FROM
-    flattened_traces qualify(ROW_NUMBER() over(PARTITION BY _call_id
-ORDER BY
-    ingested_at DESC)) = 1
+        FINAL qualify(ROW_NUMBER() over(PARTITION BY _call_id
+    ORDER BY
+        ingested_at DESC)) = 1

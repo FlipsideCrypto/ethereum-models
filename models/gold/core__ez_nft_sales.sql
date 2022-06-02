e111ccfd9dab6f2291d5f669f926700d65a612a1
Austin <93135983+austinFlipside@users.noreply.github.com>
2022-06-02 12:31:58 -0400

    An 1324/add nft exchanges (#82)
    * x2y2 sales model and tests
    
    * stash - do not merge
    
    * stash - do not merge
    
    * stash - not done
    
    * finish rarible
    
    * add nftx
    
    * add nftx to gold
    
    * final docs updates

--- models/gold/core__ez_nft_sales.sql
+++ models/gold/core__ez_nft_sales.sql
@@ -99,3 +99,67 @@ SELECT
     origin_function_signature
 FROM
     {{ ref('silver_nft__rarible_sales') }}
+UNION ALL
+SELECT
+    block_number,
+    block_timestamp,
+    tx_hash,
+    event_type,
+    platform_address,
+    platform_name,
+    seller_address,
+    buyer_address,
+    nft_address,
+    project_name,
+    erc1155_value,
+    tokenId,
+    token_metadata,
+    currency_symbol,
+    currency_address,
+    price,
+    price_usd,
+    total_fees,
+    platform_fee,
+    creator_fee,
+    total_fees_usd,
+    platform_fee_usd,
+    creator_fee_usd,
+    tx_fee,
+    tx_fee_usd,
+    origin_from_address,
+    origin_to_address,
+    origin_function_signature
+FROM
+    {{ ref('silver_nft__x2y2_sales') }}
+UNION ALL
+SELECT
+    block_number,
+    block_timestamp,
+    tx_hash,
+    event_type,
+    platform_address,
+    platform_name,
+    seller_address,
+    buyer_address,
+    nft_address,
+    project_name,
+    erc1155_value,
+    tokenId,
+    token_metadata,
+    currency_symbol,
+    currency_address,
+    price,
+    price_usd,
+    total_fees,
+    platform_fee,
+    creator_fee,
+    total_fees_usd,
+    platform_fee_usd,
+    creator_fee_usd,
+    tx_fee,
+    tx_fee_usd,
+    origin_from_address,
+    origin_to_address,
+    origin_function_signature
+FROM
+    {{ ref('silver_nft__nftx_sales') }}

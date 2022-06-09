e2844ba52f183a1584728a22d5feffda41715645
desmond-hui <97470747+desmond-hui@users.noreply.github.com>
2022-06-09 11:43:03 -0700

    An 1569/fix external tables issue (#90)
    * change dev to reference dev stage for external
    
    * change ownership for stages on dev refresh

--- macros/sp_create_prod_clone.sql
+++ macros/sp_create_prod_clone.sql
@@ -63,6 +63,15 @@ $$
             snowflake.execute({sqlText: `GRANT OWNERSHIP ON PROCEDURE ${DESTINATION_DB_NAME}.${schema}.${procedure_name}${argument_signature} to role ${ROLE_NAME} REVOKE CURRENT GRANTS;`});
         }
 
+        var existing_procedures = snowflake.execute({sqlText: `SELECT stage_schema, stage_name
+            FROM ${DESTINATION_DB_NAME}.INFORMATION_SCHEMA.STAGES;`});
+
+        while (existing_procedures.next()) {
+            var schema = existing_procedures.getColumnValue(1)
+            var stage_name = existing_procedures.getColumnValue(2)
+            snowflake.execute({sqlText: `GRANT OWNERSHIP ON STAGE ${DESTINATION_DB_NAME}.${schema}.${stage_name} to role ${ROLE_NAME} REVOKE CURRENT GRANTS;`});
+        }
+
         snowflake.execute({sqlText: `GRANT OWNERSHIP ON DATABASE ${DESTINATION_DB_NAME} TO ROLE ${ROLE_NAME};`})
         snowflake.execute({sqlText: `COMMIT;`});
     } catch (err) {

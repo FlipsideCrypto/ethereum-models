4a4b5792aa5734158c949c2f70e17fa5461aa3ec
austinFlipside <austin@flipsidecrypto.com>
2022-07-11 19:25:41 -0400

    add created block

--- models/gold/Compound/compound__ez_asset_details.sql
+++ models/gold/Compound/compound__ez_asset_details.sql
@@ -82,7 +82,27 @@ SELECT
         WHEN b.underlying_asset_address = '0x1985365e9f78359a9b6ad760e32412f4a445e862' THEN 18
         ELSE C.decimals :: INTEGER
     END AS underlying_decimals,
-    C.contract_metadata AS underlying_contract_metadata
+    C.contract_metadata AS underlying_contract_metadata,
+    CASE
+        WHEN b.ctoken_address = '0x4ddc2d193948926d02f9b1fe9e1daa0718270ed5' THEN 7710758
+        WHEN b.ctoken_address = '0xe65cdb6479bac1e22340e4e755fae7e509ecd06c' THEN 12848198
+        WHEN b.ctoken_address = '0xccf4429db6322d5c611ee964527d42e5d685dd6a' THEN 12038653
+        WHEN b.ctoken_address = '0xf5dce57282a584d2746faf1593d3121fcac444dc' THEN 7710752
+        WHEN b.ctoken_address = '0x80a2ae356fc9ef4305676f7a3e2ed04e12c33946' THEN 12848198
+        WHEN b.ctoken_address = '0x95b4ef2869ebd94beb4eee400a99824bf5dc325b' THEN 12836064
+        WHEN b.ctoken_address = '0x158079ee67fce2f58472a96584a73c7ab9ac95c1' THEN 7710755
+        WHEN b.ctoken_address = '0x5d3a536e4d6dbd6114cc1ead35777bab948e3643' THEN 8983575
+        WHEN b.ctoken_address = '0xface851a4921ce59e912d19329929ce6da6eb0c7' THEN 12286030
+        WHEN b.ctoken_address = '0x4b0181102a0112a2ef11abee5563bb4a3176c9d7' THEN 12848166
+        WHEN b.ctoken_address = '0x6c8c6b02e7b2be14d4fa6022dfd6d75921d90e4e' THEN 7710735
+        WHEN b.ctoken_address = '0xb3319f5d18bc0d84dd1b4825dcde5d5f7266d407' THEN 7710733
+        WHEN b.ctoken_address = '0x39aa39c021dfbae8fac545936693ac917d5e7563' THEN 7710760
+        WHEN b.ctoken_address = '0x35a18000230da775cac24873d00ff85bccded550' THEN 10921410
+        WHEN b.ctoken_address = '0x12392f67bdf24fae0af363c24ac620a2f67dad86' THEN 11008385
+        WHEN b.ctoken_address = '0xc11b1268c1a384e55c48c2391d8d480264a3a7f4' THEN 8163813
+        WHEN b.ctoken_address = '0xf650c3d88d12db855b8bf7d11be6c55a4e07dcc9' THEN 9879363
+        WHEN b.ctoken_address = '0x70e36f6bf80a52b3b46b3af8e106cc0ed743e8e4' THEN 10960099
+    END AS created_block
 FROM
     base b
     LEFT JOIN {{ ref('core__dim_contracts') }} C

62d31c7831b9ad2b68bdf9f4ee60fac95800b818
austinFlipside <austin@flipsidecrypto.com>
2022-07-13 12:50:10 -0400

    union all reads

--- models/silver/streamline/streamline__contract_reads.sql
+++ models/silver/streamline/streamline__contract_reads.sql
@@ -1,11 +1,8 @@
-{{ config (
-    materialized = "incremental",
-    unique_key = "id",
-    cluster_by = "ROUND(block_number, -3)",
-    merge_update_columns = ["id"],
+{{ config(
+    materialized = 'view'
 ) }}
 
-WITH curve_pool_details AS (
+WITH all_reads AS (
 
     SELECT
         id,
@@ -17,18 +14,94 @@ WITH curve_pool_details AS (
         _inserted_timestamp
     FROM
         {{ ref('streamline__curve_pool_reads') }}
-
-{% if is_incremental() %}
-WHERE
-    (
-        _inserted_timestamp >= (
-            SELECT
-                MAX(_inserted_timestamp)
-            FROM
-                {{ this }}
-        )
-    )
-{% endif %}
+    UNION
+    SELECT
+        id,
+        block_number,
+        contract_address,
+        call_name,
+        function_signature,
+        function_input,
+        _inserted_timestamp
+    FROM
+        {{ ref('streamline__aave_incentives_read') }}
+    UNION
+    SELECT
+        id,
+        block_number,
+        contract_address,
+        call_name,
+        function_signature,
+        function_input,
+        _inserted_timestamp
+    FROM
+        {{ ref('streamline__aave_prices_oracle_read') }}
+    UNION
+    SELECT
+        id,
+        block_number,
+        contract_address,
+        call_name,
+        function_signature,
+        function_input,
+        _inserted_timestamp
+    FROM
+        {{ ref('streamline__aave_reserve_data_read') }}
+    UNION
+    SELECT
+        id,
+        block_number,
+        contract_address,
+        call_name,
+        function_signature,
+        function_input,
+        _inserted_timestamp
+    FROM
+        {{ ref('streamline__compound_comptroller_stats') }}
+    UNION
+    SELECT
+        id,
+        block_number,
+        contract_address,
+        call_name,
+        function_signature,
+        function_input,
+        _inserted_timestamp
+    FROM
+        {{ ref('streamline__compound_market_stats') }}
+    UNION
+    SELECT
+        id,
+        block_number,
+        contract_address,
+        call_name,
+        function_signature,
+        function_input,
+        _inserted_timestamp
+    FROM
+        {{ ref('streamline__uni_v3_pool_reads') }}
+    UNION
+    SELECT
+        id,
+        block_number,
+        contract_address,
+        call_name,
+        function_signature,
+        function_input,
+        _inserted_timestamp
+    FROM
+        {{ ref('streamline__uni_v3_position_reads') }}
+    UNION
+    SELECT
+        id,
+        block_number,
+        contract_address,
+        call_name,
+        function_signature,
+        function_input,
+        _inserted_timestamp
+    FROM
+        {{ ref('streamline__uni_v3_token_reads') }}
 )
 SELECT
     id,
@@ -39,4 +112,4 @@ SELECT
     function_input,
     _inserted_timestamp
 FROM
-    curve_pool_details
+    all_reads

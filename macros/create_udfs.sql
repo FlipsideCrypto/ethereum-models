596a1dcd2e8e3d7fa79099f3687b43a5b868d75b
Ryan-Loofy <63126328+Ryan-Loofy@users.noreply.github.com>
2022-07-19 09:47:37 -0400

    updating create udfs macro

--- macros/create_udfs.sql
+++ macros/create_udfs.sql
@@ -1,14 +1,19 @@
 {% macro create_udfs() %}
     {% set sql %}
     CREATE schema if NOT EXISTS silver;
-{{ create_js_hex_to_int() }};
-{{ create_udf_load_nft_metadata() }};
-{{ create_udf_hex_to_int(
+    {{ create_js_hex_to_int() }};
+    {{ create_udf_hex_to_int(
         schema = "public"
     ) }}
-    {{ create_udf_get_token_balances() }}
-    {{ create_udf_get_eth_balances() }}
-    
     {% endset %}
     {% do run_query(sql) %}
+
+    {% if target.database != "ETHEREUM_COMMUNITY_DEV" %}
+        {% set sql %}
+        {{ create_udf_load_nft_metadata() }};
+        {{ create_udf_get_token_balances() }}
+        {{ create_udf_get_eth_balances() }}
+        {% endset %}
+        {% do run_query(sql) %}
+    {% endif %}
 {% endmacro %}

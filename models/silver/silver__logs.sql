fb9955334887d844d8430983b78857b141ad9a00
austinFlipside <austin@flipsidecrypto.com>
2022-07-28 12:35:06 -0400

    fix

--- models/silver/silver__logs.sql
+++ models/silver/silver__logs.sql
@@ -1,7 +1,7 @@
 {{ config(
     materialized = 'incremental',
     unique_key = "_log_id",
-    cluster_by = ['block_timestamp::DATE', '_inserted_timestamp::DATE'],,
+    cluster_by = ['block_timestamp::DATE', '_inserted_timestamp::DATE'],
     tags = ['core'],
     post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION"
 ) }}

397844690e898be22f341df237a6ea04a62e01c7
robel91 <92173230+robel91@users.noreply.github.com>
2022-08-11 15:40:21 -0400

    Balancer Swap Table

---
+++ models/silver/dex/silver_dex__balancer_swaps.sql
@@ -0,0 +1,192 @@
+{{ config(
+    materialized = 'incremental',
+    persist_docs ={ "relation": true,
+    "columns": true },
+    unique_key = 'pool_address',
+) }}
+
+
+WITH vault_creation AS (
+    SELECT
+        tx_hash,
+        event_inputs :poolId :: STRING AS poolId,
+        LOWER(
+            event_inputs :tokens [0] :: STRING
+        ) AS token0,
+        LOWER(
+            event_inputs :tokens [1] :: STRING
+        ) AS token1,
+        LOWER(
+            event_inputs :tokens [2] :: STRING
+        ) AS token2,
+        LOWER(
+            event_inputs :tokens [3] :: STRING
+        ) AS token3,
+        LOWER(
+            event_inputs :tokens [4] :: STRING
+        ) AS token4,
+        event_inputs :tokens AS token_array
+    FROM
+        {{ref('core__fact_event_logs')}}
+    WHERE
+        event_name = 'TokensRegistered'
+)
+
+,swaps AS (
+    SELECT
+        tx_hash,
+        block_number,
+        block_timestamp,
+        event_index,
+        event_inputs :amountIn :: INTEGER AS amountIn,
+        event_inputs :amountOut :: INTEGER AS amountOut,
+        event_inputs :poolId :: STRING AS poolId,
+        event_inputs :tokenIn :: STRING AS tokenIn,
+        event_inputs :tokenOut :: STRING AS tokenOut,
+        SUBSTR(
+            event_inputs :poolId :: STRING,
+            0,
+            42
+        ) AS pool_address
+    FROM
+        {{ref('core__dim_contracts')}}
+    WHERE
+        block_timestamp > '2022-06-29'
+        AND contract_address = LOWER('0xBA12222222228d8Ba445958a75a0704d566BF2C8')
+        AND event_name = 'Swap'
+)
+
+,contracts AS (
+    SELECT
+        *
+    FROM
+        core.dim_contracts
+    WHERE
+        address IN (
+            SELECT
+                DISTINCT tokenIn
+            FROM
+                swaps
+        )
+        OR address IN (
+            SELECT
+                DISTINCT tokenOut
+            FROM
+                swaps
+        )
+        OR address IN (
+            SELECT
+                DISTINCT pool_address
+            FROM
+                swaps
+        )
+)
+
+,token_prices AS (
+    SELECT
+        HOUR,
+        token_address,
+        AVG(price) AS price
+    FROM
+        core.fact_hourly_token_prices
+    WHERE
+        token_address IN (
+            SELECT
+                DISTINCT address
+            FROM
+                contracts
+        )
+        AND HOUR :: DATE IN (
+            SELECT
+                DISTINCT block_timestamp :: DATE
+            FROM
+                swaps
+        )
+    GROUP BY
+        1,
+        2
+) 
+SELECT
+    A.tx_hash,
+    A.block_timestamp,
+    A.block_number,
+    A.event_index,
+    CASE
+        WHEN b.decimals IS NOT NULL THEN A.amountin / pow(
+            10,
+            b.decimals
+        )
+        ELSE A.amountin
+    END AS amount_in,
+    CASE
+        WHEN C.decimals IS NOT NULL THEN A.amountout / pow(
+            10,
+            C.decimals
+        )
+        ELSE A.amountout
+    END AS amount_out,
+    CASE
+        WHEN b.decimals IS NOT NULL THEN ROUND(
+            amount_in * pIn.price,
+            2
+        )
+        ELSE NULL
+    END AS amount_in_usd,
+    CASE
+        WHEN C.decimals IS NOT NULL THEN ROUND(
+            amount_out * pOut.price,
+            2
+        )
+        ELSE NULL
+    END AS amount_out_usd,
+    CONCAT(
+        A.tokenIn,
+        ' -> ',
+        A.tokenOut
+    ) AS trade_direction_address,
+    CONCAT(
+        b.symbol,
+        ' -> ',
+        C.symbol
+    ) AS trade_direction_symbol,
+    A.tokenIn AS tokenIn_address,
+    b.symbol AS tokenIn_symbol,
+    b.decimals AS tokenIn_decimals,
+    A.tokenOut AS tokenOut_address,
+    C.symbol AS tokenOut_symbol,
+    C.decimals AS tokenOut_decimals,
+    d.symbol AS pool_symbol,
+    vc.token0 AS token0_address,
+    vc.token1 AS token1_address,
+    vc.token2 AS token2_address,
+    vc.token3 AS token3_address,
+    vc.token4 AS token4_address,
+    A.poolId,
+    A.pool_address,
+    d.name AS pool_name
+FROM
+    swaps A
+    LEFT JOIN contracts b
+    ON A.tokenIn = b.address
+    LEFT JOIN contracts C
+    ON A.tokenOut = C.address
+    LEFT JOIN contracts d
+    ON A.pool_address = d.address
+    LEFT JOIN vault_creation vc
+    ON A.poolid = vc.poolid
+    LEFT JOIN token_prices pIn
+    ON DATE_TRUNC(
+        'hour',
+        block_timestamp
+    ) = pIn.hour
+    AND pIn.token_address = A.tokenIn
+    LEFT JOIN token_prices pOut
+    ON DATE_TRUNC(
+        'hour',
+        block_timestamp
+    ) = pOut.hour
+    AND pOut.token_address = A.tokenOut
+ORDER BY
+    block_timestamp DESC,
+    tx_hash,
+    event_index ASC

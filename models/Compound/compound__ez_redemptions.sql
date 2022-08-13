7d69f0df4e7ba14e1a5ddaddb24b193274d0a9ca
Austin <austin@flipsidecrypto.com>
2022-08-12 20:08:28 -0400

    fix filter

--- models/Compound/compound__ez_redemptions.sql
+++ models/Compound/compound__ez_redemptions.sql
@@ -47,7 +47,7 @@ comp_redemptions AS (
             FROM
                 asset_details
         )
-        AND event_name = 'Redeem'
+        AND topics [0] :: STRING = '0xe5b754fb1abb7f01b499791d0b820ae3b6af3424ac1c59768edb53f4ec31a929'
 
 {% if is_incremental() %}
 AND _inserted_timestamp >= (

5e4b53e55a46dc1306ba9b47f927db8d5118e760
robel91 <92173230+robel91@users.noreply.github.com>
2022-08-19 14:19:47 -0400

    new_balancer_swap_Table

--- models/gold/core__ez_dex_swaps.sql
+++ models/gold/core__ez_dex_swaps.sql
@@ -75,6 +75,66 @@ balancer_swaps (
   FROM
     {{ ref('silver_dex__balancer_swaps') }}
 
-
+{% if is_incremental() %}
+WHERE
+  _inserted_timestamp >= (
+    SELECT
+      MAX(_inserted_timestamp) :: DATE - 2
+    FROM
+      {{ this }}
+  )
+{% endif %}
 )
-
+SELECT
+  block_number,
+  block_timestamp,
+  tx_hash,
+  origin_function_signature,
+  origin_from_address,
+  origin_to_address,
+  contract_address,
+  pool_name,
+  event_name,
+  amount_in,
+  amount_in_usd,
+  amount_out,
+  amount_out_usd,
+  sender,
+  tx_to,
+  event_index,
+  platform,
+  token_in,
+  token_out,
+  symbol_in,
+  symbol_out,
+  _log_id,
+  _inserted_timestamp
+FROM
+  v2_swaps
+UNION ALL
+SELECT
+  block_number,
+  block_timestamp,
+  tx_hash,
+  origin_function_signature,
+  origin_from_address,
+  origin_to_address,
+  contract_address,
+  pool_name,
+  event_name,
+  amount_in,
+  amount_in_usd,
+  amount_out,
+  amount_out_usd,
+  sender,
+  tx_to,
+  event_index,
+  platform,
+  token_in,
+  token_out,
+  symbol_in,
+  symbol_out,
+  _log_id,
+  _inserted_timestamp
+FROM
+  curve_swaps

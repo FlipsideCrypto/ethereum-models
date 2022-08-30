789afad40e2540b4a583e9f0e0e5ad86bf58f154
Ryan-Loofy <63126328+Ryan-Loofy@users.noreply.github.com>
2022-08-30 15:59:32 -0400

    Changed keyword, made inputs case insensitive

--- macros/python/hex_to_int_udf.sql
+++ macros/python/hex_to_int_udf.sql
@@ -23,14 +23,14 @@ $$;
 
 
 {% macro create_udf_hex_to_int_with_inputs(schema) %}
-create or replace function {{ schema }}.udf_hex_to_int(num_type string, hex string)
+create or replace function {{ schema }}.udf_hex_to_int(encoding string, hex string)
 returns string
 language python
 runtime_version = '3.8'
 handler = 'hex_to_int'
 as
 $$
-def hex_to_int(num_type, hex) -> str:
+def hex_to_int(encoding, hex) -> str:
   """
   Converts hex (of any size) to int (as a string). Snowflake and java script can only handle up to 64-bit (38 digits of precision)
   select hex_to_int('hex', '200000000000000000000000000000211');
@@ -44,8 +44,8 @@ def hex_to_int(num_type, hex) -> str:
   """
   if not hex:
     return None
-  if num_type == 's2c':
-    if hex[0:2] != '0x':
+  if encoding.lower() == 's2c':
+    if hex[0:2].lower() != '0x':
       hex = f'0x{hex}'
 
     bits = len(hex[2:])*4

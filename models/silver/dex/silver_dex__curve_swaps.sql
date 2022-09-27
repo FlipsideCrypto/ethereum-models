34383d5b65892a630a7b86bdc6ca487c5e86fa96
Austin <austin@flipsidecrypto.com>
2022-09-27 17:18:23 -0400

    reworked swaps

--- models/silver/dex/silver_dex__curve_swaps.sql
+++ models/silver/dex/silver_dex__curve_swaps.sql
@@ -7,11 +7,7 @@
 WITH pool_meta AS (
 
     SELECT
-        pool_address,
-        token_address,
-        token_index,
-        token_decimals,
-        token_symbol,
+        DISTINCT pool_address,
         pool_name
     FROM
         {{ ref('silver_dex__curve_pools') }}
@@ -28,6 +24,7 @@ curve_base AS (
         event_index,
         event_name,
         contract_address AS pool_address,
+        pool_name,
         regexp_substr_all(SUBSTR(DATA, 3, len(DATA)), '.{64}') AS segmented_data,
         CONCAT('0x', SUBSTR(topics [1] :: STRING, 27, 40)) AS sender,
         PUBLIC.udf_hex_to_int(
@@ -46,18 +43,14 @@ curve_base AS (
         _inserted_timestamp
     FROM
         {{ ref('silver__logs') }}
+        INNER JOIN pool_meta
+        ON pool_meta.pool_address = contract_address
     WHERE
         topics [0] :: STRING IN (
             '0x8b3e96f2b889fa771c53c981b40daf005f63f637f1869f707052d15a3dd97140',
-            '0xb2e76ae99761dc136e598d4a629bb347eccb9532a5f8bbd72e18467c3c34cc98'
+            '0xb2e76ae99761dc136e598d4a629bb347eccb9532a5f8bbd72e18467c3c34cc98',
+            '0xd013ca23e77a65003c2c659c5442c00c805371b7fc1ebd4c206c41d1536bd90b'
         )
-        AND contract_address IN (
-            SELECT
-                DISTINCT pool_address
-            FROM
-                pool_meta
-        ) -- we will only include pools in the underlying pools table
-        -- if one of relevance is missing it can be added to the seed file until the reads lambda is ready
 
 {% if is_incremental() %}
 AND _inserted_timestamp >= (
@@ -68,55 +61,129 @@ AND _inserted_timestamp >= (
 )
 {% endif %}
 ),
+token_transfers AS (
+    SELECT
+        tx_hash,
+        contract_address AS token_address,
+        TRY_TO_NUMBER(
+            PUBLIC.udf_hex_to_int(
+                DATA :: STRING
+            )
+        ) AS amount,
+        CONCAT('0x', SUBSTR(topics [1] :: STRING, 27, 40)) AS from_address,
+        CONCAT('0x', SUBSTR(topics [2] :: STRING, 27, 40)) AS to_address
+    FROM
+        {{ ref('silver__logs') }}
+    WHERE
+        topics [0] :: STRING = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
+        AND tx_hash IN (
+            SELECT
+                DISTINCT tx_hash
+            FROM
+                curve_base
+        )
+        AND CONCAT('0x', SUBSTR(topics [1] :: STRING, 27, 40)) <> '0x0000000000000000000000000000000000000000'
+        AND CONCAT('0x', SUBSTR(topics [2] :: STRING, 27, 40)) <> '0x0000000000000000000000000000000000000000'
+
+{% if is_incremental() %}
+AND _inserted_timestamp >= (
+    SELECT
+        MAX(_inserted_timestamp) :: DATE - 2
+    FROM
+        {{ this }}
+)
+{% endif %}
+),
+from_transfers AS (
+    SELECT
+        DISTINCT tx_hash,
+        token_address,
+        from_address,
+        amount
+    FROM
+        token_transfers
+),
+to_transfers AS (
+    SELECT
+        DISTINCT tx_hash,
+        token_address,
+        to_address,
+        amount
+    FROM
+        token_transfers
+),
 pool_info AS (
     SELECT
-        block_number,
-        block_timestamp,
-        origin_function_signature,
-        origin_from_address,
-        origin_to_address,
-        contract_address,
-        tx_hash,
+        s.block_number,
+        s.block_timestamp,
+        s.tx_hash,
+        s.origin_function_signature,
+        s.origin_from_address,
+        s.origin_from_address AS tx_to,
+        s.origin_to_address,
         event_index,
         event_name,
-        origin_from_address AS tx_to,
-        curve_base.pool_address AS pool_address,
-        pool0.pool_name AS pool_name,
+        pool_address,
+        pool_address AS contract_address,
+        pool_name,
         sender,
         sold_id,
-        tokens_sold AS amount_in_unadj,
-        pool0.token_decimals AS decimals_in,
-        pool0.token_symbol AS symbol_in,
-        pool0.token_address AS token_in,
+        tokens_sold,
+        COALESCE(
+            sold.token_address,
+            '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+        ) AS token_in,
+        c0.symbol symbol_in,
+        c0.decimals AS decimals_in,
         CASE
-            WHEN decimals_in IS NOT NULL THEN amount_in_unadj / pow(
+            WHEN decimals_in IS NOT NULL THEN tokens_sold / pow(
                 10,
                 decimals_in
             )
-            ELSE amount_in_unadj
+            ELSE tokens_sold
         END AS amount_in,
         bought_id,
-        tokens_bought AS amount_out_unadj,
-        pool1.token_decimals AS decimals_out,
-        pool1.token_symbol AS symbol_out,
-        pool1.token_address AS token_out,
+        tokens_bought,
+        COALESCE(
+            bought.token_address,
+            '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+        ) AS token_out,
+        c1.symbol AS symbol_out,
+        c1.decimals AS decimals_out,
         CASE
-            WHEN decimals_out IS NOT NULL THEN amount_out_unadj / pow(
+            WHEN decimals_out IS NOT NULL THEN tokens_bought / pow(
                 10,
                 decimals_out
             )
-            ELSE amount_out_unadj
+            ELSE tokens_bought
         END AS amount_out,
         _log_id,
         _inserted_timestamp
     FROM
-        curve_base
-        LEFT JOIN pool_meta pool0
-        ON curve_base.pool_address = pool0.pool_address
-        AND sold_id = pool0.token_index
-        LEFT JOIN pool_meta pool1
-        ON curve_base.pool_address = pool1.pool_address
-        AND bought_id = pool1.token_index
+        curve_base s
+        LEFT JOIN from_transfers sold
+        ON tokens_sold = sold.amount
+        AND s.tx_hash = sold.tx_hash --  AND s.pool_address = sold.from_address
+        LEFT JOIN to_transfers bought
+        ON tokens_bought = bought.amount
+        AND s.tx_hash = bought.tx_hash -- AND s.pool_address = bought.to_address
+        LEFT JOIN {{ ref('core__dim_contracts') }}
+        c0
+        ON c0.address = COALESCE(
+            sold.token_address,
+            '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+        )
+        LEFT JOIN {{ ref('core__dim_contracts') }}
+        c1
+        ON c1.address = COALESCE(
+            bought.token_address,
+            '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+        )
+    WHERE
+        tokens_sold <> 0
+        AND symbol_out <> symbol_in qualify(ROW_NUMBER() over(PARTITION BY _log_id
+    ORDER BY
+        _inserted_timestamp DESC)) = 1
 ),
 prices AS (
     SELECT

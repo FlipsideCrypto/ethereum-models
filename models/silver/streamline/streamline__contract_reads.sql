2b3807d28430af150b57c1db1ca3820fa0e35878
Austin <austin@flipsidecrypto.com>
2022-09-29 16:59:12 -0400

    chainlink reads

--- models/silver/streamline/streamline__contract_reads.sql
+++ models/silver/streamline/streamline__contract_reads.sql
@@ -112,4 +112,14 @@ SELECT
     _inserted_timestamp :: TIMESTAMP AS _inserted_timestamp
 FROM
     {{ ref('streamline__contract_meta_read') }}
-
+UNION ALL
+SELECT
+    id :: STRING AS id,
+    block_number :: INTEGER AS block_number,
+    contract_address :: STRING AS contract_address,
+    call_name :: STRING AS call_name,
+    function_signature :: STRING AS function_signature,
+    function_input :: STRING AS function_input,
+    _inserted_timestamp :: TIMESTAMP AS _inserted_timestamp
+FROM
+    {{ ref('streamline__chainlink_prices') }}

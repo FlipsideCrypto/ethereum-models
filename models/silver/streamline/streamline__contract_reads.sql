b90fe88512d66144fb0cf6c22b3facea3829ac62
Austin <austin@flipsidecrypto.com>
2022-10-04 18:33:52 -0400

    get reserve config reads

--- models/silver/streamline/streamline__contract_reads.sql
+++ models/silver/streamline/streamline__contract_reads.sql
@@ -123,3 +123,14 @@ SELECT
     _inserted_timestamp :: TIMESTAMP AS _inserted_timestamp
 FROM
     {{ ref('streamline__chainlink_prices') }}
+UNION ALL
+SELECT
+    id :: STRING AS id,
+    block_number :: INTEGER AS block_number,
+    contract_address :: STRING AS contract_address,
+    call_name :: STRING AS call_name,
+    function_signature :: STRING AS function_signature,
+    function_input :: STRING AS function_input,
+    _inserted_timestamp :: TIMESTAMP AS _inserted_timestamp
+FROM
+    {{ ref('streamline__aave_reserve_config_reads') }}

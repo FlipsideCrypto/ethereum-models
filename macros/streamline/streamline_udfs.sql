6869aa8df41f54442dab7354e2f2a1ef9874a2b9
Ryan-Loofy <63126328+Ryan-Loofy@users.noreply.github.com>
2022-10-04 20:56:57 -0400

    updated to jinja sql

--- macros/streamline/streamline_udfs.sql
+++ macros/streamline/streamline_udfs.sql
@@ -71,7 +71,7 @@
 {% endmacro %}
 
 {% macro create_udf_call_node() %}
-    CREATE EXTERNAL FUNCTION IF NOT EXISTS streamline.udf_call_node() returns variant api_integration = aws_ethereum_api AS {% if target.name == "prod" %}
+    CREATE EXTERNAL FUNCTION IF NOT EXISTS streamline.udf_call_node(data ARRAY) returns variant api_integration = aws_ethereum_api AS {% if target.name == "prod" %}
         'https://e03pt6v501.execute-api.us-east-1.amazonaws.com/prod/call_node'
     {% else %}
         'https://mryeusnrob.execute-api.us-east-1.amazonaws.com/dev/call_node'
--- models/silver/streamline/streamline__transactions_realtime.sql
+++ models/silver/streamline/streamline__transactions_realtime.sql
@@ -1,9 +1,9 @@
 {{ config (
     materialized = "view",
-    -- post_hook = if_data_call_function(
-    --     func = "{{this.schema}}.udf_get_transactions()",
-    --     target = "{{this.schema}}.{{this.identifier}}"
-    -- )
+    {# post_hook = if_data_call_function(
+        func = "{{this.schema}}.udf_get_transactions()",
+        target = "{{this.schema}}.{{this.identifier}}"
+    ) #}
 ) }}
 
 WITH last_3_days AS (

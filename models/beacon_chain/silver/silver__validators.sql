9adcb70704b2180053a013f9ddfe06b1c8bd2d15
Austin <austin@flipsidecrypto.com>
2022-10-27 16:45:53 -0400

    committies / validators

---
+++ models/beacon_chain/silver/silver__validators.sql
@@ -0,0 +1,55 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = 'block_number',
+    cluster_by = ['_PARTITION_BY_BLOCK_ID'],
+    merge_update_columns = ["block_number"]
+) }}
+
+WITH meta AS (
+
+    SELECT
+        registered_on,
+        last_modified,
+        LEAST(
+            last_modified,
+            registered_on
+        ) AS _inserted_timestamp,
+        file_name
+    FROM
+        TABLE(
+            information_schema.external_table_files(
+                table_name => '{{ source( "bronze_streamline", "validators") }}'
+            )
+        ) A
+
+{% if is_incremental() %}
+WHERE
+    LEAST(
+        registered_on,
+        last_modified
+    ) >= (
+        SELECT
+            COALESCE(MAX(_INSERTED_TIMESTAMP), '1970-01-01' :: DATE) max_INSERTED_TIMESTAMP
+        FROM
+            {{ this }})
+    )
+{% else %}
+)
+{% endif %}
+SELECT
+    VALUE as read_value,
+    _PARTITION_BY_BLOCK_ID,
+    func_type,
+    state_id,
+    block_number,
+    metadata,
+    m._inserted_timestamp :: TIMESTAMP AS _inserted_timestamp,
+    DATA
+FROM
+    {{ source(
+        "bronze_streamline",
+        "validators"
+    ) }}
+    s
+    JOIN meta m
+    ON m.file_name = metadata$filename 

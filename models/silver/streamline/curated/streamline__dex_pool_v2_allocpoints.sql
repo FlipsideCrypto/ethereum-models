0ce3e3b13f0895896428ab8f3df41922d2873f53
Vahid <Vahid@flipsidecrypto.com>
2022-11-02 00:25:22 -0400

    formatting code

--- models/silver/streamline/curated/streamline__dex_pool_v2_allocpoints.sql
+++ models/silver/streamline/curated/streamline__dex_pool_v2_allocpoints.sql
@@ -6,58 +6,66 @@
     tags = ['streamline_view']
 ) }}
 
-with Block_number as (
-select date_trunc('month',block_timestamp) as Month, min(block_number) as block_number
-    from {{ ref('silver__blocks') }}
-where block_timestamp::date >= '2020-02-01'
-  {% if is_incremental() %}
-and date_part(day, Month) = 1 
-and Month >=  (
+WITH block_number AS (
+
+    SELECT
+        DATE_TRUNC(
+            'month',
+            block_timestamp
+        ) AS MONTH,
+        MIN(block_number) AS block_number
+    FROM
+        {{ ref('silver__blocks') }}
+    WHERE
+        block_timestamp :: DATE >= '2020-02-01'
+
+{% if is_incremental() %}
+AND DATE_PART(
+    DAY,
+    MONTH
+) = 1
+AND MONTH >= (
     SELECT
         MAX(
-            Month
+            MONTH
         )
     FROM
         {{ this }}
 )
-{% endif %}  
-    group by 1
+{% endif %}
+GROUP BY
+    1
 ),
-
 function_inputs AS (
     SELECT
         SEQ4() AS function_input
     FROM
         TABLE(GENERATOR(rowcount => 355))
 ),
-
-pool_info as (
+pool_info AS (
     SELECT
-        Month,
+        MONTH,
         block_number,
-        '0xc2edad668740f1aa35e4d8f227fb8e17dca888cd' as contract_address,
+        '0xc2edad668740f1aa35e4d8f227fb8e17dca888cd' AS contract_address,
         '0x1526fe27' AS function_signature,
-        trim(to_char(function_input - 1, 'XXXXXXX')) AS function_input
-    FROM Block_number 
-    join function_inputs
- ),
-
-
-Total_alloc_point as (
+        TRIM(to_char(function_input - 1, 'XXXXXXX')) AS function_input
+    FROM
+        block_number
+        JOIN function_inputs
+),
+total_alloc_point AS (
     SELECT
-        Month,
-         block_number,
-        '0xc2edad668740f1aa35e4d8f227fb8e17dca888cd' as contract_address,
+        MONTH,
+        block_number,
+        '0xc2edad668740f1aa35e4d8f227fb8e17dca888cd' AS contract_address,
         '0x17caf6f1' AS function_signature,
         NULL AS function_input
-    FROM Block_number 
-
+    FROM
+        block_number
 ),
-
-
 FINAL AS (
     SELECT
-        Month,
+        MONTH,
         block_number,
         contract_address,
         'Pool_info_alloc_points' call_name,
@@ -67,25 +75,24 @@ FINAL AS (
         pool_info
     UNION ALL
     SELECT
-        Month,
+        MONTH,
         block_number,
         contract_address,
         'Total_alloc_points' call_name,
-        '' as function_input,
+        '' AS function_input,
         '0x17caf6f1' AS function_signature
     FROM
-        Total_alloc_point
-
+        total_alloc_point
 )
 SELECT
     {{ dbt_utils.surrogate_key(
         ['block_number', 'contract_address', 'function_signature', 'function_input']
     ) }} AS id,
-    month,
+    MONTH,
     block_number,
     contract_address,
     call_name,
     function_signature,
     function_input
 FROM
-    FINAL 
+    FINAL

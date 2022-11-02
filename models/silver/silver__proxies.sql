968382a515ae9f9beae6925af69e2723b097b644
Austin <austin@flipsidecrypto.com>
2022-11-02 17:37:26 -0400

    proxy model

---
+++ models/silver/silver__proxies.sql
@@ -0,0 +1,49 @@
+{{ config (
+    materialized = "incremental",
+    unique_key = "tx_hash",
+    cluster_by = "ROUND(block_number, -3)",
+    merge_update_columns = ["tx_hash"]
+) }}
+
+WITH base AS (
+
+    SELECT
+        tx_hash,
+        block_number,
+        contract_address,
+        CONCAT('0x', SUBSTR(DATA, 27, 40)) AS proxy_address1,
+        CONCAT('0x', SUBSTR(topics [1] :: STRING, 27, 40)) AS proxy_address2,
+        CASE
+            WHEN proxy_address1 = '0x' THEN proxy_address2
+            ELSE proxy_address1
+        END AS proxy_address,
+        topics,
+        DATA,
+        _inserted_timestamp
+    FROM
+        {{ ref('silver__logs') }}
+    WHERE
+        topics [0] :: STRING = '0xbc7cd75a20ee27fd9adebab32041f755214dbc6bffa90cc0225b39da2e5c2d3b'
+        AND tx_status = 'SUCCESS'
+
+{% if is_incremental() %}
+AND _inserted_timestamp >= (
+    SELECT
+        MAX(
+            _inserted_timestamp
+        ) :: DATE - 2
+    FROM
+        {{ this }}
+)
+{% endif %}
+)
+SELECT
+    tx_hash,
+    block_number,
+    contract_address,
+    proxy_address,
+    _inserted_timestamp
+FROM
+    base qualify(ROW_NUMBER() over(PARTITION BY contract_address
+ORDER BY
+    block_number DESC)) = 1 -- can i only look at the most recent proxy address for each contract address?

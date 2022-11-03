7c04f9b200771cc138abadd3d7dec54c16085650
Austin <austin@flipsidecrypto.com>
2022-11-02 20:48:27 -0400

    blur + docs and tests

--- models/gold/core__ez_nft_sales.sql
+++ models/gold/core__ez_nft_sales.sql
@@ -12,11 +12,17 @@ SELECT
     platform_address,
     platform_name,
     platform_exchange_version,
-    case 
-        when right(input_data, 8) = '72db8c0b' then 'Gem'
-        when right(input_data, 8) = '332d1229' then 'Blur'
-        else null 
-        end as aggregator_name,
+    CASE
+        WHEN RIGHT(
+            input_data,
+            8
+        ) = '72db8c0b' THEN 'Gem'
+        WHEN RIGHT(
+            input_data,
+            8
+        ) = '332d1229' THEN 'Blur'
+        ELSE NULL
+    END AS aggregator_name,
     nft_from_address AS seller_address,
     nft_to_address AS buyer_address,
     nft_address,
@@ -50,11 +56,17 @@ SELECT
     platform_address,
     platform_name,
     platform_exchange_version,
-    case 
-        when right(input_data, 8) = '72db8c0b' then 'Gem'
-        when right(input_data, 8) = '332d1229' then 'Blur'
-        else null 
-        end as aggregator_name,
+    CASE
+        WHEN RIGHT(
+            input_data,
+            8
+        ) = '72db8c0b' THEN 'Gem'
+        WHEN RIGHT(
+            input_data,
+            8
+        ) = '332d1229' THEN 'Blur'
+        ELSE NULL
+    END AS aggregator_name,
     nft_from_address AS seller_address,
     nft_to_address AS buyer_address,
     nft_address,
@@ -88,11 +100,17 @@ SELECT
     platform_address,
     platform_name,
     platform_exchange_version,
-    case 
-        when right(input_data, 8) = '72db8c0b' then 'Gem'
-        when right(input_data, 8) = '332d1229' then 'Blur'
-        else null 
-        end as aggregator_name,
+    CASE
+        WHEN RIGHT(
+            input_data,
+            8
+        ) = '72db8c0b' THEN 'Gem'
+        WHEN RIGHT(
+            input_data,
+            8
+        ) = '332d1229' THEN 'Blur'
+        ELSE NULL
+    END AS aggregator_name,
     seller_address,
     buyer_address,
     nft_address,
@@ -126,11 +144,17 @@ SELECT
     platform_address,
     platform_name,
     platform_exchange_version,
-    case 
-        when right(input_data, 8) = '72db8c0b' then 'Gem'
-        when right(input_data, 8) = '332d1229' then 'Blur'
-        else null 
-        end as aggregator_name,
+    CASE
+        WHEN RIGHT(
+            input_data,
+            8
+        ) = '72db8c0b' THEN 'Gem'
+        WHEN RIGHT(
+            input_data,
+            8
+        ) = '332d1229' THEN 'Blur'
+        ELSE NULL
+    END AS aggregator_name,
     seller_address,
     buyer_address,
     nft_address,
@@ -164,11 +188,17 @@ SELECT
     platform_address,
     platform_name,
     platform_exchange_version,
-    case 
-        when right(input_data, 8) = '72db8c0b' then 'Gem'
-        when right(input_data, 8) = '332d1229' then 'Blur'
-        else null 
-        end as aggregator_name,
+    CASE
+        WHEN RIGHT(
+            input_data,
+            8
+        ) = '72db8c0b' THEN 'Gem'
+        WHEN RIGHT(
+            input_data,
+            8
+        ) = '332d1229' THEN 'Blur'
+        ELSE NULL
+    END AS aggregator_name,
     seller_address,
     buyer_address,
     nft_address,
@@ -202,11 +232,17 @@ SELECT
     platform_address,
     platform_name,
     platform_exchange_version,
-    case 
-        when right(input_data, 8) = '72db8c0b' then 'Gem'
-        when right(input_data, 8) = '332d1229' then 'Blur'
-        else null 
-        end as aggregator_name,
+    CASE
+        WHEN RIGHT(
+            input_data,
+            8
+        ) = '72db8c0b' THEN 'Gem'
+        WHEN RIGHT(
+            input_data,
+            8
+        ) = '332d1229' THEN 'Blur'
+        ELSE NULL
+    END AS aggregator_name,
     seller_address,
     buyer_address,
     nft_address,
@@ -240,11 +276,17 @@ SELECT
     platform_address,
     platform_name,
     platform_exchange_version,
-    case 
-        when right(input_data, 8) = '72db8c0b' then 'Gem'
-        when right(input_data, 8) = '332d1229' then 'Blur'
-        else null 
-        end as aggregator_name,
+    CASE
+        WHEN RIGHT(
+            input_data,
+            8
+        ) = '72db8c0b' THEN 'Gem'
+        WHEN RIGHT(
+            input_data,
+            8
+        ) = '332d1229' THEN 'Blur'
+        ELSE NULL
+    END AS aggregator_name,
     seller_address,
     buyer_address,
     nft_address,
@@ -278,11 +320,17 @@ SELECT
     platform_address,
     platform_name,
     platform_exchange_version,
-    case 
-        when right(input_data, 8) = '72db8c0b' then 'Gem'
-        when right(input_data, 8) = '332d1229' then 'Blur'
-        else null 
-        end as aggregator_name,
+    CASE
+        WHEN RIGHT(
+            input_data,
+            8
+        ) = '72db8c0b' THEN 'Gem'
+        WHEN RIGHT(
+            input_data,
+            8
+        ) = '332d1229' THEN 'Blur'
+        ELSE NULL
+    END AS aggregator_name,
     seller_address,
     buyer_address,
     nft_address,
@@ -316,11 +364,17 @@ SELECT
     platform_address,
     platform_name,
     platform_exchange_version,
-    case 
-        when right(input_data, 8) = '72db8c0b' then 'Gem'
-        when right(input_data, 8) = '332d1229' then 'Blur'
-        else null 
-        end as aggregator_name,
+    CASE
+        WHEN RIGHT(
+            input_data,
+            8
+        ) = '72db8c0b' THEN 'Gem'
+        WHEN RIGHT(
+            input_data,
+            8
+        ) = '332d1229' THEN 'Blur'
+        ELSE NULL
+    END AS aggregator_name,
     seller_address,
     buyer_address,
     nft_address,
@@ -345,3 +399,47 @@ SELECT
     origin_function_signature
 FROM
     {{ ref('silver_nft__rarible_sales_update_sept_2022') }}
+UNION ALL
+SELECT
+    block_number,
+    block_timestamp,
+    tx_hash,
+    event_type,
+    platform_address,
+    platform_name,
+    platform_exchange_version,
+    CASE
+        WHEN RIGHT(
+            input_data,
+            8
+        ) = '72db8c0b' THEN 'Gem'
+        WHEN RIGHT(
+            input_data,
+            8
+        ) = '332d1229' THEN 'Blur'
+        ELSE NULL
+    END AS aggregator_name,
+    seller_address,
+    buyer_address,
+    nft_address,
+    project_name,
+    erc1155_value,
+    tokenId,
+    token_metadata,
+    currency_symbol,
+    currency_address,
+    price,
+    price_usd,
+    total_fees,
+    platform_fee,
+    creator_fee,
+    total_fees_usd,
+    platform_fee_usd,
+    creator_fee_usd,
+    tx_fee,
+    tx_fee_usd,
+    origin_from_address,
+    origin_to_address,
+    origin_function_signature
+FROM
+    {{ ref('silver_nft__blur_sales') }}

c7e6305bc95c42210e327e668299fa42983eecab
drethereum <trevor.wenokur@gmail.com>
2022-11-10 15:34:22 -0700

    added project level variable and associated IF statements to on-run-start macros to default UDF/SP updates to False

--- macros/create_sps.sql
+++ macros/create_sps.sql
@@ -1,6 +1,8 @@
 {% macro create_sps() %}
-    {% if target.database == 'ETHEREUM' %}
-        CREATE SCHEMA IF NOT EXISTS _internal;
-        {{ sp_create_prod_clone('_internal') }};
+    {% if var("UPDATE_UDFS_AND_SPS") %}
+        {% if target.database == 'ETHEREUM' %}
+            CREATE SCHEMA IF NOT EXISTS _internal;
+            {{ sp_create_prod_clone('_internal') }};
+        {% endif %}
     {% endif %}
 {% endmacro %}
--- macros/sp_create_cross_db_share_clones.sql
+++ macros/sp_create_cross_db_share_clones.sql
@@ -1,11 +1,13 @@
 {% macro sp_create_cross_db_share_clones() %}
-  {% set sql %}
-  CREATE
-  OR REPLACE PROCEDURE silver.sp_create_cross_db_share_clones() returns variant LANGUAGE SQL AS $$
-BEGIN
-    create or replace transient table silver.token_prices_hourly clone flipside_prod_db.ethereum.token_prices_hourly;
-    create or replace transient table silver.dex_liquidity_pools clone flipside_prod_db.ethereum.dex_liquidity_pools;
-    create or replace transient table silver.labels clone flipside_prod_db.silver_crosschain.address_labels;
-END;$$ {% endset %}
-{% do run_query(sql) %}
+  {% if var("UPDATE_UDFS_AND_SPS") %}
+      {% set sql %}
+      CREATE
+      OR REPLACE PROCEDURE silver.sp_create_cross_db_share_clones() returns variant LANGUAGE SQL AS $$
+    BEGIN
+        create or replace transient table silver.token_prices_hourly clone flipside_prod_db.ethereum.token_prices_hourly;
+        create or replace transient table silver.dex_liquidity_pools clone flipside_prod_db.ethereum.dex_liquidity_pools;
+        create or replace transient table silver.labels clone flipside_prod_db.silver_crosschain.address_labels;
+    END;$$ {% endset %}
+    {% do run_query(sql) %}
+  {% endif %}
 {% endmacro %}

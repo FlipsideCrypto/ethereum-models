0c6e4592228a6b2062538ecc1e22e359037bb8fc
drethereum <trevor.wenokur@gmail.com>
2022-11-10 09:19:38 -0700

    reformatted models with dbt formatter and added unique key column for incremental merge

--- models/silver/prices/silver__token_prices_coin_gecko_hourly.sql
+++ models/silver/prices/silver__token_prices_coin_gecko_hourly.sql
@@ -14,7 +14,8 @@ WITH date_hours AS (
             'crosschain',
             'dim_date_hours'
         ) }}
-    WHERE date_hour >= '2020-05-05'
+    WHERE
+        date_hour >= '2020-05-05'
         AND date_hour <= (
             SELECT
                 MAX(recorded_hour)
@@ -24,19 +25,19 @@ WITH date_hours AS (
                     'hourly_prices_coin_gecko'
                 ) }}
         )
-        {% if is_incremental() %}
-        AND date_hour > (
-            SELECT
-                MAX(recorded_hour)
-            FROM
-                {{ this }}
-        )
-        {% endif %}
-),
 
+{% if is_incremental() %}
+AND date_hour > (
+    SELECT
+        MAX(recorded_hour)
+    FROM
+        {{ this }}
+)
+{% endif %}
+),
 asset_metadata AS (
     SELECT
-        DISTINCT regexp_substr(regexp_replace(token_address,'^x','0x'),'0x[a-zA-Z0-9]*') AS token_address,
+        DISTINCT REGEXP_SUBSTR(REGEXP_REPLACE(token_address, '^x', '0x'), '0x[a-zA-Z0-9]*') AS token_address,
         id,
         platform
     FROM
@@ -44,10 +45,10 @@ asset_metadata AS (
             'crosschain_silver',
             'asset_metadata_coin_gecko'
         ) }}
-    WHERE LOWER(platform) = 'ethereum'
+    WHERE
+        LOWER(platform) = 'ethereum'
         OR id = 'ethereum'
 ),
-
 base_date_hours_address AS (
     SELECT
         date_hour,
@@ -55,36 +56,47 @@ base_date_hours_address AS (
         id
     FROM
         date_hours
-    CROSS JOIN asset_metadata
+        CROSS JOIN asset_metadata
 ),
-
 base_legacy_prices AS (
     SELECT
-        DATE_TRUNC('hour', p.recorded_at) AS recorded_hour,
+        DATE_TRUNC(
+            'hour',
+            p.recorded_at
+        ) AS recorded_hour,
         m.token_address,
         p.asset_id AS id,
-        AVG(p.price) AS close    --returns single price if multiple prices within the 59th minute
-    FROM {{ source(
+        AVG(
+            p.price
+        ) AS CLOSE --returns single price if multiple prices within the 59th minute
+    FROM
+        {{ source(
             'flipside_silver',
             'prices_v2'
-        ) }} p
-    LEFT JOIN asset_metadata m ON m.id = p.asset_id
+        ) }}
+        p
+        LEFT JOIN asset_metadata m
+        ON m.id = p.asset_id
     WHERE
         provider = 'coingecko'
         AND MINUTE(recorded_at) = 59
-        AND recorded_at::DATE < '2022-08-24'
-        AND (LOWER(m.platform) = 'ethereum' OR LOWER(p.asset_id) = 'ethereum') 
-        {% if is_incremental() %}
-        AND recorded_at > (
-            SELECT
-                MAX(recorded_hour)
-            FROM
-                {{ this }}
-        )
-        {% endif %}
-    GROUP BY 1,2,3
-),
+        AND recorded_at :: DATE < '2022-08-24'
+        AND (LOWER(m.platform) = 'ethereum'
+        OR LOWER(p.asset_id) = 'ethereum')
 
+{% if is_incremental() %}
+AND recorded_at > (
+    SELECT
+        MAX(recorded_hour)
+    FROM
+        {{ this }}
+)
+{% endif %}
+GROUP BY
+    1,
+    2,
+    3
+),
 base_prices AS (
     SELECT
         p.recorded_hour,
@@ -95,24 +107,28 @@ base_prices AS (
         {{ source(
             'crosschain_silver',
             'hourly_prices_coin_gecko'
-        ) }} p 
-    LEFT JOIN asset_metadata m ON m.id = p.id
-    WHERE recorded_hour::DATE >= '2022-08-24'
-        AND (LOWER(m.platform) = 'ethereum' OR LOWER(p.id) = 'ethereum')
-        {% if is_incremental() %}
-        AND recorded_hour > (
-            SELECT 
-                MAX(recorded_hour)
-            FROM
-                {{ this }}
-        )
-        {% endif %}
-),
+        ) }}
+        p
+        LEFT JOIN asset_metadata m
+        ON m.id = p.id
+    WHERE
+        recorded_hour :: DATE >= '2022-08-24'
+        AND (LOWER(m.platform) = 'ethereum'
+        OR LOWER(p.id) = 'ethereum')
 
+{% if is_incremental() %}
+AND recorded_hour > (
+    SELECT
+        MAX(recorded_hour)
+    FROM
+        {{ this }}
+)
+{% endif %}
+),
 prices AS (
-    SELECT 
+    SELECT
         *
-    FROM 
+    FROM
         base_legacy_prices
     UNION
     SELECT
@@ -120,7 +136,6 @@ prices AS (
     FROM
         base_prices
 ),
-
 imputed_prices AS (
     SELECT
         d.*,
@@ -134,36 +149,51 @@ imputed_prices AS (
         ) AS imputed_close
     FROM
         base_date_hours_address d
-    LEFT JOIN prices p 
-        ON p.recorded_hour = d.date_hour 
-        AND (p.token_address = d.token_address AND LOWER(p.id) = LOWER(d.id) OR (d.token_address IS NULL AND LOWER(p.id) = 'ethereum'))
+        LEFT JOIN prices p
+        ON p.recorded_hour = d.date_hour
+        AND (
+            p.token_address = d.token_address
+            AND LOWER(
+                p.id
+            ) = LOWER(
+                d.id
+            )
+            OR (
+                d.token_address IS NULL
+                AND LOWER(
+                    p.id
+                ) = 'ethereum'
+            )
+        )
 ),
-
-final AS (
-    
-SELECT
-    p.date_hour AS recorded_hour,
-    p.token_address,
-    p.id,
-    COALESCE(
-        p.hourly_close,
-        p.imputed_close
-    ) AS close,
-    CASE
-        WHEN p.hourly_close IS NULL THEN TRUE
-        ELSE FALSE
-    END AS imputed
-FROM imputed_prices p 
-WHERE close IS NOT NULL
+FINAL AS (
+    SELECT
+        p.date_hour AS recorded_hour,
+        p.token_address,
+        p.id,
+        COALESCE(
+            p.hourly_close,
+            p.imputed_close
+        ) AS CLOSE,
+        CASE
+            WHEN p.hourly_close IS NULL THEN TRUE
+            ELSE FALSE
+        END AS imputed
+    FROM
+        imputed_prices p
+    WHERE
+        CLOSE IS NOT NULL
 )
-
-SELECT 
+SELECT
     recorded_hour,
     token_address,
-    AVG(close) AS close,
+    AVG(CLOSE) AS CLOSE,
     CASE
-        WHEN (CAST(ARRAY_AGG(imputed) AS string)) ILIKE '%true%' THEN TRUE
-        ELSE FALSE 
-    END AS imputed
-FROM final
-GROUP BY 1,2
+        WHEN (CAST(ARRAY_AGG(imputed) AS STRING)) ILIKE '%true%' THEN TRUE
+        ELSE FALSEEND AS imputed,
+        concat_ws('-', recorded_hour, COALESCE(token_address, 'n-a')) AS _unique_key
+        FROM
+            FINAL
+        GROUP BY
+            1,
+            2

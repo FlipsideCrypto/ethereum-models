29ec630e5f72f2d346aa92e5abde07b2f70e3877
Vahid <Vahid@flipsidecrypto.com>
2022-11-15 14:17:30 -0500

    fixing columns

--- models/gold/dex/core__fact_dex_pool_weekly_reads.sql
+++ models/gold/dex/core__fact_dex_pool_weekly_reads.sql
@@ -5,12 +5,13 @@
 ) }}
 
 SELECT
-    date,
+    Date,
     block_number,
     pid,
     contract_address,
+    pool_address,
     allocation_points,
-    call_name,
+    function_name,
     function_signature
 FROM
     {{ ref('silver_dex__v2_pool_weekly_metrics') }}

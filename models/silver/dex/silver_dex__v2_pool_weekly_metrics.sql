19291b826e1bf18ad9b78baf888b5fe49d96b3c4
Vahid <Vahid@flipsidecrypto.com>
2022-11-15 11:59:14 -0500

    enriching the weekly models

--- models/silver/dex/silver_dex__v2_pool_weekly_metrics.sql
+++ models/silver/dex/silver_dex__v2_pool_weekly_metrics.sql
@@ -6,8 +6,23 @@
   cluster_by = ['_inserted_timestamp::DATE']
 ) }}
 
+with Block_date as (
+    SELECT block_timestamp::date as date, block_number, _inserted_timestamp 
+    from {{ref('silver__blocks')}} 
+    where date >= '2020-09-01'
+    {% if is_incremental() %}
+AND _inserted_timestamp >= (
+    SELECT
+        MAX(
+            _inserted_timestamp
+        ) :: DATE 
+    FROM
+        {{ this }}
+)
+{% endif %}
+),
 
-WITH V1_allocpoint_per_pool AS (
+V1_allocpoint_per_pool AS (
 
   SELECT
     a.contract_address,
@@ -24,25 +39,16 @@ WITH V1_allocpoint_per_pool AS (
     PUBLIC.udf_hex_to_int(A.function_input :: STRING)  as pid,
     case when function_signature = '0x1526fe27' then 'poolInfo' else 'totalAllocPoint' end as function_name,
     call_name,
-    b.block_timestamp :: DATE AS DATE,
+    b.DATE,
     A._inserted_timestamp
   FROM
-  {{ ref('bronze__successful_reads') }} A
-    JOIN {{ ref('silver__blocks') }} b
+    {{ ref('bronze__successful_reads') }} A
+    JOIN Block_date b
     ON A.block_number = b.block_number
   WHERE read_output :: STRING <> '0x'
     and contract_address = '0xc2edad668740f1aa35e4d8f227fb8e17dca888cd' --Sushiswap Masterchef
     and function_signature in ('0x1526fe27','0x17caf6f1')
-{% if is_incremental() %}
-AND A._inserted_timestamp >= (
-  SELECT
-    MAX(
-      _inserted_timestamp
-    ) :: DATE
-  FROM
-    {{ this }}
-)
-{% endif %}
+
 ),
 
 V2_PoolID_per_pool as (
@@ -52,27 +58,19 @@ V2_PoolID_per_pool as (
     A.block_number,
     function_signature,
     PUBLIC.udf_hex_to_int(A.function_input :: STRING)  as pid,
+    function_input,
     'lpToken' as function_name,
     call_name,
-    b.block_timestamp :: DATE AS DATE,
+    b.DATE,
     A._inserted_timestamp
   FROM
   {{ ref('bronze__successful_reads') }} A
-    JOIN {{ ref('silver__blocks') }} b
+    JOIN Block_date b
     ON A.block_number = b.block_number
   WHERE read_output :: STRING <> '0x'
     and contract_address = '0xef0881ec094552b2e128cf945ef17a6752b4ec5d' --Sushiswap MasterchefV2
     and function_signature = '0x78ed5d1f'
-    {% if is_incremental() %}
-AND A._inserted_timestamp >= (
-  SELECT
-    MAX(
-      _inserted_timestamp
-    ) :: DATE
-  FROM
-    {{ this }}
-)
-{% endif %}
+
 ),
 
 V2_allocpoint_per_poolID AS (
@@ -80,30 +78,21 @@ V2_allocpoint_per_poolID AS (
     a.contract_address,
     a.block_number,
     PUBLIC.udf_hex_to_int(A.function_input :: STRING)  as pid,
+    function_input,
     call_name,
     'poolInfo' as function_name,
     function_signature,
-    A._inserted_timestamp,
+    a._inserted_timestamp,
     PUBLIC.udf_hex_to_int(
       segmented_data [2] :: STRING
     ) AS allocation_points
   FROM
    {{ ref('bronze__successful_reads') }} A
-    JOIN {{ ref('silver__blocks') }} b
+    JOIN Block_date b
     ON A.block_number = b.block_number
   WHERE read_output :: STRING <> '0x'
     and contract_address = '0xef0881ec094552b2e128cf945ef17a6752b4ec5d' --Sushiswap MasterchefV2
     and function_signature = '0x1526fe27'
-    {% if is_incremental() %}
-AND A._inserted_timestamp >= (
-  SELECT
-    MAX(
-      _inserted_timestamp
-    ) :: DATE
-  FROM
-    {{ this }}
-)
-{% endif %}
 
 ),
 
@@ -115,15 +104,15 @@ A.pid,
 A.contract_address,
 A.pool_address,
 A.block_number,
+A._inserted_timestamp,
 b.allocation_points,
 b.call_name,
 b.function_name,
-b.function_signature,
-a._inserted_timestamp
+b.function_signature
 from V2_PoolID_per_pool A
 left join V2_allocpoint_per_poolID b
 on a.block_number = b.block_number
-and a.pid = b.pid
+and a.function_input = b.function_input
 and a.contract_address = b.contract_address
 )
 
@@ -150,3 +139,6 @@ and a.contract_address = b.contract_address
     function_signature,
     _inserted_timestamp
     from V2_allocpoint_per_pool
+
+
+

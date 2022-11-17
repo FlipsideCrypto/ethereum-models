73b84bdb3508f474e741641c4b0f3418c1b8aa32
drethereum <trevor.wenokur@gmail.com>
2022-11-17 10:08:15 -0700

    finalized core token price model

--- models/gold/core__fact_hourly_token_prices.sql
+++ models/gold/core__fact_hourly_token_prices.sql
@@ -2,52 +2,13 @@
     materialized = 'view'
 ) }}
 
-WITH all_providers AS (
-
-SELECT
-    'coingecko' AS provider,
-    recorded_hour AS hour,
-    token_address,
-    close AS price,
-    imputed AS is_imputed
-FROM
-    {{ ref('silver__token_prices_coin_gecko_hourly') }} 
-UNION
-SELECT
-    'coinmarketcap' AS provider,
-    recorded_hour AS hour,
-    token_address,
-    close AS price,
-    imputed AS is_imputed
-FROM
-    {{ ref('silver__token_prices_coin_market_cap_hourly')}} 
-),
-
-grouped AS (
-
-SELECT 
-    provider,
-    hour,
-    token_address,
-    price,
-    is_imputed,
-    CASE
-        WHEN provider = 'coinmarketcap' AND is_imputed = FALSE THEN 1
-        WHEN provider = 'coingecko' AND is_imputed = FALSE THEN 2
-        WHEN provider = 'coinmarketcap' AND is_imputed = TRUE THEN 3
-        WHEN provider = 'coingecko' AND is_imputed = TRUE THEN 4
-    END AS priority
-FROM all_providers
-)
-
 SELECT
     hour,
     token_address,
-    c.symbol,
-    c.decimals,
+    symbol,
+    decimals,
     price,
     is_imputed
-FROM grouped g
-LEFT JOIN {{ ref('core__dim_contracts') }} c 
-    ON LOWER(c.address) = LOWER(g.token_address)
-QUALIFY(ROW_NUMBER() OVER(PARTITION BY hour, token_address ORDER BY priority ASC)) = 1
+FROM {{ ref(
+        'silver__token_prices_all_providers_hourly'
+     ) }}

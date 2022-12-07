4af51caf22dd3e4414406054e386a9b0966dc210
drethereum <trevor.wenokur@gmail.com>
2022-12-07 12:25:20 -0700

    added synthetix_swaps to ez_dex_swaps table

--- models/gold/dex/core__ez_dex_swaps.sql
+++ models/gold/dex/core__ez_dex_swaps.sql
@@ -183,7 +183,45 @@ WHERE
       {{ this }}
   )
 {% endif %}
+),
+synthetix_swaps AS (
+  SELECT
+    block_number,
+    block_timestamp,
+    tx_hash,
+    origin_function_signature,
+    origin_from_address,
+    origin_to_address,
+    contract_address,
+    pool_name,
+    event_name,
+    amount_in,
+    amount_in * prices_in.price AS amount_in_usd,
+    amount_out,
+    amount_out * prices_out.price AS amount_out_usd,
+    sender,
+    tx_from,
+    tx_to,
+    event_index,
+    platform,
+    token_in,
+    token_out,
+    symbol_in,
+    symbol_out,
+    _log_id,
+    _inserted_timestamp
+  FROM {{ ref('silver_dex__synthetix_swaps')}}
+{% if is_incremental() %}
+WHERE
+  _inserted_timestamp >= (
+    SELECT
+      MAX(_inserted_timestamp) :: DATE - 2
+    FROM
+      {{ this }}
+  )
+{% endif %}
 )
+
 SELECT
   block_number,
   block_timestamp,
@@ -291,3 +329,30 @@ SELECT
   _inserted_timestamp
 FROM
   univ3_swaps
+UNION ALL
+SELECT
+  block_number,
+  block_timestamp,
+  tx_hash,
+  origin_function_signature,
+  origin_from_address,
+  origin_to_address,
+  contract_address,
+  pool_name,
+  event_name,
+  amount_in,
+  amount_in_usd,
+  amount_out,
+  amount_out_usd,
+  sender,
+  tx_to,
+  event_index,
+  platform,
+  token_in,
+  token_out,
+  symbol_in,
+  symbol_out,
+  _log_id,
+  _inserted_timestamp
+FROM
+  synthetix_swaps

352c73fd6a7cff774f1a789dc8f5992cca55e17a
Julius Remigio <14811322+juls858@users.noreply.github.com>
2022-12-07 21:48:09 -0800

    - add create_udf_bulk_decode_logs to create_udfs.sql

--- macros/create_udfs.sql
+++ macros/create_udfs.sql
@@ -2,8 +2,8 @@
     {% if var("UPDATE_UDFS_AND_SPS") %}
         {% set sql %}
         CREATE schema if NOT EXISTS silver;
-    {{ create_js_hex_to_int() }};
-    {{ create_udf_hex_to_int(
+{{ create_js_hex_to_int() }};
+{{ create_udf_hex_to_int(
             schema = "public"
         ) }}
         {{ create_udf_hex_to_int_with_inputs(
@@ -33,6 +33,7 @@
             {{ create_udf_get_beacon_blocks() }}
             {{ create_udf_decode_array_string() }}
             {{ create_udf_decode_array_object() }}
+            {{ create_udf_bulk_decode_logs() }}
 
             {% endset %}
             {% do run_query(sql) %}

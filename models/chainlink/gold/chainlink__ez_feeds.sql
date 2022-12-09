6b3360a1e15b087364b200a662361fbe67488de8
Austin <austin@flipsidecrypto.com>
2022-12-09 18:03:34 -0500

    add docs

--- models/chainlink/gold/chainlink__ez_feeds.sql
+++ models/chainlink/gold/chainlink__ez_feeds.sql
@@ -6,7 +6,8 @@
 
 SELECT
     contract_address AS feed_address,
-    block_number,
+    A.block_number,
+    block_timestamp,
     feed_name,
     read_result AS latest_answer_unadj,
     latest_answer_unadj / pow(
@@ -17,8 +18,11 @@ SELECT
     feed_added AS feed_added_date,
     created_block_number
 FROM
-    {{ ref('silver__chainlink_feeds') }}
+    {{ ref('silver__chainlink_feeds') }} A
     JOIN {{ ref('silver__chainlink_feeds_seed') }}
     ON contract_address = feed_address
     JOIN {{ ref('silver__contracts') }}
     ON contract_address = address
+    JOIN {{ ref('silver__blocks') }}
+    b
+    ON A.block_number = b.block_number

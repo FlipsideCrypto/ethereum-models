f296b6a4b8cdba76a14d48937f9fbc1c693a3dda
Julius Remigio <14811322+juls858@users.noreply.github.com>
2022-12-14 21:00:31 -0800

    - moved template query to macro

--- models/silver/streamline/decoder/streamline__decode_logs_history_008166633_008257623.sql
+++ models/silver/streamline/decoder/streamline__decode_logs_history_008166633_008257623.sql
@@ -5,27 +5,7 @@
 
 {% set start = this.identifier.split("_") [-2] %}
 {% set stop = this.identifier.split("_") [-1] %}
-
-SELECT
-    l.block_number,
-    l._log_id,
-    abi.data AS abi,
-    l.data
-FROM
-    {{ ref("streamline__decode_logs") }}
-    l
-    INNER JOIN {{ ref("silver__abis") }}
-    abi
-    ON l.abi_address = abi.contract_address
-WHERE
-    l.block_number BETWEEN {{ start }}
-    AND {{ stop }}
-    AND _log_id NOT IN (
-        SELECT
-            _log_id
-        FROM
-            {{ ref("streamline__complete_decode_logs") }}
-        WHERE
-            block_number BETWEEN {{ start }}
-            AND {{ stop }}
-    )
+{{ decode_logs_history(
+    start,
+    stop
+) }}

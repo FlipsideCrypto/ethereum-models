ad4d287aa8439ea99ca0a4a8b8c848184009ddcd
Julius Remigio <14811322+juls858@users.noreply.github.com>
2022-12-14 20:48:26 -0800

    - add dbt variable for wait time

--- models/silver/streamline/decoder/streamline__decode_logs_history_013532031_013562780.sql
+++ models/silver/streamline/decoder/streamline__decode_logs_history_013532031_013562780.sql
@@ -1,6 +1,6 @@
 {{ config (
     materialized = "view",
-    post_hook = [if_data_call_function( func = "{{this.schema}}.udf_bulk_decode_logs(object_construct('sql_source', '{{this.identifier}}','producer_batch_size', 20000000,'producer_limit_size', 20000000))", target = "{{this.schema}}.{{this.identifier}}" ),"call system$wait(400)" ]
+    post_hook = [if_data_call_function( func = "{{this.schema}}.udf_bulk_decode_logs(object_construct('sql_source', '{{this.identifier}}','producer_batch_size', 20000000,'producer_limit_size', 20000000))", target = "{{this.schema}}.{{this.identifier}}" ),"call system$wait(" ~ var("WAIT", 400) ~ "}" ]
 ) }}
 
 {% set start = this.identifier.split("_") [-2] %}

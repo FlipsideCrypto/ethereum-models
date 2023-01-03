e00d5ee84e6391d348b0497a296c0bbdcacfdb4b
drethereum <71602799+drethereum@users.noreply.github.com>
2023-01-03 14:46:11 -0700

    fix/snapshot-pipeline-1 (#325)
    * removed incremental logic on proposal CTE and removed where clause in bronze votes
    
    * adj spacing

--- models/silver/silver__snapshot_temp.sql
+++ models/silver/silver__snapshot_temp.sql
@@ -18,17 +18,6 @@ WITH proposals AS (
         proposal_end_time,
         _inserted_timestamp
     FROM {{ ref('bronze_api__snapshot_proposals') }}
-
-{% if is_incremental() %}
-WHERE _inserted_timestamp >= (
-    SELECT
-        MAX(
-            _inserted_timestamp
-        )
-    FROM
-        {{ this }}
-    )
-{% endif %}
 ),  
 votes AS ( 
     SELECT
@@ -41,7 +30,6 @@ votes AS (
         vote_option,
         _inserted_timestamp
     FROM {{ ref('bronze_api__snapshot_votes') }}
-
 {% if is_incremental() %}
 WHERE _inserted_timestamp >= (
     SELECT

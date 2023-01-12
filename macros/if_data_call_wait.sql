2e09a6a55f7ff67ee0ff473a5f93c95d018ffa22
austin <austin@flipsidecrypto.com>
2023-01-12 00:09:50 -0500

    do not merge - broken macro

--- macros/if_data_call_wait.sql
+++ macros/if_data_call_wait.sql
@@ -1,22 +1,15 @@
-{% macro if_data_call_wait(
-        func,
-        target
-    ) %}
-    {{ if_data_call_function(
-        func,
-        target
-    )}};
-
-    SELECT
-        system$wait({{ var("WAIT", 600) }})
-    WHERE
-        EXISTS(
-            SELECT
-                1
-            FROM
-                {{ target }}
-            LIMIT
-                1
-        );
-
+{% macro if_data_call_wait(func, target) %}
+        {{ if_data_call_function(func, target) }}
+    {% if execute %}
+        {% set query %}
+            SELECT 1 FROM {{ target }} LIMIT 1
+        {% endset %}
+        {% set result = run_query(query) %}
+        {% if result[0][0] == 1 %}
+            {% set wait_query %}
+                CALL SYSTEM$WAIT({{ var("WAIT", 600) }})
+            {% endset %}
+            {% do run_query(wait_query) %}
+        {% endif %}
+    {% endif %}
 {% endmacro %}

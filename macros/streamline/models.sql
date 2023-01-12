f896e54cd061b73d9ff525e3840dd44141b34265
Austin <93135983+austinFlipside@users.noreply.github.com>
2023-01-12 17:15:26 -0500

    An 2671/decoder speed (#345)
    * future history files
    
    * stash
    
    * new macro + future history fles
    
    * do not merge - broken macro
    
    * new macro

--- macros/streamline/models.sql
+++ macros/streamline/models.sql
@@ -2,6 +2,16 @@
         start,
         stop
     ) %}
+    WITH look_back AS (
+        SELECT
+            block_number
+        FROM
+            {{ ref("_max_block_by_date") }}
+            qualify ROW_NUMBER() over (
+                ORDER BY
+                    block_number DESC
+            ) = 1
+    )
 SELECT
     l.block_number,
     l._log_id,
@@ -14,15 +24,31 @@ FROM
     abi
     ON l.abi_address = abi.contract_address
 WHERE
-    l.block_number BETWEEN {{ start }}
-    AND {{ stop }}
+    (
+        l.block_number BETWEEN {{ start }}
+        AND {{ stop }}
+    )
+    AND l.block_number <= (
+        SELECT
+            block_number
+        FROM
+            look_back
+    )
     AND _log_id NOT IN (
         SELECT
             _log_id
         FROM
             {{ ref("streamline__complete_decode_logs") }}
         WHERE
-            block_number BETWEEN {{ start }}
-            AND {{ stop }}
+            (
+                block_number BETWEEN {{ start }}
+                AND {{ stop }}
+            )
+            AND block_number <= (
+                SELECT
+                    block_number
+                FROM
+                    look_back
+            )
     )
 {% endmacro %}

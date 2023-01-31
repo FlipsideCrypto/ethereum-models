beba518c5da002de07f65a8356a58cd3d204c634
austin <austin@flipsidecrypto.com>
2023-01-31 09:20:19 -0500

    rework transfers without event inputs / name

--- models/silver/silver__transfers.sql
+++ models/silver/silver__transfers.sql
@@ -10,23 +10,22 @@ WITH logs AS (
     SELECT
         _log_id,
         block_number,
-        block_timestamp,
         tx_hash,
+        block_timestamp,
         origin_function_signature,
         origin_from_address,
         origin_to_address,
-        contract_address,
-        event_name,
+        contract_address :: STRING AS contract_address,
+        CONCAT('0x', SUBSTR(topics [1], 27, 40)) :: STRING AS from_address,
+        CONCAT('0x', SUBSTR(topics [2], 27, 40)) :: STRING AS to_address,
+        PUBLIC.udf_hex_to_int(SUBSTR(DATA, 3, 64)) :: FLOAT AS raw_amount,
         event_index,
-        event_inputs,
-        topics,
-        DATA,
-        ingested_at :: TIMESTAMP AS ingested_at,
-        _inserted_timestamp :: TIMESTAMP AS _inserted_timestamp
+        _inserted_timestamp
     FROM
         {{ ref('silver__logs') }}
     WHERE
-        tx_status = 'SUCCESS'
+        topics [0] :: STRING = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
+        AND tx_status = 'SUCCESS'
 
 {% if is_incremental() %}
 AND _inserted_timestamp >= (
@@ -38,93 +37,6 @@ AND _inserted_timestamp >= (
         {{ this }}
 )
 {% endif %}
-),
-transfers AS (
-    SELECT
-        _log_id,
-        block_number,
-        tx_hash,
-        block_timestamp,
-        origin_function_signature,
-        origin_from_address,
-        origin_to_address,
-        contract_address :: STRING AS contract_address,
-        event_inputs :from :: STRING AS from_address,
-        event_inputs :to :: STRING AS to_address,
-        event_inputs :value :: FLOAT AS raw_amount,
-        event_index,
-        ingested_at,
-        _inserted_timestamp
-    FROM
-        logs
-    WHERE
-        event_name = 'Transfer'
-        AND raw_amount IS NOT NULL
-),
-find_missing_events AS (
-    SELECT
-        _log_id,
-        block_number,
-        tx_hash,
-        block_timestamp,
-        origin_function_signature,
-        origin_from_address,
-        origin_to_address,
-        contract_address :: STRING AS contract_address,
-        CONCAT('0x', SUBSTR(topics [1], 27, 40)) :: STRING AS from_address,
-        CONCAT('0x', SUBSTR(topics [2], 27, 40)) :: STRING AS to_address,
-        COALESCE(udf_hex_to_int(topics [3] :: STRING), udf_hex_to_int(SUBSTR(DATA, 3, 64))) :: FLOAT AS raw_amount,
-        event_index,
-        ingested_at,
-        _inserted_timestamp
-    FROM
-        logs
-    WHERE
-        event_name IS NULL
-        AND contract_address IN (
-            SELECT
-                DISTINCT contract_address
-            FROM
-                transfers
-        )
-        AND topics [0] :: STRING = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
-),
-all_transfers AS (
-    SELECT
-        _log_id,
-        tx_hash,
-        block_number,
-        block_timestamp,
-        origin_function_signature,
-        origin_from_address,
-        origin_to_address,
-        contract_address,
-        from_address,
-        to_address,
-        raw_amount,
-        event_index,
-        ingested_at,
-        _inserted_timestamp
-    FROM
-        transfers
-    UNION ALL
-    SELECT
-        _log_id,
-        tx_hash,
-        block_number,
-        block_timestamp,
-        origin_function_signature,
-        origin_from_address,
-        origin_to_address,
-        contract_address,
-        from_address,
-        to_address,
-        raw_amount,
-        event_index,
-        ingested_at,
-        _inserted_timestamp
-    FROM
-        find_missing_events
 )
 SELECT
     _log_id,
@@ -138,10 +50,13 @@ SELECT
     from_address,
     to_address,
     raw_amount,
-    ingested_at,
     _inserted_timestamp,
     event_index
 FROM
-    all_transfers qualify(ROW_NUMBER() over(PARTITION BY _log_id
+    logs
+WHERE
+    raw_amount IS NOT NULL
+    AND to_address IS NOT NULL
+    AND from_address IS NOT NULL qualify(ROW_NUMBER() over(PARTITION BY _log_id
 ORDER BY
     _inserted_timestamp DESC)) = 1

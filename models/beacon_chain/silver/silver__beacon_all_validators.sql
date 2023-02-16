691cd9d7ad78d6299829f5546d6cb51941939ae4
drethereum <trevor.wenokur@gmail.com>
2023-02-16 16:37:22 -0700

    all validators model

---
+++ models/beacon_chain/silver/silver__beacon_all_validators.sql
@@ -0,0 +1,57 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = 'block_number',
+    cluster_by = ['_PARTITION_BY_BLOCK_ID'],
+    merge_update_columns = ["block_number"]
+) }}
+
+WITH meta AS (
+
+    SELECT
+        registered_on,
+        last_modified,
+        LEAST(
+            last_modified,
+            registered_on
+        ) AS _inserted_timestamp,
+        file_name
+    FROM
+        TABLE(
+            information_schema.external_table_files(
+                table_name => '{{ source( "bronze_streamline", "validators") }}'
+            )
+        )
+
+{% if is_incremental() %}
+WHERE
+    LEAST(
+        registered_on,
+        last_modified
+    ) >= (
+        SELECT
+            COALESCE(MAX(_inserted_timestamp), '1970-01-01' :: DATE) AS max_inserted_timestamp
+        FROM
+            {{ this }})
+{% endif %}
+)
+SELECT
+    VALUE AS read_value,
+    _PARTITION_BY_BLOCK_ID,
+    func_type,
+    state_id,
+    block_number,
+    metadata,
+    INDEX,
+    m._inserted_timestamp :: TIMESTAMP AS _inserted_timestamp,
+    DATA,
+    {{ dbt_utils.surrogate_key(['block_number', 'func_type', 'index']) }} AS id
+FROM
+    {{ source(
+        "bronze_streamline",
+        "validators"
+    ) }}
+    s
+    JOIN meta m
+    ON m.file_name = metadata$filename qualify(ROW_NUMBER() over (PARTITION BY id
+ORDER BY
+    _inserted_timestamp DESC)) = 1

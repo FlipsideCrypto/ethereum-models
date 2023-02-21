d5dcfe0548a6df4226e9b9d212cbe4a525f1270f
drethereum <trevor.wenokur@gmail.com>
2023-02-21 12:42:42 -0700

    typo

--- models/silver/streamline/streamline__beacon_validator_balances_realtime.sql
+++ models/silver/streamline/streamline__beacon_validator_balances_realtime.sql
@@ -19,7 +19,7 @@ SELECT
     slot_number,
     state_id
 FROM
-    {{ ref("streamline__beacon_validators_balances") }}
+    {{ ref("streamline__beacon_validator_balances") }}
 WHERE
     (
         slot_number >= (
@@ -35,7 +35,7 @@ SELECT
     slot_number,
     state_id
 FROM
-    {{ ref("streamline__complete_beacon_validators_balances") }}
+    {{ ref("streamline__complete_beacon_validator_balances") }}
 WHERE
     slot_number >= (
         SELECT

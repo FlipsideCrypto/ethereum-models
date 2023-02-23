6bfd6fa65284e6726879455b833f207f7c5cbbf3
drethereum <trevor.wenokur@gmail.com>
2023-02-23 11:41:46 -0700

    removed streamline curve pool reads

--- models/silver/streamline/streamline__contract_reads.sql
+++ models/silver/streamline/streamline__contract_reads.sql
@@ -12,17 +12,6 @@ SELECT
     function_input :: STRING AS function_input,
     _inserted_timestamp :: TIMESTAMP AS _inserted_timestamp
 FROM
-    {{ ref('streamline__curve_pool_reads') }}
-UNION ALL
-SELECT
-    id :: STRING AS id,
-    block_number :: INTEGER AS block_number,
-    contract_address :: STRING AS contract_address,
-    call_name :: STRING AS call_name,
-    function_signature :: STRING AS function_signature,
-    function_input :: STRING AS function_input,
-    _inserted_timestamp :: TIMESTAMP AS _inserted_timestamp
-FROM
     {{ ref('streamline__aave_incentives_read') }}
 UNION ALL
 SELECT

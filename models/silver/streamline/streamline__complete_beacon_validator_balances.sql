8054e1724a6eb658562f21ed8a2761bd5e6b2d21
xiuy001 <97179309+xiuy001@users.noreply.github.com>
2023-02-28 15:20:23 -0500

    updated (#406)
    * updated
    
    * updated

--- models/silver/streamline/streamline__complete_beacon_validator_balances.sql
+++ models/silver/streamline/streamline__complete_beacon_validator_balances.sql
@@ -1,49 +1,52 @@
 {{ config (
     materialized = "incremental",
     unique_key = "id",
-    cluster_by = "ROUND(slot_number, -2)",
+    cluster_by = "ROUND(slot_number, -3)",
     merge_update_columns = ["id"],
     post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION on equality(id)"
 ) }}
 
-WITH max_date AS (
+WITH meta AS (
 
     SELECT
-        COALESCE(MAX(_INSERTED_TIMESTAMP), '1970-01-01' :: DATE) max_INSERTED_TIMESTAMP
-    FROM
-        {{ this }}
-),
-
-meta AS (
-    SELECT
+        registered_on,
         last_modified,
-        CAST(
-            SPLIT_PART(SPLIT_PART(file_name, '/', 4), '_', 1) AS INTEGER
-        ) AS _partition_by_block_number
+        file_name
     FROM
         TABLE(
             information_schema.external_table_files(
                 table_name => '{{ source( "bronze_streamline", "beacon_validator_balances") }}'
             )
         ) A
-    {% if is_incremental() %}
-        WHERE
-            last_modified >= (
-                SELECT
-                    MAX(max_INSERTED_TIMESTAMP)
-                FROM
-                    max_date
-            )
-    {% endif %}
 
+{% if is_incremental() %}
+WHERE
+    LEAST(
+        registered_on,
+        last_modified
+    ) >= (
+        SELECT
+            COALESCE(MAX(_INSERTED_TIMESTAMP), '1970-01-01' :: DATE) max_INSERTED_TIMESTAMP
+        FROM
+            {{ this }})
+    ),
+    partitions AS (
+        SELECT
+            CAST(
+                SPLIT_PART(SPLIT_PART(file_name, '/', 4), '_', 1) AS INTEGER
+            ) AS _partition_by_block_number
+        FROM
+            meta
+    )
+{% else %}
 )
-
+{% endif %}
 SELECT
     MD5(
         CAST(COALESCE(CAST(block_number AS text), '') AS text)
     ) AS id,
     block_number AS slot_number,
-    last_modified AS _inserted_timestamp
+    registered_on AS _inserted_timestamp
 FROM
     {{ source(
         "bronze_streamline",
@@ -51,7 +54,26 @@ FROM
     ) }}
     t
     JOIN meta b
-    ON b._partition_by_block_number = t._partition_by_block_id
+    ON b.file_name = metadata$filename
+
+{% if is_incremental() %}
+JOIN partitions p
+ON p._partition_by_block_number = t._partition_by_block_id
+{% endif %}
 WHERE
-    b._partition_by_block_number = t._partition_by_block_id
-GROUP BY 1,2,3
+    DATA :error :code IS NULL
+    OR DATA :error :code NOT IN (
+        '-32000',
+        '-32001',
+        '-32002',
+        '-32003',
+        '-32004',
+        '-32005',
+        '-32006',
+        '-32007',
+        '-32008',
+        '-32009',
+        '-32010'
+    ) qualify(ROW_NUMBER() over (PARTITION BY id
+ORDER BY
+    _inserted_timestamp DESC)) = 1

08713b85694e45d7bdabf2fa33c5098690f65eb3
Ryan-Loofy <63126328+Ryan-Loofy@users.noreply.github.com>
2023-03-09 14:24:16 -0500

    Add logic to abi realtime model

--- models/silver/streamline/streamline__contract_abis_realtime.sql
+++ models/silver/streamline/streamline__contract_abis_realtime.sql
@@ -6,14 +6,33 @@
     )
 ) }}
 
+WITH last_3_days AS (
 
+    SELECT
+        block_number
+    FROM
+        {{ ref("_max_block_by_date") }}
+        qualify ROW_NUMBER() over (
+            ORDER BY
+                block_number DESC
+        ) = 3
+)
 SELECT
     contract_address,
     block_number
 FROM
     {{ ref("streamline__contract_addresses") }}
 WHERE
-    block_number > 15000000
+    (
+        block_number >= (
+            SELECT
+                block_number
+            FROM
+                last_3_days
+        )
+        OR block_number > 15000000
+    )
+    AND block_number IS NOT NULL
 EXCEPT
 SELECT
     contract_address,
@@ -21,4 +40,10 @@ SELECT
 FROM
     {{ ref("streamline__complete_contract_abis") }}
 WHERE
-    block_number > 15000000
+    block_number >= (
+        SELECT
+            block_number
+        FROM
+            last_3_days
+    ) {# TODO: OR can be removed once historical load is complete #}
+    OR block_number > 15000000

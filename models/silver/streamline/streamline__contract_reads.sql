8230c2061402fae0b3f587baf38e401e68874497
austin <austin@flipsidecrypto.com>
2023-03-14 00:36:12 -0400

    clean

--- models/silver/streamline/streamline__contract_reads.sql
+++ models/silver/streamline/streamline__contract_reads.sql
@@ -100,19 +100,4 @@ SELECT
     function_input :: STRING AS function_input,
     _inserted_timestamp :: TIMESTAMP AS _inserted_timestamp
 FROM
-<<<<<<< HEAD
-    {{ ref('streamline__contract_meta_read') }}
-=======
-    {{ ref('streamline__chainlink_prices') }}
->>>>>>> 6f21f2454d2f9dab42e2addc94be0710ac35570d
-UNION ALL
-SELECT
-    id :: STRING AS id,
-    block_number :: INTEGER AS block_number,
-    contract_address :: STRING AS contract_address,
-    call_name :: STRING AS call_name,
-    function_signature :: STRING AS function_signature,
-    function_input :: STRING AS function_input,
-    _inserted_timestamp :: TIMESTAMP AS _inserted_timestamp
-FROM
     {{ ref('streamline__aave_reserve_config_reads') }}

977e0fc1425612f1e53a0c49776df01ce1e77e68
austin <austin@flipsidecrypto.com>
2023-03-16 17:20:46 -0400

    use vars

--- models/silver/streamline/core/streamline__blocks.sql
+++ models/silver/streamline/core/streamline__blocks.sql
@@ -3,18 +3,19 @@
     tags = ['streamline_view']
 ) }}
 
-
 {% if execute %}
-{% set height = run_query('SELECT streamline.udf_get_chainhead()') %}
-{% set block_height = height.columns[0].values()[0] %}
+    {% set height = run_query('SELECT streamline.udf_get_chainhead()') %}
+    {% set block_height = height.columns [0].values() [0] %}
 {% else %}
-{% set block_height = 0 %}
+    {% set block_height = 0 %}
 {% endif %}
 
 SELECT
-    height as block_number,
-    REPLACE(CONCAT_WS('', '0x', TO_CHAR(block_number, 'XXXXXXXX')), ' ', '') AS block_number_hex
+    height AS block_number,
+    REPLACE(
+        concat_ws('', '0x', to_char(block_number, 'XXXXXXXX')),
+        ' ',
+        ''
+    ) AS block_number_hex
 FROM
-    TABLE(streamline.udtf_get_base_table({{block_height}}))
-WHERE
-    height >= 15537394 -- Start block for ETH 2.0 Mainnet chain data
+    TABLE(streamline.udtf_get_base_table({{ block_height }}))

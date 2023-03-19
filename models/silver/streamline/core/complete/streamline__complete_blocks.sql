646e386a63e280449821d93644d8bb3ec3ee2ce0
austin <austin@flipsidecrypto.com>
2023-03-19 11:25:59 -0400

    configs

--- models/silver/streamline/core/complete/streamline__complete_blocks.sql
+++ models/silver/streamline/core/complete/streamline__complete_blocks.sql
@@ -2,7 +2,6 @@
     materialized = "incremental",
     unique_key = "id",
     cluster_by = "ROUND(block_number, -3)",
-    merge_update_columns = ["id"],
     post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION on equality(id)"
 ) }}
 
--- models/silver/streamline/core/complete/streamline__complete_transactions.sql
+++ models/silver/streamline/core/complete/streamline__complete_transactions.sql
@@ -1,6 +1,7 @@
 {{ config (
     materialized = "incremental",
     unique_key = "id",
+    cluster_by = "ROUND(block_number, -3)",
     post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION on equality(id)"
 ) }}
 

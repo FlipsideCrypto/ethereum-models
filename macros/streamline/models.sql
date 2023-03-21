27423f7dd4bc1f3270e59884ec79e93a96702d7c
austin <austin@flipsidecrypto.com>
2023-03-20 23:17:57 -0400

    format

--- macros/streamline/models.sql
+++ macros/streamline/models.sql
@@ -67,46 +67,46 @@ WHERE
         FROM
             TABLE(
                 information_schema.external_table_file_registration_history(
-                    table_name => '{{ source( "bronze_streamline", model) }}'
+                    start_time => DATEADD('day', -7, CURRENT_TIMESTAMP()),
+                    table_name => '{{ source( "bronze_streamline", model) }}')
+                ) A
+            )
+        SELECT
+            {{ unique_key }},
+            DATA,
+            _inserted_timestamp,
+            MD5(
+                CAST(
+                    COALESCE(CAST({{ unique_key }} AS text), '' :: STRING) AS text
                 )
-            ) A
-    )
-SELECT
-    {{ unique_key }},
-    DATA,
-    _inserted_timestamp,
-    MD5(
-        CAST(
-            COALESCE(CAST({{ unique_key }} AS text), '' :: STRING) AS text
-        )
-    ) AS id
-FROM
-    {{ source(
-        "bronze_streamline",
-        model
-    ) }}
-    s
-    JOIN meta b
-    ON b.file_name = metadata $ filename
-    AND b.{{ partition_name }} = s.{{ partition_name }}
-WHERE
-    b.{{ partition_name }} = s.{{ partition_name }}
-    AND (
-        DATA :error :code IS NULL
-        OR DATA :error :code NOT IN (
-            '-32000',
-            '-32001',
-            '-32002',
-            '-32003',
-            '-32004',
-            '-32005',
-            '-32006',
-            '-32007',
-            '-32008',
-            '-32009',
-            '-32010'
-        )
-    )
+            ) AS id
+        FROM
+            {{ source(
+                "bronze_streamline",
+                model
+            ) }}
+            s
+            JOIN meta b
+            ON b.file_name = metadata$filename
+            AND b.{{ partition_name }} = s.{{ partition_name }}
+        WHERE
+            b.{{ partition_name }} = s.{{ partition_name }}
+            AND (
+                DATA :error :code IS NULL
+                OR DATA :error :code NOT IN (
+                    '-32000',
+                    '-32001',
+                    '-32002',
+                    '-32003',
+                    '-32004',
+                    '-32005',
+                    '-32006',
+                    '-32007',
+                    '-32008',
+                    '-32009',
+                    '-32010'
+                )
+            )
 {% endmacro %}
 
 {% macro streamline_external_table_FR_query(
@@ -143,7 +143,7 @@ FROM
     ) }}
     s
     JOIN meta b
-    ON b.file_name = metadata $ filename
+    ON b.file_name = metadata$filename
     AND b.{{ partition_name }} = s.{{ partition_name }}
 WHERE
     b.{{ partition_name }} = s.{{ partition_name }}

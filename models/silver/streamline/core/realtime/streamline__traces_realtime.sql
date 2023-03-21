df7d2fb33112e89f2b8de08558d4d339d85eadc1
austin <austin@flipsidecrypto.com>
2023-03-21 11:17:38 -0400

    use debug trace method

--- models/silver/streamline/core/realtime/streamline__traces_realtime.sql
+++ models/silver/streamline/core/realtime/streamline__traces_realtime.sql
@@ -1,7 +1,7 @@
 {{ config (
     materialized = "view",
     post_hook = if_data_call_function(
-        func = "{{this.schema}}.udf_json_rpc(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'traces', 'route', 'trace_block', 'producer_batch_size',100, 'producer_limit_size', 100000, 'worker_batch_size',10, 'producer_batch_chunks_size', 100))",
+        func = "{{this.schema}}.udf_json_rpc(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'traces', 'route', 'debug_traceBlockByNumber', 'producer_batch_size',100, 'producer_limit_size', 100000, 'worker_batch_size',10, 'producer_batch_chunks_size', 100))",
         target = "{{this.schema}}.{{this.identifier}}"
     )
 ) }}
@@ -12,13 +12,13 @@ WITH last_3_days AS ({% if var('STREAMLINE_RUN_HISTORY') %}
         0 AS block_number
     {% else %}
     SELECT
-        MAX(block_number) - 20000 AS block_number --aprox 3 days
+        MAX(block_number) - 20000 AS block_number --approx 3 days
     FROM
         {{ ref("streamline__blocks") }}
     {% endif %})
 SELECT
     block_number,
-    'trace_block' AS method,
+    'debug_traceBlockByNumber' AS method,
     block_number_hex AS params
 FROM
     {{ ref("streamline__blocks") }}
@@ -35,7 +35,7 @@ WHERE
 EXCEPT
 SELECT
     block_number,
-    'trace_block' AS method,
+    'debug_traceBlockByNumber' AS method,
     REPLACE(
         concat_ws('', '0x', to_char(block_number, 'XXXXXXXX')),
         ' ',

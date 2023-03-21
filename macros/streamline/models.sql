9ca7ebe3be430a73223fdea67242ea4af20bcc99
austin <austin@flipsidecrypto.com>
2023-03-20 23:05:37 -0400

    update macros

--- macros/streamline/models.sql
+++ macros/streamline/models.sql
@@ -55,19 +55,18 @@ WHERE
 
 {% macro streamline_external_table_query(
         model,
-        view,
         partition_function,
         partition_name,
         unique_key
     ) %}
     WITH meta AS (
         SELECT
-            last_modified AS _inserted_timestamp,
+            job_created_time AS _inserted_timestamp,
             file_name,
             {{ partition_function }} AS {{ partition_name }}
         FROM
             TABLE(
-                information_schema.{{ view }}(
+                information_schema.external_table_file_registration_history(
                     table_name => '{{ source( "bronze_streamline", model) }}'
                 )
             ) A
@@ -88,7 +87,63 @@ FROM
     ) }}
     s
     JOIN meta b
-    ON b.file_name = metadata$filename
+    ON b.file_name = metadata $ filename
+    AND b.{{ partition_name }} = s.{{ partition_name }}
+WHERE
+    b.{{ partition_name }} = s.{{ partition_name }}
+    AND (
+        DATA :error :code IS NULL
+        OR DATA :error :code NOT IN (
+            '-32000',
+            '-32001',
+            '-32002',
+            '-32003',
+            '-32004',
+            '-32005',
+            '-32006',
+            '-32007',
+            '-32008',
+            '-32009',
+            '-32010'
+        )
+    )
+{% endmacro %}
+
+{% macro streamline_external_table_FR_query(
+        model,
+        partition_function,
+        partition_name,
+        unique_key
+    ) %}
+    WITH meta AS (
+        SELECT
+            registered_on AS _inserted_timestamp,
+            file_name,
+            {{ partition_function }} AS {{ partition_name }}
+        FROM
+            TABLE(
+                information_schema.external_table_files(
+                    table_name => '{{ source( "bronze_streamline", model) }}'
+                )
+            ) A
+    )
+SELECT
+    {{ unique_key }},
+    DATA,
+    _inserted_timestamp,
+    MD5(
+        CAST(
+            COALESCE(CAST({{ unique_key }} AS text), '' :: STRING) AS text
+        )
+    ) AS id
+FROM
+    {{ source(
+        "bronze_streamline",
+        model
+    ) }}
+    s
+    JOIN meta b
+    ON b.file_name = metadata $ filename
     AND b.{{ partition_name }} = s.{{ partition_name }}
 WHERE
     b.{{ partition_name }} = s.{{ partition_name }}
--- models/bronze/core/bronze__streamline_FR_transactions.sql
+++ models/bronze/core/bronze__streamline_FR_transactions.sql
@@ -3,9 +3,8 @@
 ) }}
 
 {% set model = this.identifier.split("_") [-1] %}
-{{ streamline_external_table_query(
+{{ streamline_external_table_FR_query(
     model,
-    view = "external_table_files",
     partition_function = "CAST(SPLIT_PART(SPLIT_PART(file_name, '/', 4), '_', 1) AS INTEGER )",
     partition_name = "_partition_by_block_id",
     unique_key = "block_number"

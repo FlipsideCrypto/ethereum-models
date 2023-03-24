2447da6f8e6a5079a8c39386234ecddebdfce520
Austin <93135983+austinFlipside@users.noreply.github.com>
2023-03-24 14:11:45 -0400

    streamline chainwalking (#452)
    * optimize
    
    * clean
    
    * configs
    
    * typo
    
    * add partition to complete
    
    * schema change
    
    * schema
    
    * incremental
    
    * traces and vars
    
    * macro
    
    * remove partitions
    
    * stash
    
    * update macros
    
    * format
    
    * add ref
    
    * partition
    
    * remove dupe ID calc
    
    * full backfill
    
    * backfill warehouse
    
    * use debug trace method
    
    * add call tracer
    
    * method updates
    
    * add explode
    
    * update parse
    
    * Add on receipts exploded keys an 2926 (#456)
    
    updated
    
    * remove modeled data
    
    ---------
    
    Co-authored-by: xiuy001 <97179309+xiuy001@users.noreply.github.com>

--- models/silver/streamline/core/realtime/streamline__traces_realtime.sql
+++ models/silver/streamline/core/realtime/streamline__traces_realtime.sql
@@ -1,7 +1,7 @@
 {{ config (
     materialized = "view",
     post_hook = if_data_call_function(
-        func = "{{this.schema}}.udf_json_rpc(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'traces', 'route', 'trace_block', 'producer_batch_size',100, 'producer_limit_size', 100000, 'worker_batch_size',10, 'producer_batch_chunks_size', 100))",
+        func = "{{this.schema}}.udf_json_rpc(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'traces', 'exploded_key','[\"result\"]', 'route', 'debug_traceBlockByNumber', 'producer_batch_size',100, 'producer_limit_size', 100000, 'worker_batch_size',10, 'producer_batch_chunks_size', 100))",
         target = "{{this.schema}}.{{this.identifier}}"
     )
 ) }}
@@ -12,14 +12,18 @@ WITH last_3_days AS ({% if var('STREAMLINE_RUN_HISTORY') %}
         0 AS block_number
     {% else %}
     SELECT
-        MAX(block_number) - 20000 AS block_number --aprox 3 days
+        MAX(block_number) - 20000 AS block_number --approx 3 days
     FROM
         {{ ref("streamline__blocks") }}
     {% endif %})
 SELECT
     block_number,
-    'trace_block' AS method,
-    block_number_hex AS params
+    'debug_traceBlockByNumber' AS method,
+    CONCAT(
+        block_number_hex,
+        '_-_',
+        '{"tracer": "callTracer"}'
+    ) AS params
 FROM
     {{ ref("streamline__blocks") }}
 WHERE
@@ -35,11 +39,15 @@ WHERE
 EXCEPT
 SELECT
     block_number,
-    'trace_block' AS method,
-    REPLACE(
-        concat_ws('', '0x', to_char(block_number, 'XXXXXXXX')),
-        ' ',
-        ''
+    'debug_traceBlockByNumber' AS method,
+    CONCAT(
+        REPLACE(
+            concat_ws('', '0x', to_char(block_number, 'XXXXXXXX')),
+            ' ',
+            ''
+        ),
+        '_-_',
+        '{"tracer": "callTracer"}'
     ) AS params
 FROM
     {{ ref("streamline__complete_traces") }}

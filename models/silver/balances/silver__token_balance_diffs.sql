bb553d70fc2b2e5f4eb41b0dc359c8e8d62d1ebf
Austin <93135983+austinFlipside@users.noreply.github.com>
2023-04-04 11:50:45 -0400

    remove filter (#461)
    * remove filter
    
    * update config
    
    * add back filter

--- models/silver/balances/silver__token_balance_diffs.sql
+++ models/silver/balances/silver__token_balance_diffs.sql
@@ -3,7 +3,6 @@
     unique_key = 'id',
     cluster_by = ['block_timestamp::date'],
     tags = ['balances','diffs'],
-    merge_update_columns = ["id"],
     post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION"
 ) }}
 
@@ -138,4 +137,4 @@ SELECT
 FROM
     FINAL
 WHERE
-    prev_bal_unadj <> current_bal_unadj
+    current_bal_unadj <> prev_bal_unadj

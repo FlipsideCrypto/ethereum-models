7db02675db391a9b5916b541ffcddd12260aec5b
xiuy001 <xiuyang@flipsidecrypto.com>
2023-04-04 17:22:57 -0400

    updated

--- models/silver/streamline/beacon/streamline__beacon_validators_realtime.sql
+++ models/silver/streamline/beacon/streamline__beacon_validators_realtime.sql
@@ -1,7 +1,7 @@
 {{ config (
     materialized = "view",
     post_hook = if_data_call_function(
-        func = "{{this.schema}}.udf_rest_api(object_construct('sql_source','{{this.identifier}}', 'external_table','beacon_validators', 'route','validators', 'producer_batch_size', 200,'producer_limit_size', 100000, 'worker_batch_size', 20, 'producer_batch_chunks_size', 2))",
+        func = "{{this.schema}}.udf_rest_api(object_construct('sql_source','{{this.identifier}}', 'external_table','beacon_validators', 'route','validators', 'producer_batch_size', 1,'producer_limit_size', 100000, 'worker_batch_size', 1, 'producer_batch_chunks_size', 1))",
         target = "{{this.schema}}.{{this.identifier}}"
     )
 ) }}
@@ -11,9 +11,11 @@ SELECT
     state_id
 FROM
     {{ ref("_max_beacon_block_by_date") }}
+WHERE slot_number >= 6123590 AND slot_number <= 6130797
 EXCEPT
 SELECT
     slot_number,
     state_id
 FROM
     {{ ref("streamline__complete_beacon_validators") }}
+WHERE slot_number < 5900000

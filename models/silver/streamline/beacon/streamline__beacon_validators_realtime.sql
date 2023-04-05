711e597cf3544397f129c92f23054d1321be301b
xiuy001 <xiuyang@flipsidecrypto.com>
2023-04-04 22:16:27 -0400

    updated

--- models/silver/streamline/beacon/streamline__beacon_validators_realtime.sql
+++ models/silver/streamline/beacon/streamline__beacon_validators_realtime.sql
@@ -1,7 +1,7 @@
 {{ config (
     materialized = "view",
     post_hook = if_data_call_function(
-        func = "{{this.schema}}.udf_rest_api(object_construct('sql_source','{{this.identifier}}', 'external_table','beacon_validators', 'route','validators', 'producer_batch_size', 1,'producer_limit_size', 100000, 'worker_batch_size', 1, 'producer_batch_chunks_size', 1))",
+        func = "{{this.schema}}.udf_rest_api(object_construct('sql_source','{{this.identifier}}', 'external_table','beacon_validators', 'route','validators', 'producer_batch_size', 10,'producer_limit_size', 100000, 'worker_batch_size', 1, 'producer_batch_chunks_size', 1))",
         target = "{{this.schema}}.{{this.identifier}}"
     )
 ) }}
@@ -11,9 +11,3 @@ SELECT
     state_id
 FROM
     {{ ref("_max_beacon_block_by_date") }}
-EXCEPT
-SELECT
-    slot_number,
-    state_id
-FROM
-    {{ ref("streamline__complete_beacon_validators") }}

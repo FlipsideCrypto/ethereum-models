2f606ea4ea275dfce675c50fd1f3c4f21b1537f3
drethereum <trevor.wenokur@gmail.com>
2023-05-08 10:11:42 -0600

    silver prices column names

--- models/silver/prices/silver__prices.sql
+++ models/silver/prices/silver__prices.sql
@@ -10,10 +10,10 @@ SELECT
     p.is_imputed,
     p._inserted_timestamp,
     COALESCE(
-        C.token_symbol,
+        C.symbol,
         m.symbol
     ) AS symbol,
-    C.token_decimals AS decimals
+    C.decimals AS decimals
 FROM
     {{ ref('silver__hourly_prices') }}
     p
@@ -21,7 +21,7 @@ FROM
     m
     ON p.token_address = m.token_address
     LEFT JOIN {{ ref('silver__contracts') }} C
-    ON p.token_address = C.contract_address
+    ON p.token_address = C.address
 WHERE
     1 = 1
 

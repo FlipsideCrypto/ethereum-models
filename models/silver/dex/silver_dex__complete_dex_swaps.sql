ceca04c6b1abadb78fa64599ff3de6c1e747c32d
drethereum <trevor.wenokur@gmail.com>
2023-05-11 13:02:06 -0600

    null price logic

--- models/silver/dex/silver_dex__complete_dex_swaps.sql
+++ models/silver/dex/silver_dex__complete_dex_swaps.sql
@@ -1680,14 +1680,16 @@ SELECT
   amount_in_unadj,
   amount_in,
   CASE
-    WHEN ABS((amount_in_usd - amount_out_usd) / NULLIF(amount_out_usd, 0)) > 0.75
+    WHEN amount_out_usd IS NULL
+    OR ABS((amount_in_usd - amount_out_usd) / NULLIF(amount_out_usd, 0)) > 0.75
     OR ABS((amount_in_usd - amount_out_usd) / NULLIF(amount_in_usd, 0)) > 0.75 THEN NULL
     ELSE amount_in_usd
   END AS amount_in_usd,
   amount_out_unadj,
   amount_out,
   CASE
-    WHEN ABS((amount_out_usd - amount_in_usd) / NULLIF(amount_in_usd, 0)) > 0.75
+    WHEN amount_in_usd IS NULL
+    OR ABS((amount_out_usd - amount_in_usd) / NULLIF(amount_in_usd, 0)) > 0.75
     OR ABS((amount_out_usd - amount_in_usd) / NULLIF(amount_out_usd, 0)) > 0.75 THEN NULL
     ELSE amount_out_usd
   END AS amount_out_usd,

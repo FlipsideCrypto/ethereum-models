2b9a6180813d05989e3be2e2a895c22c2cb17a5a
Austin <austin@flipsidecrypto.com>
2023-05-16 13:32:56 -0400

    remodel

--- macros/streamline/models.sql
+++ macros/streamline/models.sql
@@ -82,10 +82,10 @@ SELECT
 FROM
     {{ ref("silver__logs") }}
     l
-    INNER JOIN {{ ref("silver__event_abis") }}
+    INNER JOIN {{ ref("silver__complete_event_abis") }}
     a
     ON a.parent_contract_address = l.contract_address
-    and a.event_sig::string = l.topics[0]::string
+    and a.event_signature = l.topics[0]::string
     and l.block_number between a.start_block and a.end_block
 WHERE
     (

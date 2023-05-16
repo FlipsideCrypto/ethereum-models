33360c897068c0e31aaf59067eb446ba79c075c9
drethereum <trevor.wenokur@gmail.com>
2023-05-16 08:59:39 -0600

    removed prehook

--- models/gold/secure_views/core__sv_dim_dex_liquidity_pools.sql
+++ models/gold/secure_views/core__sv_dim_dex_liquidity_pools.sql
@@ -1,11 +1,10 @@
 {{ config(
     materialized = 'view',
     secure = true,
-    pre_hook = "call silver.sp_create_cross_db_share_clones()",
     post_hook = "{{ grant_data_share_statement('SV_DIM_DEX_LIQUIDITY_POOLS', 'VIEW') }}"
 ) }}
 
 SELECT
     *
 FROM
-    {{ source('ethereum_share','dex_liquidity_pools')}}
+    {{ ref('silver__dex_liquidity_pools') }}
--- models/gold/secure_views/core__sv_fact_hourly_token_prices.sql
+++ models/gold/secure_views/core__sv_fact_hourly_token_prices.sql
@@ -1,11 +1,10 @@
 {{ config(
     materialized = 'view',
     secure = true,
-    pre_hook = "call silver.sp_create_cross_db_share_clones()",
     post_hook = "{{ grant_data_share_statement('SV_FACT_HOURLY_TOKEN_PRICES', 'VIEW') }}"
 ) }}
 
 SELECT
     *
 FROM
-    {{ source('ethereum_share','token_prices_hourly')}}
+    {{ ref('silver__prices') }}

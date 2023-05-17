13b6517b9f0958e395a813dc8f98d123bd46582d
Austin <93135983+austinFlipside@users.noreply.github.com>
2023-05-17 15:30:53 -0400

    add file back (#547)

---
+++ models/silver/abis/silver__proxies2.sql
@@ -0,0 +1,38 @@
+{{ config (
+    materialized = "table"
+) }}
+
+WITH base AS (
+
+    SELECT
+        from_address,
+        to_address,
+        MIN(block_number) AS start_block
+    FROM
+        {{ ref('silver__traces') }}
+    WHERE
+        TYPE = 'DELEGATECALL'
+        AND tx_status = 'SUCCESS'
+        AND from_address != to_address -- exclude self-calls
+        AND output :: STRING is null -- exclude calls with output
+    GROUP BY
+        from_address,
+        to_address
+)
+SELECT
+    from_address AS contract_address,
+    to_address AS proxy_address,
+    start_block,
+    COALESCE(
+        (LAG(start_block) over(PARTITION BY from_address
+        ORDER BY
+            start_block DESC)) - 1,
+            10000000000
+    ) AS end_block,
+    CONCAT(
+        from_address,
+        '-',
+        to_address
+    ) AS _id
+FROM
+    base

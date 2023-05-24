762f4c00add11aa3625d52f9732447693d9cfbfe
Julius Remigio <14811322+juls858@users.noreply.github.com>
2023-05-24 14:39:37 -0700

    - create udfs from fsc_utils package

--- macros/create_udfs.sql
+++ macros/create_udfs.sql
@@ -49,5 +49,9 @@
             {% endset %}
             {% do run_query(sql) %}
         {% endif %}
+        {% set name %}
+        {{- fsc_utils.create_udfs() -}}
+        {% endset %}
+        {% do run_query(sql) %}
     {% endif %}
 {% endmacro %}

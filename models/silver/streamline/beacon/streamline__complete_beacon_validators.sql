ca3d7d2dcf40d2585b99677107f3671256f1debc
drethereum <71602799+drethereum@users.noreply.github.com>
2023-05-25 10:26:41 -0600

    fix/complete-val-id (#563)
    * changed id naming
    
    * removed SO

--- models/silver/streamline/beacon/streamline__complete_beacon_validators.sql
+++ models/silver/streamline/beacon/streamline__complete_beacon_validators.sql
@@ -1,17 +1,16 @@
 -- depends on: {{ ref('bronze__beacon_validators') }}
 {{ config (
     materialized = "incremental",
-    unique_key = "id",
+    unique_key = "_id",
     cluster_by = "ROUND(slot_number, -3)",
-    merge_update_columns = ["id"],
-    post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION on equality(id)",
+    merge_update_columns = ["_id"],
     incremental_predicates = ["dynamic_range", "slot_number"]
 ) }}
 
 SELECT
     MD5(
         CAST(COALESCE(CAST(block_number AS text), '') AS text)
-    ) AS id,
+    ) AS _id,
     block_number AS slot_number,
     state_id,
     _inserted_timestamp
@@ -30,6 +29,6 @@ WHERE
     {{ ref('bronze__fr_beacon_validators') }}
 {% endif %}
 
-qualify(ROW_NUMBER() over (PARTITION BY id
+qualify(ROW_NUMBER() over (PARTITION BY _id
 ORDER BY
     _inserted_timestamp DESC)) = 1

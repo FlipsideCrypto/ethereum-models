c08d1585c2eaf098afd83618cd49491e10d9e702
drethereum <71602799+drethereum@users.noreply.github.com>
2023-05-30 10:57:06 -0600

    AN-3336/snapshot-vote-cadence (#566)
    increased votes limit

--- models/bronze/api_udf/snapshot/bronze_api__snapshot_votes.sql
+++ models/bronze/api_udf/snapshot/bronze_api__snapshot_votes.sql
@@ -54,7 +54,7 @@ WITH RECURSIVE votes_request AS (
             {% endif %}
             ) max_time
         ) ON 1=1
-    WHERE r.total_retrieved <= 4000
+    WHERE r.total_retrieved <= 5000
 ),
 
 votes_final AS (

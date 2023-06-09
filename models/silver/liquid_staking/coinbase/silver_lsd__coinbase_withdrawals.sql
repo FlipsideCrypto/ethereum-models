53239179266ffcfd603e3cc82f5d5d0e8599d48b
drethereum <trevor.wenokur@gmail.com>
2023-06-09 15:03:16 -0600

    coinbase burns

---
+++ models/silver/liquid_staking/coinbase/silver_lsd__coinbase_withdrawals.sql
@@ -0,0 +1,59 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = '_log_id',
+    cluster_by = ['block_timestamp::DATE']
+) }}
+
+WITH burns AS (
+
+    SELECT
+        block_number,
+        block_timestamp,
+        origin_function_signature,
+        origin_from_address,
+        origin_to_address,
+        tx_hash,
+        event_index,
+        contract_address,
+        CONCAT('0x', SUBSTR(topics [1] :: STRING, 27, 40)) AS burner,
+        regexp_substr_all(SUBSTR(DATA, 3, len(DATA)), '.{64}') AS segmented_data,
+        TRY_TO_NUMBER(
+            utils.udf_hex_to_int(
+                segmented_data [0] :: STRING
+            )
+        ) AS amount,
+        (amount / pow(10, 18)) :: FLOAT AS amount_adj,
+        _log_id,
+        _inserted_timestamp
+    FROM
+        {{ ref('silver__logs') }}
+    WHERE
+        topics [0] :: STRING = '0xcc16f5dbb4873280815c1ee09dbd06736cffcc184412cf7a71a0fdb75d397ca5' --Burn
+        AND contract_address = '0xbe9895146f7af43049ca1c1ae358b0541ea49704' --Coinbase Wrapped Staked ETH (cbETH)
+
+{% if is_incremental() %}
+AND _inserted_timestamp >= (
+    SELECT
+        MAX(_inserted_timestamp) :: DATE
+    FROM
+        {{ this }}
+)
+{% endif %}
+)
+SELECT
+    block_number,
+    block_timestamp,
+    origin_function_signature,
+    origin_from_address,
+    origin_to_address,
+    tx_hash,
+    event_index,
+    contract_address,
+    burner AS sender,
+    burner AS recipient,
+    amount AS token_amount,
+    amount_adj AS token_amount_adj,
+    _log_id,
+    _inserted_timestamp
+FROM
+    burns

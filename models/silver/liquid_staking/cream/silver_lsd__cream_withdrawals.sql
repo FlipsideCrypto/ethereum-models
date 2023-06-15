48d0c8383d73d0b21f0cf5621f9a8185c7332dd1
drethereum <trevor.wenokur@gmail.com>
2023-06-15 15:43:06 -0600

    cream withdrawals

---
+++ models/silver/liquid_staking/cream/silver_lsd__cream_withdrawals.sql
@@ -0,0 +1,74 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = '_log_id',
+    cluster_by = ['block_timestamp::DATE']
+) }}
+
+WITH withdrawals AS (
+
+    SELECT
+        block_number,
+        block_timestamp,
+        origin_function_signature,
+        origin_from_address,
+        origin_to_address,
+        tx_hash,
+        event_index,
+        contract_address,
+        regexp_substr_all(SUBSTR(DATA, 3, len(DATA)), '.{64}') AS segmented_data,
+        CONCAT(
+            '0x',
+            SUBSTR(
+                segmented_data [0] :: STRING,
+                25,
+                40
+            )
+        ) AS account,
+        TRY_TO_NUMBER(
+            utils.udf_hex_to_int(
+                segmented_data [1] :: STRING
+            )
+        ) AS eth_amount,
+        (eth_amount / pow(10, 18)) :: FLOAT AS eth_amount_adj,
+        TRY_TO_NUMBER(
+            utils.udf_hex_to_int(
+                segmented_data [1] :: STRING
+            )
+        ) AS creth2_amount,
+        (creth2_amount / pow(10, 18)) :: FLOAT AS creth2_amount_adj,
+        _log_id,
+        _inserted_timestamp
+    FROM
+        {{ ref('silver__logs') }}
+    WHERE
+        topics [0] :: STRING = '0x5bb95829671915ece371da722f91d5371159095dcabf2f75cd6c53facb7e1bab' --WithdrawEvent
+        AND contract_address = '0xcbc1065255cbc3ab41a6868c22d1f1c573ab89fd' --Cream ETH 2 (CRETH2)
+
+{% if is_incremental() %}
+AND _inserted_timestamp >= (
+    SELECT
+        MAX(_inserted_timestamp) :: DATE
+    FROM
+        {{ this }}
+)
+{% endif %}
+)
+SELECT
+    block_number,
+    block_timestamp,
+    origin_function_signature,
+    origin_from_address,
+    origin_to_address,
+    tx_hash,
+    event_index,
+    contract_address,
+    account AS sender,
+    account AS recipient,
+    eth_amount,
+    eth_amount_adj,
+    creth2_amount AS token_amount,
+    creth2_amount_adj AS token_amount_adj,
+    _log_id,
+    _inserted_timestamp
+FROM
+    withdrawals

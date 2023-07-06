b01968ea8f0633fa5d1fe06e2ffca6204bd8bbc2
Austin <93135983+austinFlipside@users.noreply.github.com>
2023-07-06 11:25:10 -0400

    remove temp job (#599)
    * remove temp job
    
    * core tag
    
    * regular logic

--- models/silver/core/silver__confirmed_blocks.sql
+++ models/silver/core/silver__confirmed_blocks.sql
@@ -3,7 +3,8 @@
     materialized = 'incremental',
     incremental_strategy = 'delete+insert',
     unique_key = "block_number",
-    cluster_by = "round(block_number,-3)"
+    cluster_by = "round(block_number,-3)",
+    tags = ['core']
 ) }}
 
 WITH base AS (
@@ -14,7 +15,6 @@ WITH base AS (
         DATA :result :transactions txs,
         _inserted_timestamp
     FROM
-        {#
 
 {% if is_incremental() %}
 {{ ref('bronze__streamline_confirm_blocks') }}
@@ -34,38 +34,17 @@ WHERE
     {{ ref('bronze__streamline_FR_confirm_blocks') }}
 {% endif %}
 
-#}
-
-{% if is_incremental() %}
-{{ ref('bronze__streamline_FR_confirm_blocks') }}
-WHERE
-    _partition_by_block_id BETWEEN (
-        SELECT
-            ROUND(MAX(block_number), -4)
-        FROM
-            {{ this }})
-            AND (
-                SELECT
-                    ROUND(MAX(block_number), -4) + 1000000
-                FROM
-                    {{ this }})
-                {% else %}
-                    {{ ref('bronze__streamline_FR_confirm_blocks') }}
-                WHERE
-                    _partition_by_block_id <= 3000000
-                {% endif %}
-
-                qualify(ROW_NUMBER() over (PARTITION BY block_number
-                ORDER BY
-                    _inserted_timestamp DESC)) = 1
-            )
-        SELECT
-            block_number,
-            block_hash,
-            VALUE :: STRING AS tx_hash,
-            _inserted_timestamp
-        FROM
-            base,
-            LATERAL FLATTEN (
-                input => txs
-            )
+qualify(ROW_NUMBER() over (PARTITION BY block_number
+ORDER BY
+    _inserted_timestamp DESC)) = 1
+)
+SELECT
+    block_number,
+    block_hash,
+    VALUE :: STRING AS tx_hash,
+    _inserted_timestamp
+FROM
+    base,
+    LATERAL FLATTEN (
+        input => txs
+    )

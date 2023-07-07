5e2fcd41c2386464e7349eae711403bb95613283
drethereum <trevor.wenokur@gmail.com>
2023-07-07 15:10:04 -0600

    sharedstake deposits

--- models/silver/liquid_staking/sharedstake/silver_lsd__sharedstake_deposits.sql
+++ models/silver/liquid_staking/sharedstake/silver_lsd__sharedstake_deposits.sql
@@ -4,48 +4,78 @@
     cluster_by = ['block_timestamp::DATE']
 ) }}
 
-WITH deposits AS (
+WITH deposit_logs AS (
 
     SELECT
-        l.block_number,
-        l.block_timestamp,
-        l.origin_function_signature,
-        l.origin_from_address,
-        l.origin_to_address,
-        l.tx_hash,
+        block_number,
+        block_timestamp,
+        origin_function_signature,
+        origin_from_address,
+        origin_to_address,
+        tx_hash,
         event_index,
-        l.contract_address,
-        CONCAT('0x', SUBSTR(topics [1] :: STRING, 27, 40)) AS l_from_address,
+        contract_address,
+        CONCAT('0x', SUBSTR(topics [1] :: STRING, 27, 40)) AS from_address,
         CONCAT('0x', SUBSTR(topics [2] :: STRING, 27, 40)) AS to_address,
-        regexp_substr_all(SUBSTR(l.data, 3, len(l.data)), '.{64}') AS segmented_data,
+        regexp_substr_all(SUBSTR(DATA, 3, len(DATA)), '.{64}') AS segmented_data,
         TRY_TO_NUMBER(
             ethereum.public.udf_hex_to_int(
                 segmented_data [0] :: STRING
             )
         ) AS token_amount,
         (token_amount / pow(10, 18)) :: FLOAT AS token_amount_adj,
-        t.eth_value * pow(
-            10,
-            18
-        ) AS eth_amount,
-        t.eth_value AS eth_amount_adj,
         _log_id,
-        l._inserted_timestamp
+        _inserted_timestamp
     FROM
         {{ ref('silver__logs') }}
-        l
-        LEFT JOIN {{ ref('silver__traces') }}
-        t
-        ON l.tx_hash = t.tx_hash
-        AND CONCAT('0x', SUBSTR(topics [2] :: STRING, 27, 40)) = t.from_address
     WHERE
         topics [0] :: STRING = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef' --Deposit/Mint (Transfer)
-        AND l.contract_address = '0x898bad2774eb97cf6b94605677f43b41871410b1' --validator-Eth2 (vETH2)
-        AND l_from_address = '0x0000000000000000000000000000000000000000'
+        AND contract_address = '0x898bad2774eb97cf6b94605677f43b41871410b1' --validator-Eth2 (vETH2)
+        AND from_address = '0x0000000000000000000000000000000000000000'
         AND origin_to_address = '0xbca3b7b87dcb15f0efa66136bc0e4684a3e5da4d'
 
 {% if is_incremental() %}
-AND l._inserted_timestamp >= (
+AND _inserted_timestamp >= (
+    SELECT
+        MAX(_inserted_timestamp) :: DATE
+    FROM
+        {{ this }}
+)
+{% endif %}
+),
+deposit_traces AS (
+    SELECT
+        block_number,
+        block_timestamp,
+        tx_hash,
+        from_address,
+        to_address,
+        eth_value * pow(
+            10,
+            18
+        ) AS eth_amount,
+        eth_value AS eth_amount_adj,
+        _call_id,
+        _inserted_timestamp
+    FROM
+        {{ ref('silver__traces') }}
+    WHERE
+        block_timestamp :: DATE >= '2020-12-10'
+        AND tx_hash IN (
+            SELECT
+                tx_hash
+            FROM
+                deposit_logs
+        )
+        AND from_address IN (
+            SELECT
+                to_address
+            FROM
+                deposit_logs
+        )
+
+{% if is_incremental() %}
+AND _inserted_timestamp >= (
     SELECT
         MAX(_inserted_timestamp) :: DATE
     FROM
@@ -54,21 +84,24 @@ AND l._inserted_timestamp >= (
 {% endif %}
 )
 SELECT
-    block_number,
-    block_timestamp,
-    origin_function_signature,
-    origin_from_address,
-    origin_to_address,
-    tx_hash,
-    event_index,
-    contract_address,
-    l_from_address AS sender,
-    to_address AS recipient,
+    l.block_number,
+    l.block_timestamp,
+    l.origin_function_signature,
+    l.origin_from_address,
+    l.origin_to_address,
+    l.tx_hash,
+    l.event_index,
+    l.contract_address,
+    l.from_address AS sender,
+    l.to_address AS recipient,
     eth_amount,
     eth_amount_adj,
     token_amount,
     token_amount_adj,
     _log_id,
-    _inserted_timestamp
+    l._inserted_timestamp
 FROM
-    deposits
+    deposit_logs l
+    LEFT JOIN deposit_traces t
+    ON l.tx_hash = t.tx_hash
+    AND t.from_address = l.to_address

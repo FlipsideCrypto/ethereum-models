499cc6f4cd661511bbbf95439370a64592450d9f
mattromano <mattromano88@gmail.com>
2023-07-13 10:19:03 -0700

    fix function delete

--- macros/create_udfs.sql
+++ macros/create_udfs.sql
@@ -5,6 +5,9 @@
         {{ create_udf_transform_logs(
             schema = 'silver'
         ) }}
+        {{ create_udf_hex_to_int_with_inputs(
+            schema = "public"
+        ) }}
         {{ create_udtf_get_base_table(
             schema = "streamline"
         ) }}

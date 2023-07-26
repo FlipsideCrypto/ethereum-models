7a3cdc039a9fe949c4e8dfd72f11d371fac285bf
drethereum <trevor.wenokur@gmail.com>
2023-07-26 16:42:43 -0600

    balancer complete

--- models/silver/dex/balancer/silver_dex__balancer_pools.sql
+++ models/silver/dex/balancer/silver_dex__balancer_pools.sql
@@ -5,7 +5,7 @@
 
 --    full_refresh = false
 
-WITH pool_creation AS (
+WITH pools_registered AS (
 
     SELECT
         block_number,
@@ -21,17 +21,50 @@ WITH pool_creation AS (
     FROM
         {{ ref('silver__logs') }}
     WHERE
-        topics[0]::STRING = '0x3c13bc30b8e878c53fd2a36b679409c073afd75950be43d8858768e956fbc20e'
+        topics[0]::STRING = '0x3c13bc30b8e878c53fd2a36b679409c073afd75950be43d8858768e956fbc20e' --PoolRegistered
         AND contract_address = '0xba12222222228d8ba445958a75a0704d566bf2c8'
 {% if is_incremental() %}
-AND pool_address NOT IN (
-        SELECT
-            DISTINCT pool_address
-        FROM
-            {{ this }}
-    )
+AND _inserted_timestamp >= (
+    SELECT
+        MAX(_inserted_timestamp) :: DATE - 2
+    FROM
+        {{ this }}
+)
 {% endif %}
 
+),
+
+tokens_registered AS (
+
+SELECT
+    block_number,
+    block_timestamp,
+    event_index,
+    tx_hash,
+    contract_address,
+    decoded_flat :poolId :: STRING AS pool_id,
+    decoded_flat :tokens AS tokens,
+    tokens[0] :: STRING AS token0,
+    tokens[1] :: STRING AS token1,
+    tokens[2] :: STRING AS token2,
+    tokens[3] :: STRING AS token3,
+    tokens[4] :: STRING AS token4,
+    tokens[5] :: STRING AS token5,
+    tokens[6] :: STRING AS token6,
+    tokens[7] :: STRING AS token7,
+    decoded_flat :assetManagers AS asset_managers,
+    _log_id,
+    _inserted_timestamp
+FROM ethereum.silver.decoded_logs
+WHERE
+    topics[0]::STRING = '0xf5847d3f2197b16cdcd2098ec95d0905cd1abdaf415f07bb7cef2bba8ac5dec4' --TokensRegistered
+    AND contract_address = '0xba12222222228d8ba445958a75a0704d566bf2c8'
+    AND tx_hash IN (
+        SELECT
+            tx_hash
+        FROM
+            pools_registered
+    )
 ),
 
 function_sigs AS (
@@ -57,7 +90,7 @@ SELECT
     function_sig,
     (ROW_NUMBER() OVER (PARTITION BY pool_address
         ORDER BY block_number)) - 1 AS function_input
-FROM pool_creation
+FROM pools_registered
 JOIN function_sigs ON 1=1 
 ),
 
@@ -94,7 +127,7 @@ FROM (
                 ) AS read_input,
                 row_num
         FROM inputs_pools
-        LEFT JOIN pool_creation USING(pool_address) 
+        LEFT JOIN pools_registered USING(pool_address) 
             ) ready_reads_pools
     WHERE row_num BETWEEN {{ item * 50 + 1 }} AND {{ (item + 1) * 50}}
     ) batch_reads_pools
@@ -147,7 +180,9 @@ FINAL AS (
 SELECT
     pool_address,
     MIN(CASE WHEN function_name = 'symbol' THEN TRY_HEX_DECODE_STRING(segmented_output [2] :: STRING) END) AS pool_symbol,
+    MIN(CASE WHEN function_name = 'symbol' THEN utils.udf_hex_to_string(segmented_output [2] :: STRING) END) AS pool_symbol2,
     MIN(CASE WHEN function_name = 'name' THEN TRY_HEX_DECODE_STRING(segmented_output [2] :: STRING) END) AS pool_name,
+    MIN(CASE WHEN function_name = 'name' THEN utils.udf_hex_to_string(segmented_output [2] :: STRING) END) AS pool_name2,
     MIN(CASE 
             WHEN read_result::STRING = '0x' THEN NULL
             ELSE utils.udf_hex_to_int(LEFT(read_result::STRING,66))
@@ -166,11 +201,22 @@ SELECT
     p.pool_id,
     f.pool_address,
     f.pool_symbol,
+    f.pool_symbol2,
     f.pool_name,
+    f.pool_name2,
     f.pool_decimals,
+    t.token0,
+    t.token1,
+    t.token2,
+    t.token3,
+    t.token4,
+    t.token5,
+    t.token6,
+    t.token7,
+    t.asset_managers,
     p._log_id,
     f._inserted_timestamp
 FROM FINAL f
-LEFT JOIN pool_creation p ON f.pool_address = p.pool_address
-WHERE f.pool_name IS NOT NULL 
-    AND f.pool_symbol IS NOT NULL
+LEFT JOIN pools_registered p ON f.pool_address = p.pool_address
+LEFT JOIN tokens_registered t ON p.pool_id = t.pool_id
+WHERE t.token0 IS NOT NULL
--- models/silver/dex/silver_dex__complete_dex_liquidity_pools.sql
+++ models/silver/dex/silver_dex__complete_dex_liquidity_pools.sql
@@ -7,8 +7,8 @@
 WITH contracts AS ( --join core dim_contracts in gold view instead if possible
 
   SELECT
-    address :: STRING AS address,
-    symbol :: STRING AS symbol,
+    address,
+    symbol,
     decimals,
     _inserted_timestamp
   FROM
@@ -22,24 +22,31 @@ WHERE address NOT IN (
 {% endif %} #}
 ),
 
-{# balancer AS ( --custom
+balancer AS (
 
 SELECT
     block_number,
     block_timestamp,
     tx_hash,
-    contract_address,
+    deployer_address AS contract_address,
     pool_address,
     pool_name,
-    NULL AS tokens,
     'balancer' AS platform,
-    _log_id,
-    _inserted_timestamp
+    _call_id AS _id,
+    _inserted_timestamp,
+    token0,
+    token1,
+    token2,
+    token3,
+    token4,
+    token5,
+    token6,
+    token7
 FROM
     {{ ref('silver_dex__balancer_pools') }}
-),#}
+),
 
-curve AS ( --custom
+curve AS (
 
 SELECT
     block_number,
@@ -56,7 +63,9 @@ SELECT
     MAX(CASE WHEN token_num = 3 THEN token_address END) AS token2,
     MAX(CASE WHEN token_num = 4 THEN token_address END) AS token3,
     MAX(CASE WHEN token_num = 5 THEN token_address END) AS token4,
-    MAX(CASE WHEN token_num = 6 THEN token_address END) AS token5
+    MAX(CASE WHEN token_num = 6 THEN token_address END) AS token5,
+    MAX(CASE WHEN token_num = 7 THEN token_address END) AS token6,
+    MAX(CASE WHEN token_num = 8 THEN token_address END) AS token7
 FROM
     {{ ref('silver_dex__curve_pools') }}
 GROUP BY all
@@ -294,10 +303,10 @@ all_pools_v3 AS (
 
 all_pools_other AS (
     SELECT *
+    FROM balancer
+    UNION ALL
+    SELECT *
     FROM curve
-    --UNION ALL
-    --SELECT *
-    --FROM balancer
 ),
 
 FINAL AS (
@@ -365,17 +374,19 @@ FINAL AS (
             CASE WHEN token2 IS NOT NULL THEN '-' || COALESCE(c2.symbol, SUBSTRING(token2, 1, 5) || '...' || SUBSTRING(token2, 39, 42)) ELSE '' END,
             CASE WHEN token3 IS NOT NULL THEN '-' || COALESCE(c3.symbol, SUBSTRING(token3, 1, 5) || '...' || SUBSTRING(token3, 39, 42)) ELSE '' END,
             CASE WHEN token4 IS NOT NULL THEN '-' || COALESCE(c4.symbol, SUBSTRING(token4, 1, 5) || '...' || SUBSTRING(token4, 39, 42)) ELSE '' END,
-            CASE WHEN token5 IS NOT NULL THEN '-' || COALESCE(c5.symbol, SUBSTRING(token5, 1, 5) || '...' || SUBSTRING(token5, 39, 42)) ELSE '' END
+            CASE WHEN token5 IS NOT NULL THEN '-' || COALESCE(c5.symbol, SUBSTRING(token5, 1, 5) || '...' || SUBSTRING(token5, 39, 42)) ELSE '' END,
+            CASE WHEN token6 IS NOT NULL THEN '-' || COALESCE(c6.symbol, SUBSTRING(token6, 1, 5) || '...' || SUBSTRING(token6, 39, 42)) ELSE '' END,
+            CASE WHEN token7 IS NOT NULL THEN '-' || COALESCE(c7.symbol, SUBSTRING(token7, 1, 5) || '...' || SUBSTRING(token7, 39, 42)) ELSE '' END
         ) AS pool_name,
-        OBJECT_CONSTRUCT('token0', token0, 'token1', token1, 'token2', token2, 'token3', token3, 'token4', token4, 'token5', token5) AS tokens,
-        OBJECT_CONSTRUCT('token0', c0.symbol, 'token1', c1.symbol, 'token2', c2.symbol, 'token3', c3.symbol, 'token4', c4.symbol, 'token5', c5.symbol) AS symbols,
-        OBJECT_CONSTRUCT('token0', c0.decimals, 'token1', c1.decimals, 'token2', c2.decimals, 'token3', c3.decimals, 'token4', c4.decimals, 'token5', c5.decimals) AS decimals,
+        OBJECT_CONSTRUCT('token0', token0, 'token1', token1, 'token2', token2, 'token3', token3, 'token4', token4, 'token5', token5, 'token6', token6, 'token7', token7) AS tokens,
+        OBJECT_CONSTRUCT('token0', c0.symbol, 'token1', c1.symbol, 'token2', c2.symbol, 'token3', c3.symbol, 'token4', c4.symbol, 'token5', c5.symbol, 'token6', c6.symbol, 'token7', c7.symbol) AS symbols,
+        OBJECT_CONSTRUCT('token0', c0.decimals, 'token1', c1.decimals, 'token2', c2.decimals, 'token3', c3.decimals, 'token4', c4.decimals, 'token5', c5.decimals, 'token6', c6.decimals, 'token7', c7.decimals) AS decimals,
         platform,
         _id,
         p._inserted_timestamp
     FROM all_pools_other p
     LEFT JOIN contracts c0
-    ON c0.address = p.token0
+        ON c0.address = p.token0
     LEFT JOIN contracts c1
         ON c1.address = p.token1
     LEFT JOIN contracts c2
@@ -386,6 +397,10 @@ FINAL AS (
         ON c4.address = p.token4
     LEFT JOIN contracts c5
         ON c5.address = p.token5
+    LEFT JOIN contracts c6
+        ON c6.address = p.token6
+    LEFT JOIN contracts c7
+        ON c7.address = p.token7
 )
 
 SELECT

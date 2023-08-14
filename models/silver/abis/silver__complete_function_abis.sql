96873a427238fe9449e8295796209fdca6b40e67
Austin <93135983+austinFlipside@users.noreply.github.com>
2023-08-14 17:25:36 -0400

    proxies (#639)

--- models/silver/abis/silver__complete_function_abis.sql
+++ models/silver/abis/silver__complete_function_abis.sql
@@ -65,10 +65,96 @@ flat_outputs AS (
         outputs,
         NAME
 ),
-apply_udfs AS (
+proxy_base AS (
+    SELECT
+        C.created_contract_address AS contract_address,
+        p.proxy_address,
+        p.start_block,
+        C.block_number AS created_block
+    FROM
+        {{ ref('silver__created_contracts') }} C
+        INNER JOIN {{ ref('silver__proxies') }}
+        p
+        ON C.created_contract_address = p.contract_address
+        AND p.proxy_address <> '0x0000000000000000000000000000000000000000'
+),
+all_contracts AS (
     SELECT
         contract_address,
         NAME AS function_name,
+        inputs,
+        outputs,
+        inputs_type,
+        outputs_type
+    FROM
+        flat_inputs
+        LEFT JOIN flat_outputs USING (
+            contract_address,
+            NAME
+        )
+),
+stacked AS (
+    SELECT
+        ea.contract_address,
+        ea.inputs,
+        ea.outputs,
+        ea.function_name,
+        ea.inputs_type,
+        ea.outputs_type,
+        pb.start_block,
+        pb.contract_address AS base_contract_address,
+        1 AS priority
+    FROM
+        all_contracts ea
+        INNER JOIN proxy_base pb
+        ON ea.contract_address = pb.proxy_address
+    UNION ALL
+    SELECT
+        eab.contract_address,
+        eab.inputs,
+        eab.outputs,
+        eab.function_name,
+        eab.inputs_type,
+        eab.outputs_type,
+        pbb.created_block AS start_block,
+        pbb.contract_address AS base_contract_address,
+        2 AS priority
+    FROM
+        all_contracts eab
+        INNER JOIN (
+            SELECT
+                DISTINCT contract_address,
+                created_block
+            FROM
+                proxy_base
+        ) pbb
+        ON eab.contract_address = pbb.contract_address
+    UNION ALL
+    SELECT
+        eac.contract_address,
+        eac.inputs,
+        eac.outputs,
+        eac.function_name,
+        eac.inputs_type,
+        eac.outputs_type,
+        0 AS start_block,
+        eac.contract_address AS base_contract_address,
+        3 AS priority
+    FROM
+        all_contracts eac
+    WHERE
+        contract_address NOT IN (
+            SELECT
+                DISTINCT contract_address
+            FROM
+                proxy_base
+        )
+),
+apply_udfs AS (
+    SELECT
+        contract_address AS source_contract_address,
+        base_contract_address AS parent_contract_address,
+        function_name,
         PARSE_JSON(
             OBJECT_CONSTRUCT(
                 'inputs',
@@ -76,44 +162,57 @@ apply_udfs AS (
                 'outputs',
                 outputs,
                 'name',
-                NAME,
+                function_name,
                 'type',
                 'function'
             ) :: STRING
         ) AS abi,
+        start_block,
         utils.udf_evm_text_signature(abi) AS simple_function_name,
         utils.udf_keccak256(simple_function_name) AS function_signature,
-        NAME,
+        priority,
         inputs,
         outputs,
         inputs_type,
         outputs_type
     FROM
-        flat_inputs
-        LEFT JOIN flat_outputs USING(
-            contract_address,
-            NAME
-        )
+        stacked
+),
+FINAL AS (
+    SELECT
+        parent_contract_address,
+        function_name,
+        abi,
+        start_block,
+        simple_function_name,
+        function_signature,
+        inputs,
+        outputs,
+        inputs_type,
+        outputs_type
+    FROM
+        apply_udfs qualify ROW_NUMBER() over (
+            PARTITION BY parent_contract_address,
+            function_name,
+            inputs_type,
+            start_block
+            ORDER BY
+                priority ASC
+        ) = 1
 )
 SELECT
-    contract_address,
+    parent_contract_address,
     function_name,
     abi,
+    start_block,
     simple_function_name,
     function_signature,
     LEFT(
         function_signature,
         10
     ) AS function_signature_prefix,
-    NAME,
-    inputs,
-    outputs,
-    inputs_type,
-    outputs_type
+    IFNULL(LEAD(start_block) over (PARTITION BY parent_contract_address, function_signature
+ORDER BY
+    start_block) -1, 1e18) AS end_block
 FROM
-    apply_udfs qualify ROW_NUMBER() over (
-        PARTITION BY contract_address,
-        NAME
-        ORDER BY
-            inputs_type DESC
-    ) = 1
+    FINAL

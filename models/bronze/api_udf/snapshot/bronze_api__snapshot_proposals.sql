b5c1fedbb2660ebd0c2cb10cd1c89cd94daed7ec
drethereum <trevor.wenokur@gmail.com>
2023-08-28 10:18:50 -0600

    api key

--- models/bronze/api_udf/snapshot/bronze_api__snapshot_proposals.sql
+++ models/bronze/api_udf/snapshot/bronze_api__snapshot_proposals.sql
@@ -6,17 +6,17 @@
 ) }}
 
 WITH max_time AS (
-    {% if is_incremental() %}
-    SELECT
-        MAX(created_at) AS max_prop_created
-    FROM
-        {{ this }}
-    {% else %}
-    SELECT
-        0 AS max_prop_created
-    {% endif %}
-),
 
+{% if is_incremental() %}
+
+SELECT
+    MAX(created_at) AS max_prop_created
+FROM
+    {{ this }}
+{% else %}
+SELECT
+    0 AS max_prop_created
+{% endif %}),
 ready_prop_requests AS (
     SELECT
         CONCAT(
@@ -24,60 +24,78 @@ ready_prop_requests AS (
             max_time_created,
             '}) { id space{id} ipfs author created network type title body start end state votes choices scores_state scores } }'
         ) AS proposal_request_created
-    FROM (
-        SELECT 
-            DATE_PART(epoch_second, max_prop_created::TIMESTAMP) AS max_time_created
-        FROM 
-            max_time
-    )
+    FROM
+        (
+            SELECT
+                DATE_PART(
+                    epoch_second,
+                    max_prop_created :: TIMESTAMP
+                ) AS max_time_created
+            FROM
+                max_time
+        )
 ),
-
 proposal_data_created AS (
     SELECT
         ethereum.streamline.udf_api(
             'GET',
-            'https://hub.snapshot.org/graphql',{},{ 'query': proposal_request_created }
+            'https://hub.snapshot.org/graphql',{ 'apiKey':(
+                SELECT
+                    api_key
+                FROM
+                    {{ source(
+                        'crosschain_silver',
+                        'apis_keys'
+                    ) }}
+                WHERE
+                    api_name = 'snapshot'
+            ) },{ 'query': proposal_request_created }
         ) AS resp,
         SYSDATE() AS _inserted_timestamp
     FROM
         ready_prop_requests
 ),
-
 proposals_final AS (
-    
-SELECT
-    VALUE :id :: STRING AS proposal_id, 
-    VALUE :ipfs :: STRING AS ipfs, 
-    STRTOK_TO_ARRAY(VALUE :choices, ';') AS choices, 
-    VALUE :author :: STRING AS proposal_author, 
-    VALUE :title :: STRING AS proposal_title, 
-    VALUE :body :: STRING AS proposal_text, 
-    VALUE :space :id :: STRING AS space_id, 
-    VALUE :network :: STRING AS network,
-    TO_TIMESTAMP_NTZ(VALUE :created) AS created_at, 
-    TO_TIMESTAMP_NTZ(VALUE :start) AS proposal_start_time, 
-    TO_TIMESTAMP_NTZ(VALUE :end) AS proposal_end_time,
-    VALUE,
-    _inserted_timestamp
-FROM
-    proposal_data_created,
-LATERAL FLATTEN(
-    input => resp :data :data :proposals
+    SELECT
+        VALUE :id :: STRING AS proposal_id,
+        VALUE :ipfs :: STRING AS ipfs,
+        STRTOK_TO_ARRAY(
+            VALUE :choices,
+            ';'
+        ) AS choices,
+        VALUE :author :: STRING AS proposal_author,
+        VALUE :title :: STRING AS proposal_title,
+        VALUE :body :: STRING AS proposal_text,
+        VALUE :space :id :: STRING AS space_id,
+        VALUE :network :: STRING AS network,
+        TO_TIMESTAMP_NTZ(
+            VALUE :created
+        ) AS created_at,
+        TO_TIMESTAMP_NTZ(
+            VALUE :start
+        ) AS proposal_start_time,
+        TO_TIMESTAMP_NTZ(
+            VALUE :end
+        ) AS proposal_end_time,
+        VALUE,
+        _inserted_timestamp
+    FROM
+        proposal_data_created,
+        LATERAL FLATTEN(
+            input => resp :data :data :proposals
+        )
+    WHERE
+        choices IS NOT NULL
+        AND proposal_author IS NOT NULL
+        AND proposal_title IS NOT NULL
+        AND proposal_text IS NOT NULL
+        AND proposal_start_time IS NOT NULL
+        AND proposal_end_time IS NOT NULL
+        AND space_id IS NOT NULL
+        AND network IS NOT NULL qualify(ROW_NUMBER() over (PARTITION BY proposal_id
+    ORDER BY
+        TO_TIMESTAMP_NTZ(VALUE :created) DESC)) = 1
 )
-WHERE 
-    choices IS NOT NULL
-    AND proposal_author IS NOT NULL
-    AND proposal_title IS NOT NULL 
-    AND proposal_text IS NOT NULL
-    AND proposal_start_time IS NOT NULL
-    AND proposal_end_time IS NOT NULL 
-    AND space_id IS NOT NULL
-    AND network IS NOT NULL
-QUALIFY(ROW_NUMBER() over (PARTITION BY proposal_id
-  ORDER BY
-    TO_TIMESTAMP_NTZ(VALUE :created) DESC)) = 1
-)
-
 SELECT
     proposal_id,
     ipfs,
@@ -91,4 +109,5 @@ SELECT
     proposal_start_time,
     proposal_end_time,
     _inserted_timestamp
-FROM proposals_final
+FROM
+    proposals_final
--- models/bronze/api_udf/snapshot/bronze_api__snapshot_voting_strategy.sql
+++ models/bronze/api_udf/snapshot/bronze_api__snapshot_voting_strategy.sql
@@ -5,90 +5,125 @@
     tags = ['non_realtime']
 ) }}
 
-WITH RECURSIVE props_request AS (
+WITH recursive props_request AS (
+
     SELECT
         ethereum.streamline.udf_api(
             'GET',
-            'https://hub.snapshot.org/graphql',{},{ 
-                'query': 'query { proposals(orderBy: "created", orderDirection: asc,first:1000,where:{created_gte: ' || max_time_start || '}) { id space{id voting {delay quorum period type}} created start end } }' }
-            ) AS resp,
+            'https://hub.snapshot.org/graphql',{ 'apiKey':(
+                SELECT
+                    api_key
+                FROM
+                    {{ source(
+                        'crosschain_silver',
+                        'apis_keys'
+                    ) }}
+                WHERE
+                    api_name = 'snapshot'
+            ) },{ 'query': 'query { proposals(orderBy: "created", orderDirection: asc,first:1000,where:{created_gte: ' || max_time_start || '}) { id space{id voting {delay quorum period type}} created start end } }' }
+        ) AS resp,
         SYSDATE() AS _inserted_timestamp,
         1000 AS total_retrieved,
         1000 AS records_to_retrieve
-    FROM (
-        SELECT 
-            DATE_PART(epoch_second, max_prop_start::TIMESTAMP) AS max_time_start
-        FROM (
-            {% if is_incremental() %}
+    FROM
+        (
             SELECT
-                MAX(created_at) AS max_prop_start
+                DATE_PART(
+                    epoch_second,
+                    max_prop_start :: TIMESTAMP
+                ) AS max_time_start
             FROM
-                {{ this }}
-            {% else %}
-            SELECT
-                0 AS max_prop_start
-            {% endif %}
-            ) max_time
-    )
-    UNION ALL
-    SELECT
-        ethereum.streamline.udf_api(
-            'GET',
-            'https://hub.snapshot.org/graphql',{},{ 'query': 'query { proposals(orderBy: "created", orderDirection: asc,first: ' || r.records_to_retrieve || ', skip: ' || r.total_retrieved || ', where:{created_gte: ' || max_time_start || '}) { id space{id voting {delay quorum period type}} created start end } }' }
-        ) AS resp,
-        SYSDATE() AS _inserted_timestamp,
-        r.total_retrieved + r.records_to_retrieve AS total_retrieved,
-        r.records_to_retrieve
-    FROM props_request r
-    JOIN (
-        SELECT 
-            DATE_PART(epoch_second, max_prop_start::TIMESTAMP) AS max_time_start
-        FROM (
-            {% if is_incremental() %}
-            SELECT
-                MAX(created_at) AS max_prop_start
-            FROM
-                {{ this }}
-            {% else %}
-            SELECT
-                0 AS max_prop_start
-            {% endif %}
-            ) max_time
-        ) ON 1=1
-    WHERE r.total_retrieved <= 5000
-),
+                (
 
-proposals_final AS (
-    
+{% if is_incremental() %}
 SELECT
-    VALUE :id :: STRING AS proposal_id, 
-    VALUE :space :id :: STRING AS space_id, 
-    VALUE :space :voting :delay :: INTEGER AS delay,
-    VALUE :space :voting :quorum :: INTEGER AS quorum,
-    VALUE :space :voting :period :: INTEGER AS voting_period,
-    VALUE :space :voting :type :: STRING AS voting_type,
-    TO_TIMESTAMP_NTZ(VALUE :created) AS prop_timestamp,
-    TO_TIMESTAMP_NTZ(VALUE :start) AS proposal_start_time,
-    TO_TIMESTAMP_NTZ(VALUE :end) AS proposal_end_time,
-    VALUE,
-    _inserted_timestamp
+    MAX(created_at) AS max_prop_start
 FROM
-    props_request,
-LATERAL FLATTEN(
-    input => resp :data :data :proposals
-)
-WHERE space_id IS NOT NULL
-    AND proposal_start_time IS NOT NULL
-    AND proposal_end_time IS NOT NULL 
-    AND (delay IS NOT NULL 
-        OR quorum IS NOT NULL 
-        OR voting_period IS NOT NULL 
-        OR voting_type IS NOT NULL)
-QUALIFY(ROW_NUMBER() over (PARTITION BY proposal_id
-  ORDER BY
-    TO_TIMESTAMP_NTZ(VALUE :created) DESC)) = 1
-)
+    {{ this }}
+{% else %}
+SELECT
+    0 AS max_prop_start
+{% endif %}) max_time)
+UNION ALL
+SELECT
+    ethereum.streamline.udf_api(
+        'GET',
+        'https://hub.snapshot.org/graphql',{ 'apiKey':(
+            SELECT
+                api_key
+            FROM
+                {{ source(
+                    'crosschain_silver',
+                    'apis_keys'
+                ) }}
+            WHERE
+                api_name = 'snapshot'
+        ) },{ 'query': 'query { proposals(orderBy: "created", orderDirection: asc,first: ' || r.records_to_retrieve || ', skip: ' || r.total_retrieved || ', where:{created_gte: ' || max_time_start || '}) { id space{id voting {delay quorum period type}} created start end } }' }
+    ) AS resp,
+    SYSDATE() AS _inserted_timestamp,
+    r.total_retrieved + r.records_to_retrieve AS total_retrieved,
+    r.records_to_retrieve
+FROM
+    props_request r
+    JOIN (
+        SELECT
+            DATE_PART(
+                epoch_second,
+                max_prop_start :: TIMESTAMP
+            ) AS max_time_start
+        FROM
+            (
 
+{% if is_incremental() %}
+SELECT
+    MAX(created_at) AS max_prop_start
+FROM
+    {{ this }}
+{% else %}
+SELECT
+    0 AS max_prop_start
+{% endif %}) max_time)
+ON 1 = 1
+WHERE
+    r.total_retrieved <= 5000
+),
+proposals_final AS (
+    SELECT
+        VALUE :id :: STRING AS proposal_id,
+        VALUE :space :id :: STRING AS space_id,
+        VALUE :space :voting :delay :: INTEGER AS delay,
+        VALUE :space :voting :quorum :: INTEGER AS quorum,
+        VALUE :space :voting :period :: INTEGER AS voting_period,
+        VALUE :space :voting :type :: STRING AS voting_type,
+        TO_TIMESTAMP_NTZ(
+            VALUE :created
+        ) AS prop_timestamp,
+        TO_TIMESTAMP_NTZ(
+            VALUE :start
+        ) AS proposal_start_time,
+        TO_TIMESTAMP_NTZ(
+            VALUE :end
+        ) AS proposal_end_time,
+        VALUE,
+        _inserted_timestamp
+    FROM
+        props_request,
+        LATERAL FLATTEN(
+            input => resp :data :data :proposals
+        )
+    WHERE
+        space_id IS NOT NULL
+        AND proposal_start_time IS NOT NULL
+        AND proposal_end_time IS NOT NULL
+        AND (
+            delay IS NOT NULL
+            OR quorum IS NOT NULL
+            OR voting_period IS NOT NULL
+            OR voting_type IS NOT NULL
+        ) qualify(ROW_NUMBER() over (PARTITION BY proposal_id
+    ORDER BY
+        TO_TIMESTAMP_NTZ(VALUE :created) DESC)) = 1
+)
 SELECT
     proposal_id,
     space_id,
@@ -100,4 +135,5 @@ SELECT
     proposal_start_time,
     proposal_end_time,
     _inserted_timestamp
-FROM proposals_final
+FROM
+    proposals_final

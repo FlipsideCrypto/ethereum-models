e67543ee775afc8e6fd00064dd9e08224845cb1a
drethereum <trevor.wenokur@gmail.com>
2023-09-07 09:18:29 -0600

    snapshot votes fix for timeout

--- models/bronze/api_udf/snapshot/bronze_api__snapshot_votes.sql
+++ models/bronze/api_udf/snapshot/bronze_api__snapshot_votes.sql
@@ -5,88 +5,53 @@
     tags = ['snapshot']
 ) }}
 
-WITH recursive votes_request AS (
-
+WITH votes_request AS (
+{% for item in range(5) %}
+(
     SELECT
         ethereum.streamline.udf_api(
             'GET',
-            'https://hub.snapshot.org/graphql',{ 'apiKey':(
-                SELECT
-                    api_key
-                FROM
-                    {{ source(
-                        'crosschain_silver',
-                        'apis_keys'
-                    ) }}
-                WHERE
-                    api_name = 'snapshot'
-            ) },{ 'query': 'query { votes(orderBy: "created", orderDirection: asc, first: 1000, where:{created_gte: ' || max_time_start || '}) { id proposal{id} ipfs voter created choice vp } }' }
+            'https://hub.snapshot.org/graphql',
+            {
+                'apiKey': (
+                    SELECT
+                        api_key
+                    FROM
+                        {{ source(
+                            'crosschain_silver',
+                             'apis_keys'
+                             ) }}
+                    WHERE
+                        api_name = 'snapshot'
+                )
+            },
+            {
+                'query': 'query { votes(orderBy: "created", orderDirection: asc, first: 1000, skip: ' || {{ item * 1000 }} || ', where:{created_gte: ' || max_time_start || '}) { id proposal{id} ipfs voter created choice vp } }'
+            }
         ) AS resp,
-        SYSDATE() AS _inserted_timestamp,
-        1000 AS total_retrieved,
-        1000 AS records_to_retrieve
-    FROM
-        (
-            SELECT
-                DATE_PART(
-                    epoch_second,
-                    max_vote_start :: TIMESTAMP
-                ) AS max_time_start
-            FROM
-                (
-
-{% if is_incremental() %}
-SELECT
-    MAX(vote_timestamp) AS max_vote_start
-FROM
-    {{ this }}
-{% else %}
-SELECT
-    0 AS max_vote_start
-{% endif %}) max_time)
-UNION ALL
-SELECT
-    ethereum.streamline.udf_api(
-        'GET',
-        'https://hub.snapshot.org/graphql',{ 'apiKey':(
-            SELECT
-                api_key
-            FROM
-                {{ source(
-                    'crosschain_silver',
-                    'apis_keys'
-                ) }}
-            WHERE
-                api_name = 'snapshot'
-        ) },{ 'query': 'query { votes(orderBy: "created", orderDirection: asc, first: ' || r.records_to_retrieve || ', skip: ' || r.total_retrieved || ', where:{created_gte: ' || max_time_start || '}) { id proposal{id} ipfs voter created choice vp } }' }
-    ) AS resp,
-    SYSDATE() AS _inserted_timestamp,
-    r.total_retrieved + r.records_to_retrieve AS total_retrieved,
-    r.records_to_retrieve
-FROM
-    votes_request r
-    JOIN (
+        SYSDATE() AS _inserted_timestamp
+    FROM (
         SELECT
-            DATE_PART(
-                epoch_second,
-                max_vote_start :: TIMESTAMP
-            ) AS max_time_start
-        FROM
-            (
-
-{% if is_incremental() %}
-SELECT
-    MAX(vote_timestamp) AS max_vote_start
-FROM
-    {{ this }}
-{% else %}
-SELECT
-    0 AS max_vote_start
-{% endif %}) max_time)
-ON 1 = 1
-WHERE
-    r.total_retrieved <= 5000
+            DATE_PART(epoch_second, max_vote_start :: TIMESTAMP) AS max_time_start
+        FROM (
+            {% if is_incremental() %}
+            SELECT
+                MAX(vote_timestamp) AS max_vote_start
+            FROM
+                {{ this }}
+            {% else %}
+            SELECT
+                0 AS max_vote_start
+            {% endif %}
+        ) AS max_time
+    )
+)
+{% if not loop.last %}
+UNION ALL
+{% endif %}
+{% endfor %}
 ),
+
 votes_final AS (
     SELECT
         SPLIT(

179c91b83270c0bb8d7cdae4e2681ecbaaa898e1
mattromano <mattromano88@gmail.com>
2023-09-12 10:27:55 -0700

    add withdraws

---
+++ models/silver/protocols/complete_lending/silver__complete_lending_withdraws.sql
@@ -0,0 +1,103 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = '_log_id',
+    cluster_by = ['block_timestamp::DATE'],
+    tags = ['non_realtime'],
+) }}
+
+WITH withdraws AS (
+
+    SELECT
+        tx_hash,
+        block_number,
+        block_timestamp,
+        event_index,
+        aave_token AS protocol_token,
+        aave_market AS withdraw_asset,
+        symbol,
+        withdrawn_tokens AS withdraw_amount,
+        withdrawn_usd AS withdraw_amount_usd,
+        depositor_address,
+        aave_version AS protocol,
+        blockchain,
+        _LOG_ID,
+        _INSERTED_TIMESTAMP
+    FROM
+        {{ ref('silver__aave_ez_withdraws') }}
+
+{% if is_incremental() %}
+WHERE
+    l._inserted_timestamp >= (
+        SELECT
+            MAX(
+                _inserted_timestamp
+            ) :: DATE
+        FROM
+            {{ this }}
+    )
+{% endif %}
+UNION ALL
+SELECT
+    tx_hash,
+    block_number,
+    block_timestamp,
+    event_index,
+    compound_market AS protocol_token,
+    asset AS withdraw_asset,
+    symbol,
+    withdraw_tokens AS withdraw_amount,
+    withdrawn_usd AS withdraw_amount_usd,
+    depositor_address,
+    compound_version AS protocol,
+    blockchain,
+    _LOG_ID,
+    _INSERTED_TIMESTAMP
+FROM
+    {{ ref('silver__compv3_ez_withdraws') }}
+
+{% if is_incremental() %}
+WHERE
+    l._inserted_timestamp >= (
+        SELECT
+            MAX(
+                _inserted_timestamp
+            ) :: DATE
+        FROM
+            {{ this }}
+    )
+{% endif %}
+UNION ALL
+SELECT
+    tx_hash,
+    block_number,
+    block_timestamp,
+    event_index,
+    ctoken AS protocol_token,
+    received_contract_address AS withdraw_asset,
+    received_contract_symbol,
+    received_amount AS withdraw_amount,
+    received_amount_usd AS withdraw_amount_usd,
+    redeemer AS depositor_address,
+    'Comp V2' AS protocol,
+    'ethereum' AS blockchain,
+    _LOG_ID,
+    _INSERTED_TIMESTAMP
+FROM
+    {{ ref('compound__ez_redemptions') }}
+
+{% if is_incremental() %}
+WHERE
+    l._inserted_timestamp >= (
+        SELECT
+            MAX(
+                _inserted_timestamp
+            ) :: DATE
+        FROM
+            {{ this }}
+    )
+{% endif %}
+)
+SELECT
+    *
+FROM
+    withdraws

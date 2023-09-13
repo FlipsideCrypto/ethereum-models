862d8a585325fb6870d0d63694f7b3937325065b
mattromano <mattromano88@gmail.com>
2023-09-13 14:46:31 -0700

    frax lend borrows

---
+++ models/silver/protocols/fraxlend/silver__fraxlend_ez_borrows.sql
@@ -0,0 +1,69 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = '_log_id',
+    cluster_by = ['block_timestamp::DATE'],
+    tags = ['non_realtime'],
+) }}
+
+WITH log_join AS (
+
+    SELECT
+        tx_hash,
+        block_timestamp,
+        block_number,
+        regexp_substr_all(SUBSTR(DATA, 3, len(DATA)), '.{64}') AS segmented_data,
+        CONCAT('0x', SUBSTR(topics [1] :: STRING, 27, 42)) AS borrower,
+        CONCAT('0x', SUBSTR(topics [2] :: STRING, 27, 42)) AS reciever,
+        utils.udf_hex_to_int(
+            segmented_data [0] :: STRING
+        ) :: INTEGER / pow(
+            10,
+            decimals
+        ) AS borrow_amount,
+        utils.udf_hex_to_int(
+            segmented_data [1] :: STRING
+        ) :: INTEGER / pow(
+            10,
+            decimals
+        ) AS shares_added,
+        borrow_amount / NULLIF(
+            shares_added,
+            0
+        ) AS borrow_share_price,
+        f.*
+    FROM
+        {{ ref('silver__fraxlend_ez_asset_details') }}
+        f
+        LEFT JOIN {{ ref('silver__logs') }}
+        l
+        ON f.frax_market_address = l.contract_address
+    WHERE
+        topics [0] = '0x01348584ec81ac7acd52b7d66d9ade986dd909f3d513881c190fc31c90527efe'
+
+{% if is_incremental() %}
+AND _inserted_timestamp >= (
+    SELECT
+        MAX(
+            _inserted_timestamp
+        ) :: DATE - 2
+    FROM
+        {{ this }}
+)
+{% endif %}
+)
+SELECT
+    tx_hash,
+    block_timestamp,
+    block_number,
+    borrower,
+    reciever,
+    borrow_amount,
+    shares_added,
+    borrow_share_price,
+    frax_market_address,
+    frax_market_symbol,
+    underlying_asset,
+    _log_id,
+    _inserted_timestamp
+FROM
+    log_join

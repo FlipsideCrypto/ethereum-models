27fee48dcf5bb6e97f04d5a2a3765ba737a87adc
sam <sam@flipsidecrypto.com>
2023-09-14 21:42:37 +0800

    cast as int

--- models/bronze/api_udf/nft/bronze_api__nft_metadata_list.sql
+++ models/bronze/api_udf/nft/bronze_api__nft_metadata_list.sql
@@ -1,5 +1,5 @@
 {{ config(
-    materialized = 'view',
+    materialized = 'table',
     unique_key = 'collection_page'
 ) }}
 
--- models/bronze/api_udf/nft/bronze_api__nft_metadata_reads.sql
+++ models/bronze/api_udf/nft/bronze_api__nft_metadata_reads.sql
@@ -8,7 +8,7 @@ WITH max_row_number as (
 {% if is_incremental() %}
 
 SELECT
-    MAX(row_num) AS max_row_num
+    MAX(row_num)::int AS max_row_num
 FROM
     {{ this }}
 {% else %}
@@ -17,14 +17,8 @@ SELECT
 {% endif %}), 
 
 
--- WITH max_row_number as (
-
--- SELECT
---     0 AS max_row_num
--- ),
-
-requests AS (
-    {% for item in range(2) %}
+ requests AS (
+    {% for item in range(5) %}
     (
 
     SELECT
@@ -34,24 +28,26 @@ requests AS (
         collection_page, 
         row_num, 
         max_row_num,
-        ethereum.streamline.udf_api('POST', node_url,{}, PARSE_JSON(json_request)) AS api_resp, 
+        --ethereum.streamline.udf_api('POST', node_url,{}, PARSE_JSON(json_request)) AS api_resp, 
         SYSDATE() AS _inserted_timestamp
     FROM
         {{ ref('bronze_api__nft_metadata_list') }}
-        JOIN max_row_number 
-        ON 1 = 1 
-        
-    where row_num is not null 
-    {% if is_incremental() %}
-        AND row_num BETWEEN ({{ item }} * 10 + 1 + max_row_num) 
-            AND ((({{ item }} + 1) * 10)  + max_row_num ) 
+        JOIN max_row_number
+        on 1 = 1 
 
+    
+    {% if is_incremental() %}
+    where row_num BETWEEN ({{ item }} * 10 + 1 + max_row_num)  -- + max_row_num
+    AND ((({{ item }} + 1) * 10 + max_row_num)   ) -- + max_row_num
+    
     {% else %}
-    AND row_num BETWEEN ({{ item }} * 20 + 1 + max_row_num) 
-            AND ((({{ item }} + 1) * 20)  + max_row_num ) 
+    where row_num BETWEEN ({{ item }} * 30 + 1 )  --+ max_row_num
+            AND ((({{ item }} + 1) * 30)   ) --+ max_row_num
 
     {% endif %}
-) {% if not loop.last %}
+) 
+
+{% if not loop.last %}
 UNION ALL
 {% endif %}
 {% endfor %}

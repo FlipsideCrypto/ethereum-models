bf852590326b55ba0295a07012035ee89007b745
mattromano <mattromano88@gmail.com>
2023-10-02 13:04:56 -0700

    fix aave backfills and add where filter for borrow test to exclude native ETH protocol token test for Aave v1

--- models/silver/protocols/aave/silver__aave_token_backfill.sql
+++ models/silver/protocols/aave/silver__aave_token_backfill.sql
@@ -4,11 +4,12 @@
     tags = ['non_realtime']
 ) }}
 
-WITH aave_v1_1 AS (
+WITH aave_token_pull AS (
 
     SELECT
         block_number AS atoken_created_block,
         C.symbol AS a_token_symbol,
+        regexp_substr_all(SUBSTR(DATA, 3, len(DATA)), '.{64}') AS segmented_data,
         CONCAT('0x', SUBSTR(topics [2] :: STRING, 27, 40)) AS a_token_address,
         NULL AS atoken_stable_debt_address,
         NULL AS atoken_variable_debt_address,
@@ -22,28 +23,122 @@ WITH aave_v1_1 AS (
         l._inserted_timestamp,
         l._log_id
     FROM
-        ethereum_dev.silver.logs l
+        {{ ref('silver__logs') }}
+        l
         LEFT JOIN ethereum_dev.silver.contracts C
         ON a_token_address = C.address
         LEFT JOIN ethereum_dev.silver.contracts c2
         ON underlying_address = c2.address
     WHERE
         topics [0] = '0x1d9fcd0dc935b4778d5af97f55c4d7b2553257382f1ef25c412114c8eeebd88e'
-        AND origin_from_address = LOWER('0x2fbB0c60a41cB7Ea5323071624dCEAD3d213D0Fa')
-        AND a_token_name IS NOT NULL
+        AND (
+            a_token_name LIKE '%Aave%'
+            OR a_token_name = 'Gho Token'
+        )
+
+{% if is_incremental() %}
+AND l._inserted_timestamp > (
+    SELECT
+        MAX(
+            _inserted_timestamp
+        ) :: DATE
+    FROM
+        {{ this }}
+)
+{% endif %}
+UNION ALL
+SELECT
+    block_number AS atoken_created_block,
+    C.symbol AS a_token_symbol,
+    regexp_substr_all(SUBSTR(DATA, 3, len(DATA)), '.{64}') AS segmented_data,
+    CONCAT('0x', SUBSTR(topics [2] :: STRING, 27, 40)) AS a_token_address,
+    CONCAT('0x', SUBSTR(segmented_data [0] :: STRING, 27, 40)) :: STRING AS atoken_stable_debt_address,
+    CONCAT('0x', SUBSTR(segmented_data [1] :: STRING, 27, 40)) :: STRING AS atoken_variable_debt_address,
+    C.decimals AS a_token_decimals,
+    CASE
+        WHEN C.name LIKE '%AMM%' THEN 'Aave AMM'
+        ELSE 'Aave V2'
+    END AS aave_version,
+    C.name AS a_token_name,
+    c2.symbol AS underlying_symbol,
+    CONCAT('0x', SUBSTR(topics [1] :: STRING, 27, 40)) AS underlying_address,
+    c2.name AS underlying_name,
+    c2.decimals AS underlying_decimals,
+    l._inserted_timestamp,
+    l._log_id
+FROM
+    {{ ref('silver__logs') }}
+    l
+    LEFT JOIN ethereum_dev.silver.contracts C
+    ON a_token_address = C.address
+    LEFT JOIN ethereum_dev.silver.contracts c2
+    ON underlying_address = c2.address
+WHERE
+    topics [0] = '0x3a0ca721fc364424566385a1aa271ed508cc2c0949c2272575fb3013a163a45f'
+    AND block_number < 16291127
+    AND (
+        a_token_name LIKE '%Aave%'
+        OR c2.symbol = 'GHO'
+    )
+
+{% if is_incremental() %}
+AND l._inserted_timestamp > (
+    SELECT
+        MAX(
+            _inserted_timestamp
+        ) :: DATE
+    FROM
+        {{ this }}
+)
+{% endif %}
+UNION ALL
+SELECT
+    block_number AS atoken_created_block,
+    C.symbol AS a_token_symbol,
+    regexp_substr_all(SUBSTR(DATA, 3, len(DATA)), '.{64}') AS segmented_data,
+    CONCAT('0x', SUBSTR(topics [2] :: STRING, 27, 40)) AS a_token_address,
+    CONCAT('0x', SUBSTR(segmented_data [0] :: STRING, 27, 40)) :: STRING AS atoken_stable_debt_address,
+    CONCAT('0x', SUBSTR(segmented_data [1] :: STRING, 27, 40)) :: STRING AS atoken_variable_debt_address,
+    C.decimals AS a_token_decimals,
+    CASE
+        WHEN C.name LIKE '%AMM%' THEN 'Aave AMM'
+        ELSE 'Aave V3'
+    END AS aave_version,
+    C.name AS a_token_name,
+    c2.symbol AS underlying_symbol,
+    CONCAT('0x', SUBSTR(topics [1] :: STRING, 27, 40)) AS underlying_address,
+    c2.name AS underlying_name,
+    c2.decimals AS underlying_decimals,
+    l._inserted_timestamp,
+    l._log_id
+FROM
+    {{ ref('silver__logs') }}
+    l
+    LEFT JOIN ethereum_dev.silver.contracts C
+    ON a_token_address = C.address
+    LEFT JOIN ethereum_dev.silver.contracts c2
+    ON underlying_address = c2.address
+WHERE
+    topics [0] = '0x3a0ca721fc364424566385a1aa271ed508cc2c0949c2272575fb3013a163a45f'
+    AND block_number >= 16291127
+    AND (
+        a_token_name LIKE '%Aave%'
+        OR c2.symbol = 'GHO'
+    )
+
 
 {% if is_incremental() %}
 AND l._inserted_timestamp >= (
     SELECT
         MAX(
             _inserted_timestamp
-        ) :: DATE - 2
+        ) :: DATE -1
     FROM
         {{ this }}
 )
 {% endif %}
 ),
-aave_v1_2 AS (
+aave_token_pull_2 AS (
     SELECT
         atoken_created_block,
         a_token_symbol,
@@ -69,132 +164,8 @@ aave_v1_2 AS (
         _inserted_timestamp,
         _log_id
     FROM
-        aave_v1_1
-),
-debt_tokens_1 AS (
-    SELECT
-        contract_address AS debt_token_address,
-        decoded_flat: debtTokenName :: STRING AS debt_token_name,
-        decoded_flat: debtTokenDecimals :: STRING,
-        decoded_flat: debtTokenSymbol :: STRING,
-        decoded_flat: pool :: STRING AS aave_version_pool,
-        decoded_flat: underlyingAsset :: STRING AS underlying_asset
-    FROM
-        {{ ref('silver__decoded_logs') }}
-    WHERE
-        topics [0] = '0x40251fbfb6656cfa65a00d7879029fec1fad21d28fdcff2f4f68f52795b74f2c'
-        AND decoded_flat: debtTokenName :: STRING LIKE '%Aave%'
-),
-debt_tokens_2 AS (
-    SELECT
-        debt_token_address,
-        debt_token_name,
-        underlying_asset,
-        CASE
-            WHEN debt_token_name LIKE '%variable%' THEN 'variable_debt_token'
-            WHEN debt_token_name LIKE '%Variable%' THEN 'variable_debt_token'
-            WHEN debt_token_name LIKE '%stable%' THEN 'stable_debt_token'
-            WHEN debt_token_name LIKE '%Stable%' THEN 'stable_debt_token'
-            ELSE 'Other'
-        END AS token_type,
-        CASE
-            WHEN debt_token_name LIKE '%variable%' THEN debt_token_address
-            WHEN debt_token_name LIKE '%Variable%' THEN debt_token_address
-        END AS variable_debt_address,
-        CASE
-            WHEN debt_token_name LIKE '%stable%' THEN debt_token_address
-            WHEN debt_token_name LIKE '%Stable%' THEN debt_token_address
-        END AS stable_debt_address,
-        CASE
-            WHEN aave_version_pool = LOWER('0x7d2768de32b0b80b7a3454c06bdac94a69ddc7a9') THEN 'Aave V2'
-            WHEN aave_version_pool = LOWER('0x398eC7346DcD622eDc5ae82352F02bE94C62d119') THEN 'Aave V1'
-            WHEN aave_version_pool = LOWER('0x7937d4799803fbbe595ed57278bc4ca21f3bffcb') THEN 'Aave AMM'
-            WHEN aave_version_pool = LOWER('0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2') THEN 'Aave V3'
-            ELSE 'ERROR'
-        END AS aave_version
-    FROM
-        debt_tokens_1
-    WHERE
-        aave_version <> 'ERROR'
-),
-debt_tokens_3 AS (
-    SELECT
-        underlying_asset,
-        aave_version,
-        MAX(variable_debt_address) AS atoken_variable_debt_address,
-        MAX(stable_debt_address) AS atoken_stable_debt_address
-    FROM
-        debt_tokens_2
-    GROUP BY
-        ALL
-),
-a_token_step_1 AS (
-    SELECT
-        block_number AS atoken_created_block,
-        contract_address AS a_token_address,
-        decoded_flat: aTokenName :: STRING AS a_token_name,
-        decoded_flat: aTokenDecimals :: STRING AS a_token_decimals,
-        decoded_flat: aTokenSymbol :: STRING AS a_token_symbol,
-        decoded_flat: pool :: STRING AS aave_version_pool,
-        decoded_flat: treasury :: STRING AS treasury,
-        decoded_flat: underlyingAsset :: STRING AS underlying_asset,
-        _inserted_timestamp,
-        _log_id
-    FROM
-        {{ ref('silver__decoded_logs') }}
-    WHERE
-        topics [0] = '0xb19e051f8af41150ccccb3fc2c2d8d15f4a4cf434f32a559ba75fe73d6eea20b'
-        AND decoded_flat: aTokenName :: STRING LIKE '%Aave%'
-
-{% if is_incremental() %}
-AND _inserted_timestamp >= (
-    SELECT
-        MAX(
-            _inserted_timestamp
-        ) :: DATE - 2
-    FROM
-        {{ this }}
+        aave_token_pull
 )
-{% endif %}
-),
-a_token_step_2 AS (
-    SELECT
-        *,
-        CASE
-            WHEN aave_version_pool = LOWER('0x7d2768de32b0b80b7a3454c06bdac94a69ddc7a9') THEN 'Aave V2'
-            WHEN aave_version_pool = LOWER('0x398eC7346DcD622eDc5ae82352F02bE94C62d119') THEN 'Aave V1'
-            WHEN aave_version_pool = LOWER('0x7937d4799803fbbe595ed57278bc4ca21f3bffcb') THEN 'Aave AMM'
-            WHEN aave_version_pool = LOWER('0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2') THEN 'Aave V3'
-            ELSE 'ERROR'
-        END AS aave_version
-    FROM
-        a_token_step_1
-)
-SELECT
-    atoken_created_block,
-    a_token_symbol AS atoken_symbol,
-    a_token_address AS atoken_address,
-    atoken_stable_debt_address,
-    atoken_variable_debt_address,
-    a_token_decimals AS atoken_decimals,
-    A.aave_version AS atoken_version,
-    a_token_name AS atoken_name,
-    C.symbol AS underlying_symbol,
-    A.underlying_asset AS underlying_address,
-    C.decimals AS underlying_decimals,
-    C.name AS underlying_name,
-    A._inserted_timestamp,
-    A._log_id
-FROM
-    a_token_step_2 A
-    LEFT JOIN debt_tokens_3 b
-    ON A.underlying_asset = b.underlying_asset
-    AND A.aave_version = b.aave_version
-    LEFT JOIN ethereum_dev.silver.contracts C
-    ON address = A.underlying_asset
-WHERE
-    A.aave_version <> 'ERROR'
-UNION ALL
 SELECT
     atoken_created_block,
     a_token_symbol AS atoken_symbol,
@@ -211,4 +182,4 @@ SELECT
     _inserted_timestamp,
     _log_id
 FROM
-    aave_v1_2
+    aave_token_pull_2

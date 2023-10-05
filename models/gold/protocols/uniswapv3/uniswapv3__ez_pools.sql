46526462c6a58bd5a25add053baf2ad37d642921
Matt Romano <42412983+mattromano@users.noreply.github.com>
2023-10-05 11:36:52 -0700

    AN-3995/optimize-univ3-views (#677)
    * add prices filter to lp actions, and move gold ez pool transformations to silver table - looks like lp action queries are down around 50% w/ change
    
    * add price filter to rest of uni views
    
    * format pools view
    
    * move swaps prices model to silver table
    
    * move all transformation and joins to incremental silver tables
    
    * update so downstream models are not impacted by silver changes
    
    * format update
    
    * update look back to 36 hour and remove prices table look back
    
    * move gold tests to silver .ymls

--- models/gold/protocols/uniswapv3/uniswapv3__ez_pools.sql
+++ models/gold/protocols/uniswapv3/uniswapv3__ez_pools.sql
@@ -12,96 +12,11 @@
     }
 ) }}
 
-WITH contracts AS (
-    SELECT
-        LOWER(address) AS address,
-        symbol,
-        NAME,
-        decimals
-    FROM
-        {{ ref('silver__contracts') }}
-    WHERE
-        decimals IS NOT NULL
-),
-
-token_prices AS (
-    SELECT
-        HOUR,
-        LOWER(token_address) AS token_address,
-        price
-    FROM
-        {{ ref('price__ez_hourly_token_prices') }}
-    WHERE
-        HOUR :: DATE IN (
-            SELECT
-                DISTINCT created_time :: DATE
-            FROM
-                {{ ref('silver__univ3_pools') }} 
-        )
-),
-
-FINAL AS (
-
 SELECT
-    'ethereum' AS blockchain,
+    blockchain,
     created_block AS block_number,
     created_time AS block_timestamp,
     created_tx_hash AS tx_hash,
-    '0x1f98431c8ad98523631ae4a59f267346ea31f984' AS factory_address,
-    token0_address,
-    token1_address,
-    c0.symbol AS token0_symbol,
-    c1.symbol AS token1_symbol,
-    c0.name AS token0_name,
-    c1.name AS token1_name,
-    c0.decimals AS token0_decimals,
-    c1.decimals AS token1_decimals,
-    p0.price AS token0_price,
-    p1.price AS token1_price,
-    fee,
-    fee_percent,
-    COALESCE(
-        init_price_1_0_unadj / pow(
-            10,
-            token1_decimals - token0_decimals
-        ),
-        0
-    ) AS init_price_1_0,
-    init_price_1_0 * token1_price AS init_price_1_0_usd,
-    init_tick,
-    tick_spacing,
-    div0(
-        token1_price,
-        token0_price
-    ) AS usd_ratio,
-    pool_address,
-    CONCAT(
-        token0_symbol,
-        '-',
-        token1_symbol,
-        ' ',
-        fee,
-        ' ',
-        tick_spacing
-    ) AS pool_name
-FROM {{ ref('silver__univ3_pools') }} p 
-LEFT JOIN contracts c0
-    ON c0.address = p.token0_address
-LEFT JOIN contracts c1
-    ON c1.address = p.token1_address
-LEFT JOIN token_prices p0
-    ON p0.token_address = p.token0_address
-    AND p0.hour = DATE_TRUNC('hour',created_time)
-LEFT JOIN token_prices p1
-    ON p1.token_address = p.token1_address
-    AND p1.hour = DATE_TRUNC('hour',created_time)
-)
-
-SELECT
-    blockchain,
-    block_number,
-    block_timestamp,
-    tx_hash,
     factory_address,
     fee,
     fee_percent,
@@ -119,4 +34,5 @@ SELECT
     token1_name,
     token0_decimals,
     token1_decimals    
-FROM FINAL
+FROM 
+    {{ ref('silver__univ3_pools') }}
--- models/gold/protocols/uniswapv3/uniswapv3__ez_swaps.sql
+++ models/gold/protocols/uniswapv3/uniswapv3__ez_swaps.sql
@@ -12,101 +12,6 @@
     }
 ) }}
 
-WITH uni_pools AS (
-    SELECT
-        token0_address,
-        token1_address,
-        fee,
-        fee_percent,
-        tick_spacing,
-        pool_address,
-        token0_symbol,
-        token1_symbol,
-        token0_decimals,
-        token1_decimals,
-        pool_name
-    FROM
-        {{ ref('uniswapv3__ez_pools') }}
-),
-
-token_prices AS (
-    SELECT
-        HOUR,
-        LOWER(token_address) AS token_address,
-        price
-    FROM
-        {{ ref('price__ez_hourly_token_prices') }}
-    WHERE
-        HOUR :: DATE IN (
-            SELECT
-                DISTINCT block_timestamp :: DATE
-            FROM
-                {{ ref('silver__univ3_swaps') }} 
-        )
-),
-
-FINAL AS (
-    SELECT
-        s.blockchain,
-        s.block_number,
-        s.block_timestamp,
-        s.tx_hash,
-        s.pool_address,
-        p.pool_name,
-        recipient,
-        sender,
-        tick,
-        liquidity,
-        COALESCE(
-            liquidity / pow(10,((token0_decimals + token1_decimals) / 2)),0
-                ) AS liquidity_adjusted,
-        event_index,
-        amount0_unadj / pow(
-            10,
-            COALESCE(
-                token0_decimals,
-                18
-            )
-        ) AS amount0_adjusted,
-        amount1_unadj / pow(
-            10,
-            COALESCE(
-                token1_decimals,
-                18
-            )
-        ) AS amount1_adjusted,
-        COALESCE(div0(ABS(amount1_adjusted), ABS(amount0_adjusted)), 0) AS price_1_0,
-        COALESCE(div0(ABS(amount0_adjusted), ABS(amount1_adjusted)), 0) AS price_0_1,
-        p0.price AS token0_price,
-        p1.price AS token1_price,
-        CASE
-            WHEN token0_decimals IS NOT NULL THEN ROUND(
-                token0_price * amount0_adjusted,
-                2
-            )
-        END AS amount0_usd,
-        CASE
-            WHEN token1_decimals IS NOT NULL THEN ROUND(
-                token1_price * amount1_adjusted,
-                2
-            )
-        END AS amount1_usd,
-        s.token0_address,
-        s.token1_address,
-        token0_symbol,
-        token1_symbol
-    FROM
-        {{ ref('silver__univ3_swaps') }} s 
-    LEFT JOIN uni_pools p
-        ON s.pool_address = p.pool_address
-    LEFT JOIN token_prices p0
-        ON p0.token_address = s.token0_address
-            AND p0.hour = DATE_TRUNC('hour',block_timestamp)
-    LEFT JOIN token_prices p1
-        ON p1.token_address = s.token1_address
-            AND p1.hour = DATE_TRUNC('hour',block_timestamp)
-)
-
 SELECT
     blockchain,
     block_number,
@@ -133,4 +38,4 @@ SELECT
     amount0_usd,
     amount1_usd
 FROM
-    FINAL
+    {{ ref('silver__univ3_swaps') }} 

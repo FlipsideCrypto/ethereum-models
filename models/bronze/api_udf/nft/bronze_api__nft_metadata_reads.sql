185d4bf0dc113cb551598ff09ec59e03213773fe
sam <sam@flipsidecrypto.com>
2023-10-10 22:58:15 +0800

    reduce to 25

--- models/bronze/api_udf/nft/bronze_api__nft_metadata_reads.sql
+++ models/bronze/api_udf/nft/bronze_api__nft_metadata_reads.sql
@@ -34,7 +34,7 @@ LIMIT
     FROM
         raw
 ),
-requests AS ({% for item in range(6) %}
+requests AS ({% for item in range(5) %}
     (
 SELECT
     nft_address, current_page, end_page, collection_page, row_num, ethereum.streamline.udf_api('POST', node_url,{}, PARSE_JSON(json_request)) AS api_resp, SYSDATE() AS _inserted_timestamp
@@ -43,8 +43,8 @@ FROM
 
 {% if is_incremental() %}
 WHERE
-    row_num BETWEEN ({{ item }} * 6 + 1)
-    AND ((({{ item }} + 1) * 6))
+    row_num BETWEEN ({{ item }} * 5 + 1)
+    AND ((({{ item }} + 1) * 5))
 {% else %}
 WHERE
     row_num BETWEEN ({{ item }} * 20 + 1)

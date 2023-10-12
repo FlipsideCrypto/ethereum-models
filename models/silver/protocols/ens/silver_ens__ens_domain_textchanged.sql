3cd7ab5f120033a9ce201105958209f57cd94df4
drethereum <71602799+drethereum@users.noreply.github.com>
2023-10-12 15:58:51 -0600

    AN-3734/ens-rebuild (#695)
    * ens renewals, registrations, and nft wraps
    
    * config for ens
    
    * moved folders
    
    * new models
    
    * table clean up
    
    * column type
    
    * nulls
    
    * current records initial build
    
    * updates to current reg
    
    * current records progress
    
    * current records ,registrations, and wrapped updates
    
    * dynamic resolver
    
    * domain wrapped
    
    * transfer owners
    
    * text changed
    
    * current text changed records model updated
    
    * updated current records
    
    * added column
    
    * subdomains
    
    * clean up
    
    * processed name
    
    * edits
    
    * removed unwrapped events and registrations from wrapped
    
    * new ens subdomain columns in current records and deleted subdomain model
    
    * unique manager on textchanged
    
    * delete+insert logic
    
    * new owner and resolver tables
    
    * ens views
    
    * ymls and descriptions
    
    * null logic on subdomains
    
    * reorder columns
    
    * overview docs
    
    * remove config file
    
    * old registar contract
    
    * typo
    
    * new contracts
    
    * new contract on owners

---
+++ models/silver/protocols/ens/silver_ens__ens_domain_textchanged.sql
@@ -0,0 +1,180 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = 'node',
+    tags = ['non_realtime']
+) }}
+
+WITH base_events AS (
+
+    SELECT
+        block_number,
+        block_timestamp,
+        tx_hash,
+        origin_function_signature,
+        origin_from_address,
+        origin_to_address,
+        contract_address,
+        event_index,
+        topics [0] :: STRING AS topic_0,
+        event_name,
+        decoded_flat :"indexedKey" :: STRING AS indexedKey,
+        decoded_flat :"key" :: STRING AS key,
+        decoded_flat :"node" :: STRING AS node,
+        decoded_flat,
+        event_removed,
+        tx_status,
+        _log_id,
+        _inserted_timestamp
+    FROM
+        {{ ref('silver__decoded_logs') }}
+    WHERE
+        topics [0] :: STRING = '0xd8c9334b1a9c2f9da342a0a2b32629c1a229b6445dad78947f674b44444a7550'
+        AND contract_address = '0x4976fb03c32e5b8cfe2b6ccb31c09ba78ebaba41'
+
+{% if is_incremental() %}
+AND _inserted_timestamp >= (
+    SELECT
+        MAX(_inserted_timestamp) - INTERVAL '12 hours'
+    FROM
+        {{ this }}
+)
+{% endif %}
+),
+base_input_data AS (
+    SELECT
+        block_number,
+        block_timestamp,
+        tx_hash,
+        origin_function_signature,
+        origin_from_address,
+        origin_to_address,
+        contract_address,
+        event_index,
+        event_name,
+        from_address,
+        to_address,
+        input_data,
+        indexedKey,
+        key,
+        node,
+        HEX_ENCODE(
+            key :: STRING
+        ) AS key_encoded,
+        CHARINDEX(LOWER(key_encoded), input_data) AS key_index,
+        CASE
+            WHEN key_index = 0 THEN NULL
+            ELSE SUBSTR(
+                input_data,
+                key_index + 128,
+                128
+            )
+        END AS key_name_part,
+        utils.udf_hex_to_string(
+            key_name_part :: STRING
+        ) AS key_translate,
+        _log_id,
+        _inserted_timestamp
+    FROM
+        base_events b
+        LEFT JOIN {{ ref('silver__transactions') }}
+        t USING(tx_hash) qualify(ROW_NUMBER() over (PARTITION BY node, key
+    ORDER BY
+        block_timestamp DESC)) = 1
+),
+aggregated_object AS (
+    SELECT
+        MAX(block_number) AS latest_block,
+        MAX(block_timestamp) AS latest_timestamp,
+        MAX(manager) AS manager,
+        node,
+        OBJECT_AGG(
+            key :: variant,
+            key_translate :: variant
+        ) AS profile_info,
+        MAX(_inserted_timestamp) AS _inserted_timestamp
+    FROM
+        base_input_data 
+    INNER JOIN (
+        SELECT node, origin_from_address AS manager, MAX(block_timestamp) AS latest_timestamp
+        FROM base_input_data
+        GROUP BY 1,2
+        QUALIFY(ROW_NUMBER() OVER (PARTITION BY node ORDER BY latest_timestamp DESC)) = 1
+    ) USING(node)
+    GROUP BY
+        node
+)
+
+{% if is_incremental() %},
+merged AS (
+    SELECT
+        latest_block,
+        latest_timestamp,
+        manager,
+        node,
+        profile_info,
+        _inserted_timestamp
+    FROM
+        aggregated_object
+    UNION ALL
+    SELECT
+        latest_block,
+        latest_timestamp,
+        manager,
+        node,
+        profile_info,
+        _inserted_timestamp
+    FROM
+        {{ this }}
+),
+flattened AS (
+    SELECT
+        latest_block,
+        latest_timestamp,
+        manager,
+        node,
+        profile_info,
+        key,
+        VALUE :: STRING AS VALUE,
+        _inserted_timestamp
+    FROM
+        merged,
+        LATERAL FLATTEN(input => profile_info) qualify(ROW_NUMBER() over (PARTITION BY node, key
+    ORDER BY
+        latest_timestamp DESC)) = 1
+),
+FINAL AS (
+    SELECT
+        MAX(latest_block) AS latest_block,
+        MAX(latest_timestamp) AS latest_timestamp,
+        MAX(manager) AS manager,
+        node,
+        OBJECT_AGG(
+            key :: variant,
+            VALUE :: variant
+        ) AS profile_info,
+        MAX(_inserted_timestamp) AS _inserted_timestamp
+    FROM
+        flattened
+    GROUP BY
+        node
+)
+SELECT
+    latest_block,
+    latest_timestamp,
+    manager,
+    node,
+    profile_info,
+    _inserted_timestamp
+FROM
+    FINAL
+{% else %}
+SELECT
+    latest_block,
+    latest_timestamp,
+    manager,
+    node,
+    profile_info,
+    _inserted_timestamp
+FROM
+    aggregated_object
+{% endif %}

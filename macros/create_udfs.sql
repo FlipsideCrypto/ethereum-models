01db23fae2d3df1d9df97f77e5dea548a77f6c48
drethereum <trevor.wenokur@gmail.com>
2023-10-24 14:54:26 -0600

    updated udf 2

--- macros/create_udfs.sql
+++ macros/create_udfs.sql
@@ -8,9 +8,6 @@
         {{ create_udf_transform_logs2(
             schema = 'silver'
         ) }}
-        {{ create_udf_transform_logs3(
-            schema = 'silver'
-        ) }}
         {{ create_udtf_get_base_table(
             schema = "streamline"
         ) }}
--- macros/python/transform_logs2.sql
+++ macros/python/transform_logs2.sql
@@ -7,31 +7,30 @@ handler = 'transform' as $$
 from copy import deepcopy
 
 def transform_tuple(components: list, values: list):
-    mapped_values = {}
+    transformed_values = []
     for i, component in enumerate(components):
         if i < len(values):
             if component["type"] == "tuple[]":
-                mapped_values[component["name"]] = [transform_tuple(component["components"], v) for v in values[i]]
+                sub_values = [transform_tuple(component["components"], v) for v in values[i]]
+                transformed_values.append({"value": sub_values, **component})
             else:
-                mapped_values[component["name"]] = values[i]
-    return mapped_values
+                transformed_values.append({"value": values[i], **component})
+    return {item["name"]: item["value"] for item in transformed_values}
 
 def transform_event(event: dict):
     new_event = deepcopy(event)
     if new_event.get("components"):
         components = new_event.get("components")
-        transformed_data = {}
         
         if isinstance(new_event["value"][0], list):
             result_list = []
             for value_set in new_event["value"]:
                 result_list.append(transform_tuple(components, value_set))
-            transformed_data["value"] = result_list
+            new_event["value"] = result_list
 
         else:
-            transformed_data["value"] = transform_tuple(components, new_event["value"])
+            new_event["value"] = transform_tuple(components, new_event["value"])
 
-        new_event.update(transformed_data)
         return new_event
 
     else:

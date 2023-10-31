a465e71f1886ef1a40f3c01ccc55086314c0ca4d
drethereum <71602799+drethereum@users.noreply.github.com>
2023-10-31 14:21:42 -0600

    Add/bridge-curation-ethereum (#700)
    * arbitrum transfers
    
    * native bridges
    
    * bridge casts
    
    * synapse
    
    * complete bridge transfers out
    
    * native bridges and hop token reads
    
    * chain id
    
    * filter on source
    
    * name
    
    * axelar squid
    
    * updated tests
    
    * destination name and id clean up
    
    * gitignoree
    
    * gitignore revert
    
    * package update
    
    * tests, cleanup and docs
    
    * moved folders
    
    * removed duped bridges
    
    * native transfers
    
    * docs
    
    * remove config
    
    * clean up
    
    * amount_usd
    
    * add curated tags
    
    * wormhole transfers
    
    * wormhole and docs
    
    * qualify
    
    * stargate
    
    * meson seed
    
    * meson added
    
    * updated tags
    
    * name change

---
+++ models/silver/defi/bridge/allbridge/silver_bridge__allbridge_sent.sql
@@ -0,0 +1,119 @@
+{{ config(
+    materialized = 'incremental',
+    incremental_strategy = 'delete+insert',
+    unique_key = "block_number",
+    cluster_by = ['block_timestamp::DATE'],
+    tags = ['curated','reorg']
+) }}
+
+WITH base_evt AS (
+
+    SELECT
+        block_number,
+        block_timestamp,
+        tx_hash,
+        origin_function_signature,
+        origin_from_address,
+        origin_to_address,
+        contract_address,
+        'allbridge' AS NAME,
+        event_index,
+        topics [0] :: STRING AS topic_0,
+        event_name,
+        TRY_TO_NUMBER(
+            decoded_flat :"amount" :: STRING
+        ) AS amount,
+        utils.udf_hex_to_string(
+            SUBSTRING(
+                decoded_flat :"destination" :: STRING,
+                3
+            )
+        ) AS destination_chain,
+        decoded_flat :"lockId" :: STRING AS lockId,
+        decoded_flat :"recipient" :: STRING AS recipient,
+        decoded_flat :"sender" :: STRING AS sender,
+        utils.udf_hex_to_string(
+            SUBSTRING(
+                decoded_flat :"tokenSource" :: STRING,
+                3
+            )
+        ) AS token_source,
+        REGEXP_REPLACE(
+            decoded_flat :"tokenSourceAddress" :: STRING, '0+$', '') AS tokenSourceAddress,
+        decoded_flat,
+        event_removed,
+        tx_status,
+        _log_id,
+        _inserted_timestamp
+    FROM
+        {{ ref('silver__decoded_logs') }}
+    WHERE
+        topics [0] :: STRING = '0x884a8def17f0d5bbb3fef53f3136b5320c9b39f75afb8985eeab9ea1153ee56d'
+        AND contract_address = '0xbbbd1bbb4f9b936c3604906d7592a644071de884'
+
+{% if is_incremental() %}
+AND _inserted_timestamp >= (
+    SELECT
+        MAX(_inserted_timestamp) - INTERVAL '12 hours'
+    FROM
+        {{ this }}
+)
+{% endif %}
+HAVING LENGTH(tokenSourceAddress) = 42
+)
+SELECT
+    block_number,
+    block_timestamp,
+    origin_function_signature,
+    origin_from_address,
+    origin_to_address,
+    tx_hash,
+    event_index,
+    topic_0,
+    event_name,
+    event_removed,
+    tx_status,
+    contract_address AS bridge_address,
+    NAME AS platform,
+    sender,
+    recipient AS receiver,
+    amount,
+    lockId AS lock_id,
+    CASE 
+        WHEN destination_chain = 'AURO' THEN 'aurora mainnet'
+        WHEN destination_chain = 'AVA' THEN 'avalanche c-chain'
+        WHEN destination_chain = 'BSC' THEN 'bnb smart chain mainnet'
+        WHEN destination_chain = 'CELO' THEN 'celo mainnet'
+        WHEN destination_chain = 'ETH' THEN 'ethereum mainnet'
+        WHEN destination_chain = 'FTM' THEN 'fantom opera'
+        WHEN destination_chain = 'HECO' THEN 'huobi eco chain mainnet'
+        WHEN destination_chain = 'KLAY' THEN 'klaytn mainnet cypress'
+        WHEN destination_chain = 'POL' THEN 'polygon mainnet'
+        WHEN destination_chain = 'SOL' THEN 'solana'
+        WHEN destination_chain = 'TRA' THEN 'terra'
+        WHEN destination_chain = 'TEZ' THEN 'tezos'
+        WHEN destination_chain = 'WAVE' THEN 'waves'
+        ELSE LOWER(destination_chain) 
+    END AS destination_chain,
+    CASE 
+        WHEN token_source = 'AURO' THEN 'aurora mainnet'
+        WHEN token_source = 'AVA' THEN 'avalanche c-chain'
+        WHEN token_source = 'BSC' THEN 'bnb smart chain mainnet'
+        WHEN token_source = 'CELO' THEN 'celo mainnet'
+        WHEN token_source = 'ETH' THEN 'ethereum mainnet'
+        WHEN token_source = 'FTM' THEN 'fantom opera'
+        WHEN token_source = 'HECO' THEN 'huobi eco chain mainnet'
+        WHEN token_source = 'KLAY' THEN 'klaytn mainnet cypress'
+        WHEN token_source = 'POL' THEN 'polygon mainnet'
+        WHEN token_source = 'SOL' THEN 'solana'
+        WHEN token_source = 'TRA' THEN 'terra'
+        WHEN token_source = 'TEZ' THEN 'tezos'
+        WHEN token_source = 'WAVE' THEN 'waves'
+        ELSE LOWER(token_source)
+    END AS source_chain,
+    tokenSourceAddress AS token_address,
+    _log_id,
+    _inserted_timestamp
+FROM
+    base_evt
+WHERE source_chain = 'ethereum mainnet'
---
+++ models/silver/defi/bridge/wormhole/silver_bridge__wormhole_transfers.sql
@@ -0,0 +1,218 @@
+{{ config(
+    materialized = 'incremental',
+    incremental_strategy = 'delete+insert',
+    unique_key = "block_number",
+    cluster_by = ['block_timestamp::DATE'],
+    tags = ['curated','reorg']
+) }}
+
+WITH token_transfers AS (
+
+    SELECT
+        tr.block_number,
+        tr.block_timestamp,
+        tr.origin_function_signature,
+        tr.origin_from_address,
+        tr.origin_to_address,
+        tr.tx_hash,
+        event_index,
+        tr.contract_address,
+        tr.from_address,
+        tr.to_address,
+        raw_amount,
+        regexp_substr_all(SUBSTR(input_data, 11, len(input_data)), '.{64}') AS segmented_data,
+        TRY_TO_NUMBER(
+            utils.udf_hex_to_int(
+                segmented_data [2] :: STRING
+            )
+        ) AS destination_chain_id,
+        CONCAT(
+            '0x',
+            segmented_data [3] :: STRING
+        ) AS recipient1,
+        CONCAT('0x', SUBSTR(segmented_data [3] :: STRING, 25, 40)) AS recipient2,
+        LENGTH(
+            REGEXP_SUBSTR(
+                segmented_data [3] :: STRING,
+                '^(0*)'
+            )
+        ) AS len,
+        CASE
+            WHEN len >= 24 THEN recipient2
+            ELSE recipient1
+        END AS destination_recipient_address,
+        CONCAT('0x', SUBSTR(segmented_data [0] :: STRING, 25, 40)) AS token,
+        TRY_TO_NUMBER(utils.udf_hex_to_int(segmented_data [1] :: STRING)) AS amount,
+        utils.udf_hex_to_int(
+            segmented_data [4] :: STRING
+        ) AS arbiterFee,
+        utils.udf_hex_to_int(
+            segmented_data [5] :: STRING
+        ) AS nonce,
+        _log_id,
+        tr._inserted_timestamp
+    FROM
+        {{ ref('silver__transfers') }}
+        tr
+        INNER JOIN {{ ref('silver__transactions') }}
+        tx
+        ON tr.block_number = tx.block_number
+        AND tr.tx_hash = tx.tx_hash
+    WHERE
+        tr.from_address <> '0x0000000000000000000000000000000000000000'
+        AND tr.to_address = '0x3ee18b2214aff97000d974cf647e7c347e8fa585'
+        AND tr.origin_function_signature = '0x0f5287b0' -- tokenTransfer
+        AND destination_chain_id <> 0
+
+{% if is_incremental() %}
+AND tr._inserted_timestamp >= (
+    SELECT
+        MAX(_inserted_timestamp) - INTERVAL '12 hours'
+    FROM
+        {{ this }}
+)
+{% endif %}
+),
+native_transfers AS (
+    SELECT
+        et.block_number,
+        et.block_timestamp,
+        et.tx_hash,
+        tx.from_address AS origin_from_address,
+        tx.to_address AS origin_to_address,
+        tx.origin_function_signature,
+        et.from_address,
+        et.to_address,
+        eth_value,
+        identifier,
+        input,
+        regexp_substr_all(SUBSTR(input_data, 11, len(input_data)), '.{64}') AS segmented_data,
+        TRY_TO_NUMBER(
+            utils.udf_hex_to_int(
+                segmented_data [0] :: STRING
+            )
+        ) AS destination_chain_id,
+        CONCAT(
+            '0x',
+            segmented_data [1] :: STRING
+        ) AS recipient1,
+        CONCAT('0x', SUBSTR(segmented_data [1] :: STRING, 25, 40)) AS recipient2,
+        LENGTH(
+            REGEXP_SUBSTR(
+                segmented_data [1] :: STRING,
+                '^(0*)'
+            )
+        ) AS len,
+        CASE
+            WHEN len >= 24 THEN recipient2
+            ELSE recipient1
+        END AS destination_recipient_address,
+        utils.udf_hex_to_int(
+            segmented_data [2] :: STRING
+        ) AS arbiterFee,
+        utils.udf_hex_to_int(
+            segmented_data [3] :: STRING
+        ) AS nonce,
+        _call_id,
+        et._inserted_timestamp
+    FROM
+        {{ ref('silver__eth_transfers') }}
+        et
+        INNER JOIN {{ ref('silver__transactions') }}
+        tx
+        ON et.block_number = tx.block_number
+        AND et.tx_hash = tx.tx_hash
+    WHERE
+        et.to_address = '0x3ee18b2214aff97000d974cf647e7c347e8fa585'
+        AND tx.origin_function_signature = '0x9981509f' -- wrapAndTransfer
+        AND destination_chain_id <> 0
+
+{% if is_incremental() %}
+AND et._inserted_timestamp >= (
+    SELECT
+        MAX(_inserted_timestamp) - INTERVAL '12 hours'
+    FROM
+        {{ this }}
+)
+{% endif %}
+),
+all_transfers AS (
+    SELECT
+        block_number,
+        block_timestamp,
+        origin_from_address,
+        origin_to_address,
+        origin_function_signature,
+        tx_hash,
+        event_index,
+        'Transfer' AS event_name,
+        to_address AS bridge_address,
+        from_address AS sender,
+        to_address AS receiver,
+        raw_amount AS amount_unadj,
+        destination_chain_id,
+        contract_address AS token_address,
+        destination_recipient_address,
+        {{ dbt_utils.generate_surrogate_key(
+            ['_log_id']
+        ) }} AS _id,
+        _inserted_timestamp
+    FROM
+        token_transfers
+    UNION ALL
+    SELECT
+        block_number,
+        block_timestamp,
+        origin_from_address,
+        origin_to_address,
+        origin_function_signature,
+        tx_hash,
+        NULL AS event_index,
+        NULL AS event_name,
+        to_address AS bridge_address,
+        from_address AS sender,
+        to_address AS receiver,
+        eth_value * pow(
+            10,
+            18
+        ) AS amount_unadj,
+        destination_chain_id,
+        '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' AS token_address,
+        destination_recipient_address,
+        {{ dbt_utils.generate_surrogate_key(
+            ['_call_id']
+        ) }} AS _id,
+        _inserted_timestamp
+    FROM
+        native_transfers
+)
+SELECT
+    block_number,
+    block_timestamp,
+    origin_from_address,
+    origin_to_address,
+    origin_function_signature,
+    tx_hash,
+    event_index,
+    event_name,
+    'wormhole' AS platform,
+    bridge_address,
+    sender,
+    receiver,
+    amount_unadj,
+    destination_chain_id,
+    chain_name AS destination_chain,
+    token_address,
+    destination_recipient_address,
+    --address on the destination chain, requires decoding for non-EVM - more info: https://docs.wormhole.com/wormhole/blockchain-environments/environments
+    _id,
+    _inserted_timestamp
+FROM
+    all_transfers t
+    LEFT JOIN {{ ref('silver_bridge__wormhole_chain_id_seed') }}
+    s
+    ON t.destination_chain_id :: STRING = s.wormhole_chain_id :: STRING
+WHERE
+    origin_to_address IS NOT NULL qualify (ROW_NUMBER() over (PARTITION BY _id
+ORDER BY
+    _inserted_timestamp DESC)) = 1

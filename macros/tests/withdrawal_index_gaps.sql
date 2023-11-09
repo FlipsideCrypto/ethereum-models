1ee8c37c33b3557453298c0d2e18380b4f78e6a5
drethereum <trevor.wenokur@gmail.com>
2023-11-09 12:18:19 -0700

    updated test

--- macros/tests/withdrawal_index_gaps.sql
+++ macros/tests/withdrawal_index_gaps.sql
@@ -29,26 +29,9 @@ gaps AS (
     WHERE
         next_index IS NOT NULL
         AND expected_index <> next_index
-),
-FINAL AS (
-    SELECT
-        withdrawal_index,
-        next_index,
-        expected_index,
-        start_slot_number,
-        end_slot_number,
-        start_slot_number + _id AS missing_slot_number
-    FROM
-        gaps
-        JOIN {{ ref('silver__number_sequence') }}
-        ON _id BETWEEN 1
-        AND (
-            end_slot_number - start_slot_number - 1
-        )
-    WHERE
-        missing_slot_number < end_slot_number
 )
 SELECT
-    DISTINCT missing_slot_number AS slot_number
+    next_index - withdrawal_index AS gaps
 FROM
-    FINAL {% endtest %}
+    gaps 
+{% endtest %}

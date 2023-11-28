20eb6ed802ea3397056920a148ce21b015261bf4
mattromano <mattromano88@gmail.com>
2023-11-28 12:39:06 -0800

    merge main and update ranker

--- models/silver/core/silver__contracts.sql
+++ models/silver/core/silver__contracts.sql
@@ -80,12 +80,13 @@ SELECT
     decimals,
     contract_metadata,
     _inserted_timestamp,
-    CASE 
-        WHEN decimals IS NOT NULL AND symbol IS NOT NULL AND NAME IS NOT NULL THEN 3
-        WHEN symbol IS NOT NULL AND decimals IS NOT NULL THEN 2
-        WHEN NAME IS NOT NULL AND decimals IS NOT NULL THEN 2
-        WHEN decimals IS NOT NULL THEN  1
-        WHEN symbol IS NOT NULL THEN  1
+  CASE 
+        WHEN decimals IS NOT NULL AND symbol IS NOT NULL AND NAME IS NOT NULL THEN 7
+        WHEN symbol IS NOT NULL AND decimals IS NOT NULL THEN 6
+        WHEN NAME IS NOT NULL AND decimals IS NOT NULL THEN 5
+        WHEN NAME IS NOT NULL AND symbol IS NOT NULL THEN 4
+        WHEN decimals IS NOT NULL THEN  3
+        WHEN symbol IS NOT NULL THEN  2
         WHEN name IS NOT NULL THEN  1
         ELSE 0
     END AS ranker

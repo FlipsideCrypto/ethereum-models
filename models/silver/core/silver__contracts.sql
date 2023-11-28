15c31d1e04a9ca8dc2efcfff953fc938e545e36f
Matt Romano <42412983+mattromano@users.noreply.github.com>
2023-11-28 15:09:14 -0800

    AN-4166/Incorrect-decimals-in-ethereum-silver-token-reads-bug-fix (#751)
    * add read_output exclude
    
    * add read_output len filter
    
    * add blank name and symbol filters
    
    * add qualify to decimals cte
    
    * REMOVE THE REGEX AND REPLACE WITH NOT BLANK FILTER
    
    * add qualify statements to symbol and name
    
    * weird decimal fix
    
    * add max to decimals
    
    * decimal statement
    
    * fix call name filter
    
    * add qualify with inserted_timestamp and add decimal trailing 0 regex
    
    * add read_output filter
    
    * add null read filter
    
    * case when ranking
    
    * ranking
    
    * finish ranker
    
    * update where clause
    
    * merge main and update ranker

--- models/silver/core/silver__contracts.sql
+++ models/silver/core/silver__contracts.sql
@@ -40,14 +40,9 @@ streamline_reads AS (
         {{ ref('silver__token_meta_reads') }} A
         LEFT JOIN legacy
         ON LOWER(contract_address) = LOWER(address)
-    WHERE
-        (
-            token_name IS NOT NULL
-            AND token_symbol IS NOT NULL
-        )
-
 {% if is_incremental() %}
-AND A._inserted_timestamp >= (
+WHERE
+A._inserted_timestamp >= (
     SELECT
         MAX(
             _inserted_timestamp
@@ -79,8 +74,23 @@ all_records AS (
         streamline_reads
 )
 SELECT
-    *
+    address,
+    symbol,
+    NAME,
+    decimals,
+    contract_metadata,
+    _inserted_timestamp,
+  CASE 
+        WHEN decimals IS NOT NULL AND symbol IS NOT NULL AND NAME IS NOT NULL THEN 7
+        WHEN symbol IS NOT NULL AND decimals IS NOT NULL THEN 6
+        WHEN NAME IS NOT NULL AND decimals IS NOT NULL THEN 5
+        WHEN NAME IS NOT NULL AND symbol IS NOT NULL THEN 4
+        WHEN decimals IS NOT NULL THEN  3
+        WHEN symbol IS NOT NULL THEN  2
+        WHEN name IS NOT NULL THEN  1
+        ELSE 0
+    END AS ranker
 FROM
     all_records qualify(ROW_NUMBER() over(PARTITION BY address
 ORDER BY
-    _inserted_timestamp DESC)) = 1
+    ranker DESC)) = 1

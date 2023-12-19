1ae66d743592d084a803c06265c7bc7ef0137f82
Matt Romano <42412983+mattromano@users.noreply.github.com>
2023-12-19 09:13:40 -0800

    AN-4158/move-recent-tests-to-72-hour-from-block-look-back (#783)
    72 hour look back

--- models/silver/core/tests/blocks/test_silver__blocks_recent.sql
+++ models/silver/core/tests/blocks/test_silver__blocks_recent.sql
@@ -8,11 +8,7 @@ WITH last_3_days AS (
     SELECT
         block_number
     FROM
-        {{ ref("_max_block_by_date") }}
-        qualify ROW_NUMBER() over (
-            ORDER BY
-                block_number DESC
-        ) = 3
+        {{ ref("_block_lookback") }}
 )
 SELECT
     *

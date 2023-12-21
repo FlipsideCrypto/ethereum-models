153d365a876d22f04ce08c66ce5d2136f956afce
mattromano <mattromano88@gmail.com>
2023-12-21 12:18:26 -0800

    add router to pools, fix xchainswaps

--- models/silver/defi/dex/hashflow/silver_dex__hashflow_v3_pools.sql
+++ models/silver/defi/dex/hashflow/silver_dex__hashflow_v3_pools.sql
@@ -5,23 +5,24 @@
     tags = ['curated']
 ) }}
 
-WITH contract_deployments AS (
-
+WITH logs_pull as (
     SELECT
         tx_hash,
         block_number,
         block_timestamp,
-        regexp_substr_all(SUBSTR(DATA, 3, len(DATA)), '.{64}') AS segmented_data,
-        origin_from_address AS deployer_address,
-        CONCAT('0x', SUBSTR(segmented_data [0] :: STRING, 25, 40)) AS pool_address,
+        data,
+        contract_address,
+        origin_from_address,
         _log_id,
         _inserted_timestamp
     FROM
         {{ ref('silver__logs') }}
     WHERE
-        contract_address = '0xde828fdc3f497f16416d1bb645261c7c6a62dab5'
-        AND topics [0] :: STRING = '0xdbd2a1ea6808362e6adbec4db4969cbc11e3b0b28fb6c74cb342defaaf1daada'
-
+        (
+            contract_address = '0xde828fdc3f497f16416d1bb645261c7c6a62dab5'
+            AND topics [0] :: STRING = '0xdbd2a1ea6808362e6adbec4db4969cbc11e3b0b28fb6c74cb342defaaf1daada'
+        )
+        OR tx_hash = '0x89ded8a7efaec848cc4230e160dbfdb262008d43276b75d0647d2f96938aefb1'
 {% if is_incremental() %}
 AND _inserted_timestamp >= (
     SELECT
@@ -30,12 +31,48 @@ AND _inserted_timestamp >= (
         {{ this }}
 )
 {% endif %}
+),
+contract_deployments AS (
+
+    SELECT
+        tx_hash,
+        block_number,
+        block_timestamp,
+        regexp_substr_all(SUBSTR(DATA, 3, len(DATA)), '.{64}') AS segmented_data,
+        origin_from_address AS deployer_address,
+        c.name as pool_name,
+        CONCAT('0x', SUBSTR(segmented_data [0] :: STRING, 25, 40)) AS pool_address,
+        l._log_id,
+        l._inserted_timestamp
+    FROM
+         logs_pull l 
+    LEFT JOIN
+        {{ ref('silver__contracts') }} C
+    ON
+        pool_address = address
+    WHERE
+        contract_address = '0xde828fdc3f497f16416d1bb645261c7c6a62dab5'
+
 )
 SELECT
     tx_hash,
     block_number,
     block_timestamp,
+    origin_from_address as deployer_address,
+    'router' as pool_name,
+    contract_address as pool_address,
+    _log_id,
+    _inserted_timestamp
+FROM
+    logs_pull
+WHERE tx_hash = '0x89ded8a7efaec848cc4230e160dbfdb262008d43276b75d0647d2f96938aefb1'
+UNION ALL
+SELECT
+    tx_hash,
+    block_number,
+    block_timestamp,
     deployer_address,
+    pool_name,
     pool_address,
     _log_id,
     _inserted_timestamp
--- models/silver/defi/dex/hashflow/silver_dex__hashflow_v3_swaps.sql
+++ models/silver/defi/dex/hashflow/silver_dex__hashflow_v3_swaps.sql
@@ -71,45 +71,27 @@ router_swaps AS (
         l.origin_function_signature,
         l.origin_from_address,
         l.origin_to_address,
-        CASE
-            WHEN l.topics [0] = '0x3f72b2a38919490277652bb34955c871b20e23068c243319c9fa5e27963d9e12' THEN 'Trade'
-            WHEN l.topics [0] = '0x34f57786fb01682fb4eec88d340387ef01a168fe345ea5b76f709d4e560c10eb' THEN 'XChainTrade'
-        ELSE NULL END AS event_name,
+        'XChainTrade'AS event_name,
         l.event_index,
         contract_address,
         regexp_substr_all(SUBSTR(DATA, 3, len(DATA)), '.{64}') AS segmented_data,
-        CONCAT('0x', SUBSTR(segmented_data [2] :: STRING, 25, 40)) AS effective_trader_address,
+        decoded_flat :effectiveTrader AS effective_trader_address,
         --The trader wallet address that will swap with the contract. This can be a proxy contract
-        NULL AS trader_address, 
+        NULL AS trader_address,
         --The wallet address of the actual trader
-        CONCAT(
-            '0x',
-            segmented_data [2] :: STRING
-        ) AS txid,
-        CONCAT('0x', SUBSTR(segmented_data [5] :: STRING, 25, 40)) AS tokenIn,
-        CONCAT('0x', SUBSTR(segmented_data [6] :: STRING, 25, 40)) AS tokenOut,
-        TRY_TO_NUMBER(
-            utils.udf_hex_to_int(
-                segmented_data [7] :: STRING
-            )
-        ) AS amountIn,
-        TRY_TO_NUMBER(
-            utils.udf_hex_to_int(
-                segmented_data [8] :: STRING
-            )
-        ) AS amountOut,
+        decoded_flat :txid AS txid,
+        decoded_flat :baseToken AS tokenIn,
+        decoded_flat :quoteToken AS tokenOut,
+        decoded_flat :baseTokenAmount AS amountIn,
+        decoded_flat :quoteTokenAmount AS amountOut,
         l._log_id,
         l._inserted_timestamp
     FROM
         {{ ref('silver__decoded_logs') }}
         l
     WHERE
-        l.topics [0] :: STRING IN (
-            '0x3f72b2a38919490277652bb34955c871b20e23068c243319c9fa5e27963d9e12' --Xchaintrade
-            ,'0x34f57786fb01682fb4eec88d340387ef01a168fe345ea5b76f709d4e560c10eb' --Router trade
-        )
-    AND
-        origin_to_address = lower('0x55084eE0fEf03f14a305cd24286359A35D735151') --router
+        l.topics [0] :: STRING = '0x3f72b2a38919490277652bb34955c871b20e23068c243319c9fa5e27963d9e12' --Xchaintrade
+        AND origin_to_address = LOWER('0x55084eE0fEf03f14a305cd24286359A35D735151') --router
 
 {% if is_incremental() %}
 AND _inserted_timestamp >= (

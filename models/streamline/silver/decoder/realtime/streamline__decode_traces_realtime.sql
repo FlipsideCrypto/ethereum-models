d81acfd0d2750c7e614fdc22a674113d21f004d5
Austin <austin@flipsidecrypto.com>
2024-02-12 16:01:40 -0500

    functions

--- models/streamline/silver/decoder/realtime/streamline__decode_traces_realtime.sql
+++ models/streamline/silver/decoder/realtime/streamline__decode_traces_realtime.sql
@@ -1,5 +1,6 @@
 {{ config (
     materialized = "view",
+    post_hook = [if_data_call_function( func = "{{this.schema}}.udf_bulk_decode_functions(object_construct('sql_source', '{{this.identifier}}','producer_batch_size', 20000000,'producer_limit_size', 20000000))", target = "{{this.schema}}.{{this.identifier}}" ),"call system$wait(" ~ var("WAIT", 400) ~ ")" ],
     tags = ['streamline_decoded_traces_realtime']
 ) }}
 

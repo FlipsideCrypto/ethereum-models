46809adaa652efd2bf5b461e59b4d37c19289cbe
Austin <93135983+austinFlipside@users.noreply.github.com>
2024-02-13 16:23:55 -0500

    traces history (#819)
    * traces history
    
    * timing

--- macros/streamline/models.sql
+++ macros/streamline/models.sql
@@ -62,6 +62,77 @@ WHERE
     )
 {% endmacro %}
 
+{% macro decode_traces_history(
+        start,
+        stop
+    ) %}
+
+WITH look_back AS (
+
+    SELECT
+        block_number
+    FROM
+        {{ ref("_max_block_by_date") }}
+        qualify ROW_NUMBER() over (
+            ORDER BY
+                block_number DESC
+        ) = 1
+)
+SELECT
+    t.block_number,
+    t.tx_hash,
+    t.trace_index,
+    _call_id,
+    A.abi AS abi,
+    A.function_name AS function_name,
+    CASE
+        WHEN TYPE = 'DELEGATECALL' THEN from_address
+        ELSE to_address
+    END AS abi_address,
+    t.input AS input,
+    COALESCE(
+        t.output,
+        '0x'
+    ) AS output
+FROM
+    {{ ref("silver__traces") }}
+    t
+    INNER JOIN {{ ref("silver__complete_function_abis") }} A
+    ON A.parent_contract_address = abi_address
+    AND LEFT(
+        t.input,
+        10
+    ) = LEFT(
+        A.function_signature,
+        10
+    )
+    AND t.block_number BETWEEN A.start_block
+    AND A.end_block
+WHERE
+    (t.block_number BETWEEN {{ start }} AND {{ stop }})
+    and t.block_number < (
+        SELECT
+            block_number
+        FROM
+            look_back
+    )
+    AND t.block_number IS NOT NULL
+    AND _call_id NOT IN (
+        SELECT
+            _call_id
+        FROM
+            {{ ref("streamline__complete_decode_traces") }}
+        WHERE
+            (block_number BETWEEN {{ start }} AND {{ stop }})
+            and block_number < (
+                SELECT
+                    block_number
+                FROM
+                    look_back
+            ))
+
+{% endmacro %}
+
 {% macro streamline_external_table_query(
         model,
         partition_function,

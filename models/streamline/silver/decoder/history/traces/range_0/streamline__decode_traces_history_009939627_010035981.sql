e664724b2484171f6a553841b3ea00cf8beef89f
Austin <austin@flipsidecrypto.com>
2024-03-07 10:44:09 -0500

    adds range amd edits params

--- models/streamline/silver/decoder/history/traces/range_0/streamline__decode_traces_history_009939627_010035981.sql
+++ models/streamline/silver/decoder/history/traces/range_0/streamline__decode_traces_history_009939627_010035981.sql
@@ -1,6 +1,6 @@
 {{ config (
     materialized = "view",
-    post_hook = [if_data_call_function( func = "{{this.schema}}.udf_bulk_decode_traces(object_construct('sql_source', '{{this.identifier}}','producer_batch_size', 500000,'producer_limit_size', 20000000))", target = "{{this.schema}}.{{this.identifier}}" ),if_data_call_wait()],
+    post_hook = [if_data_call_function( func = "{{this.schema}}.udf_bulk_decode_traces(object_construct('sql_source', '{{this.identifier}}','producer_batch_size', 500000,'producer_limit_size', 10000000))", target = "{{this.schema}}.{{this.identifier}}" ),if_data_call_wait()],
     tags = ['streamline_decoded_traces_history_range_0']
 ) }}
 

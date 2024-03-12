27ff0c3319f6dd16173e3f76449cbe0457e7e4cf
Austin <austin@flipsidecrypto.com>
2024-03-11 23:06:29 -0400

    update

--- models/beacon_chain/bronze/bronze__blob_sidecars.sql
+++ models/beacon_chain/bronze/bronze__blob_sidecars.sql
@@ -24,6 +24,7 @@ WHERE
         resp :error :: STRING,
         1
     ) <> 'F'
+    OR resp :error :: STRING IS NULL
 {% endif %}
 ORDER BY
     slot_number ASC
@@ -35,65 +36,29 @@ create_range AS (
             ORDER BY
                 slot_number
         ) AS row_no,
-<<<<<<< HEAD
-        row_no / 2 AS batch_no
+        row_no / 3 AS batch_no
     FROM
         slot_range
-) {% for item in range(400) %}
-=======
-        CEIL(
-            row_no / 3
-        ) AS batch_no
-    FROM
-        (
-            SELECT
-                _id AS slot_number
-            FROM
-                {{ ref("silver__number_sequence") }}
-            WHERE
-                _id BETWEEN 4537722
-                AND (
-                    SELECT
-                        max_slot
-                    FROM
-                        current_slot
-                )
-
-{% if is_incremental() %}
-EXCEPT
-SELECT
-    slot_number
-FROM
-    {{ this }}
-WHERE
-    LEFT(
-        resp :error :: STRING,
-        1
-    ) <> 'F'
-    OR resp :error IS NULL
-{% endif %}
 )
 ORDER BY
-    slot_number ASC
-) {% for item in range(200) %}
->>>>>>> cc5968f2e0d1b43111f72e5b5df5b7cdb52db664
-SELECT
-    slot_number,
-    live.udf_api(
-        CONCAT(
-            '{service}',
-            '/',
-            '{Authentication}',
-            'eth/v1/beacon/blob_sidecars/',
-            slot_number :: STRING
-        ),
-        'Vault/prod/ethereum/quicknode/mainnet'
-    ) AS resp,
-    SYSDATE() AS _inserted_timestamp
-FROM
-    create_range
-WHERE
-    batch_no = {{ item }} + 1 {% if not loop.last %}
-    UNION ALL
-    {% endif %}
-{% endfor %}
+    slot_number ASC {% for item in range(200) %}
+    SELECT
+        slot_number,
+        live.udf_api(
+            CONCAT(
+                '{service}',
+                '/',
+                '{Authentication}',
+                'eth/v1/beacon/blob_sidecars/',
+                slot_number :: STRING
+            ),
+            'Vault/prod/ethereum/quicknode/mainnet'
+        ) AS resp,
+        SYSDATE() AS _inserted_timestamp
+    FROM
+        create_range
+    WHERE
+        batch_no = {{ item }} + 1 {% if not loop.last %}
+        UNION ALL
+        {% endif %}
+    {% endfor %}

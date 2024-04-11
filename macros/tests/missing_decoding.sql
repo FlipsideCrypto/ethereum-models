b8f1889298de49d48b25075f5e39944ec2496d19
Austin <93135983+austinFlipside@users.noreply.github.com>
2024-04-11 11:49:38 -0400

    missing decoding (#885)

---
+++ macros/tests/missing_decoding.sql
@@ -0,0 +1,37 @@
+{% test missing_decoded_logs(model) %}
+SELECT
+    l.block_number,
+    l._log_id
+FROM
+    {{ ref('silver__logs') }}
+    l
+    LEFT JOIN {{ model }}
+    d
+    ON l.block_number = d.block_number
+    AND l._log_id = d._log_id
+WHERE
+    l.contract_address = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' -- WETH
+    AND l.topics [0] :: STRING = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef' -- Transfer
+    AND l.block_timestamp BETWEEN DATEADD('hour', -48, SYSDATE())
+    AND DATEADD('hour', -6, SYSDATE())
+    AND d._log_id IS NULL {% endtest %}
+    {% test missing_decoded_traces(model) %}
+SELECT
+    t.block_number,
+    t._call_id
+FROM
+    {{ ref('silver__traces') }}
+    t
+    LEFT JOIN {{ model }}
+    d
+    ON t.block_number = d.block_number
+    AND t._call_id = d._call_id
+WHERE
+    t.to_address = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' -- WETH
+    AND LEFT(
+        t.input,
+        10
+    ) = '0xa9059cbb' -- transfer(address,uint256)
+    AND t.block_timestamp BETWEEN DATEADD('hour', -48, SYSDATE())
+    AND DATEADD('hour', -6, SYSDATE())
+    AND d._call_id IS NULL {% endtest %}

b1c5cf4f49d54db1dadb34be6560dae493d5ef16
drethereum <trevor.wenokur@gmail.com>
2024-05-23 11:57:41 -0600

    ether fi

--- models/silver/defi/liquid_staking/etherfi/silver_lsd__etherfi_withdrawals.sql
+++ models/silver/defi/liquid_staking/etherfi/silver_lsd__etherfi_withdrawals.sql
@@ -3,44 +3,43 @@
     incremental_strategy = 'delete+insert',
     unique_key = "block_number",
     cluster_by = ['block_timestamp::DATE'],
-    enabled = false,
     tags = ['curated','reorg']
 ) }}
---disabled until launch of liquid staking derivative token
 
 WITH withdrawals AS (
 
     SELECT
-        l.block_number,
-        l.block_timestamp,
-        l.origin_function_signature,
-        l.origin_from_address,
-        l.origin_to_address,
-        l.tx_hash,
+        block_number,
+        block_timestamp,
+        origin_function_signature,
+        origin_from_address,
+        origin_to_address,
+        tx_hash,
         event_index,
-        'Withdrawn' AS event_name,
-        l.contract_address,
-        CONCAT('0x', SUBSTR(topics [1] :: STRING, 27, 40)) AS sender,
-        t.eth_value * pow(
-            10,
-            18
+        'Transfer' AS event_name,
+        contract_address,
+        CONCAT('0x', SUBSTR(topics [1] :: STRING, 27, 40)) AS from_address,
+        CONCAT('0x', SUBSTR(topics [2] :: STRING, 27, 40)) AS to_address,
+        regexp_substr_all(SUBSTR(DATA, 3, len(DATA)), '.{64}') AS segmented_data,
+        TRY_TO_NUMBER(
+            utils.udf_hex_to_int(
+                segmented_data [0] :: STRING
+            )
         ) AS amount,
-        t.eth_value AS amount_adj,
+        (amount / pow(10, 18)) :: FLOAT AS amount_adj,
         _log_id,
-        l._inserted_timestamp
+        _inserted_timestamp
     FROM
         {{ ref('silver__logs') }}
-        l
-        LEFT JOIN {{ ref('silver__traces') }}
-        t
-        ON l.tx_hash = t.tx_hash
     WHERE
-        topics [0] :: STRING = '0xf45a04d08a70caa7eb4b747571305559ad9fdf4a093afd41506b35c8a306fa94' --Withdrawn
-        AND l.contract_address = '0x7623e9dc0da6ff821ddb9ebaba794054e078f8c4' --Ether.fi Early Adopter Program (eETH LSD token not yet deployed)
-        AND CONCAT('0x', SUBSTR(topics [1] :: STRING, 27, 40)) = t.to_address
+        block_timestamp :: DATE >= '2023-07-01'
+        AND topics [0] :: STRING = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef' --Withdraw/Burn (Transfer)
+        AND contract_address = LOWER('0x35fA164735182de50811E8e2E824cFb9B6118ac2') --ether.fi: eETH Token (eETH)
+        AND to_address = '0x0000000000000000000000000000000000000000'
+        AND tx_status = 'SUCCESS'
 
 {% if is_incremental() %}
-AND l._inserted_timestamp >= (
+AND _inserted_timestamp >= (
     SELECT
         MAX(_inserted_timestamp) - INTERVAL '12 hours'
     FROM
@@ -58,13 +57,16 @@ SELECT
     event_index,
     event_name,
     contract_address,
-    sender,
-    sender AS recipient,
+    from_address AS sender,
+    from_address AS recipient,
     amount AS eth_amount,
     amount_adj AS eth_amount_adj,
+    eth_amount AS token_amount,
+    eth_amount_adj AS token_amount_adj,
+    contract_address AS token_address,
+    'eETH' AS token_symbol,
+    'etherfi' AS platform,
     _log_id,
     _inserted_timestamp
 FROM
-    withdrawals qualify (ROW_NUMBER() over (PARTITION BY _log_id
-ORDER BY
-    _inserted_timestamp DESC)) = 1
+    withdrawals

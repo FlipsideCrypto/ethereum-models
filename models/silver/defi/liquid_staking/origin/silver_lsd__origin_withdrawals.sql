261fa65c9b71720c8fd520be6a3eb66df832cc29
drethereum <trevor.wenokur@gmail.com>
2024-05-23 17:38:26 -0600

    stakestone liquid and origin

---
+++ models/silver/defi/liquid_staking/origin/silver_lsd__origin_withdrawals.sql
@@ -0,0 +1,72 @@
+{{ config(
+    materialized = 'incremental',
+    incremental_strategy = 'delete+insert',
+    unique_key = "block_number",
+    cluster_by = ['block_timestamp::DATE'],
+    tags = ['curated','reorg']
+) }}
+
+WITH withdrawals AS (
+
+    SELECT
+        block_number,
+        block_timestamp,
+        origin_function_signature,
+        origin_from_address,
+        origin_to_address,
+        tx_hash,
+        event_index,
+        'Transfer' AS event_name,
+        contract_address,
+        CONCAT('0x', SUBSTR(topics [1] :: STRING, 27, 40)) AS from_address,
+        CONCAT('0x', SUBSTR(topics [2] :: STRING, 27, 40)) AS to_address,
+        regexp_substr_all(SUBSTR(DATA, 3, len(DATA)), '.{64}') AS segmented_data,
+        TRY_TO_NUMBER(
+            utils.udf_hex_to_int(
+                segmented_data [0] :: STRING
+            )
+        ) AS amount,
+        (amount / pow(10, 18)) :: FLOAT AS amount_adj,
+        _log_id,
+        _inserted_timestamp
+    FROM
+        {{ ref('silver__logs') }}
+    WHERE
+        block_timestamp :: DATE >= '2023-03-01'
+        AND topics [0] :: STRING = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef' --Withdraw/Burn (Transfer)
+        AND contract_address = LOWER('0x856c4Efb76C1D1AE02e20CEB03A2A6a08b0b8dC3') --Origin Ether (OETH)
+        AND to_address = '0x0000000000000000000000000000000000000000'
+        AND tx_status = 'SUCCESS'
+
+{% if is_incremental() %}
+AND _inserted_timestamp >= (
+    SELECT
+        MAX(_inserted_timestamp) - INTERVAL '12 hours'
+    FROM
+        {{ this }}
+)
+{% endif %}
+)
+SELECT
+    block_number,
+    block_timestamp,
+    origin_function_signature,
+    origin_from_address,
+    origin_to_address,
+    tx_hash,
+    event_index,
+    event_name,
+    contract_address,
+    from_address AS sender,
+    from_address AS recipient,
+    amount AS eth_amount,
+    amount_adj AS eth_amount_adj,
+    eth_amount AS token_amount,
+    eth_amount_adj AS token_amount_adj,
+    LOWER(contract_address) AS token_address,
+    'OETH' AS token_symbol,
+    'origin' AS platform,
+    _log_id,
+    _inserted_timestamp
+FROM
+    withdrawals

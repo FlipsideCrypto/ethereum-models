9b304ebc369f64c5381171fff8385e4a214bad32
sam <sam@flipsidecrypto.com>
2024-06-13 09:45:49 +0800

    add arb

---
+++ models/silver/nft/mev/silver_nft__arbitrage_trades.sql
@@ -0,0 +1,55 @@
+{{ config(
+    materialized = 'table',
+    unique_key = ['block_number', 'bought_event_index', 'nft_address', 'tokenid'],
+    cluster_by = ['block_timestamp::DATE']
+) }}
+
+WITH base_sales AS (
+
+    SELECT
+        *
+    FROM
+        {{ ref('nft__ez_nft_sales') }}
+)
+SELECT
+    b1.block_number,
+    b1.block_timestamp,
+    b1.tx_hash,
+    b1.event_index AS bought_event_index,
+    b2.event_index AS sold_event_index,
+    b1.nft_address,
+    b1.tokenid,
+    b1.erc1155_value,
+    b2.erc1155_value AS sold_erc1155_value,
+    b1.project_name,
+    b1.buyer_address,
+    b1.price AS bought,
+    b2.price AS sold,
+    b1.currency_address AS bought_currency,
+    b2.currency_address AS sold_currency,
+    b1.platform_name AS bought_platform_name,
+    b2.platform_name AS sold_platform_name,
+    b1.event_type AS bought_event_type,
+    b2.event_type AS sold_event_type,
+    b1.price_usd AS bought_usd,
+    b2.price_usd AS sold_usd,
+    b1.tx_fee,
+    b1.tx_fee_usd,
+    sold_usd - (
+        bought_usd + b1.tx_fee_usd
+    ) AS profit_usd
+FROM
+    base_sales b1
+    INNER JOIN base_sales b2
+    ON b1.tx_hash = b2.tx_hash
+    AND b1.buyer_address = b2.seller_address
+    AND b1.nft_address = b2.nft_address
+    AND b1.tokenid = b2.tokenid
+    AND b2.event_index > b1.event_index
+WHERE
+    1 = 1 --    b1.erc1155_value IS NULL
+    AND b1.buyer_address != b1.seller_address
+    AND b1.platform_exchange_version NOT IN (
+        'nftx',
+        'cryptopunks'
+    )

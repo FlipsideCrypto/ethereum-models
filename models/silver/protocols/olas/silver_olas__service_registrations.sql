9ca3e5c637e9a2b201af6708137372428f209b7a
drethereum <trevor.wenokur@gmail.com>
2024-06-27 16:02:49 -0600

    heal tag

--- models/silver/protocols/olas/silver_olas__service_registrations.sql
+++ models/silver/protocols/olas/silver_olas__service_registrations.sql
@@ -3,7 +3,7 @@
     incremental_strategy = 'delete+insert',
     unique_key = 'block_number',
     cluster_by = ['block_timestamp::DATE'],
-    tags = ['curated','reorg']
+    tags = ['curated','reorg','heal']
 ) }}
 
 WITH registry_evt AS (

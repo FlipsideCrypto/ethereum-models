63e51b1b9eed24fbdb747a739ecabec7732aaabb
drethereum <trevor.wenokur@gmail.com>
2024-07-26 10:57:57 -0600

    decoder 2

---
+++ models/streamline/silver/decoder/realtime2/streamline__decode_logs_realtime2.sql
@@ -0,0 +1,67 @@
+{{ config (
+    materialized = "view",
+    post_hook = [fsc_utils.if_data_call_function_v2(
+        func = 'streamline.udf_bulk_decode_logs_v2',
+        target = "{{this.schema}}.{{this.identifier}}",
+        params ={ "external_table" :"DECODED_LOGS",
+        "sql_limit" :"20000000",
+        "producer_batch_size" :"20000000",
+        "worker_batch_size" :"10000000",
+        "sql_source" :"{{this.identifier}}" }
+    ),
+    "call system$wait(" ~ var("WAIT", 400) ~ ")" ],
+    tags = ['streamline_decoded_logs_realtime']
+) }}
+-- v2 external tables required?
+
+WITH look_back AS (
+
+    SELECT
+        block_number
+    FROM
+        {{ ref("_24_hour_lookback") }}
+)
+SELECT
+    l.block_number,
+    l._log_id,
+    A.abi AS abi,
+    OBJECT_CONSTRUCT(
+        'topics',
+        l.topics,
+        'data',
+        l.data,
+        'address',
+        l.contract_address
+    ) AS DATA
+FROM
+    {{ ref("silver__logs") }}
+    l
+    INNER JOIN {{ ref("silver__complete_event_abis") }} A
+    ON A.parent_contract_address = l.contract_address
+    AND A.event_signature = l.topics [0] :: STRING
+    AND l.block_number BETWEEN A.start_block
+    AND A.end_block
+WHERE
+    (
+        l.block_number >= (
+            SELECT
+                block_number
+            FROM
+                look_back
+        )
+    )
+    AND l.block_number IS NOT NULL
+    AND l.block_timestamp >= DATEADD('day', -2, CURRENT_DATE())
+    AND _log_id NOT IN (
+        SELECT
+            _log_id
+        FROM
+            {{ ref("streamline__complete_decode_logs") }}
+        WHERE
+            block_number >= (
+                SELECT
+                    block_number
+                FROM
+                    look_back
+            )
+            AND _inserted_timestamp >= DATEADD('day', -2, CURRENT_DATE()))

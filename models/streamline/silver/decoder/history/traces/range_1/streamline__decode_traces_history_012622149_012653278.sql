b900b34e5435ef3cee6f7b5722fd537fcd26572b
drethereum <71602799+drethereum@users.noreply.github.com>
2024-09-17 15:53:57 -0600

    revert/if-data-call-fnc-traces (#950)
    data call function

--- models/streamline/silver/decoder/history/traces/range_1/streamline__decode_traces_history_012622149_012653278.sql
+++ models/streamline/silver/decoder/history/traces/range_1/streamline__decode_traces_history_012622149_012653278.sql
@@ -1,15 +1,6 @@
 {{ config (
     materialized = "view",
-    post_hook = [fsc_utils.if_data_call_function_v2(
-        func = 'streamline.udf_bulk_decode_traces',
-        target = "{{this.schema}}.{{this.identifier}}",
-        params ={ "external_table" :"DECODED_TRACES", 
-        "sql_limit" :"20000000",
-        "producer_batch_size" :"500000",
-        "worker_batch_size" :"20000",
-        "sql_source" :"{{this.identifier}}" }
-    ),
-    fsc_utils.if_data_call_wait()],
+    post_hook = [if_data_call_function( func = "{{this.schema}}.udf_bulk_decode_traces(object_construct('sql_source', '{{this.identifier}}','producer_batch_size', 500000,'producer_limit_size', 10000000))", target = "{{this.schema}}.{{this.identifier}}" ),if_data_call_wait()],
     tags = ['streamline_decoded_traces_history_range_1']
 ) }}
 
--- models/streamline/silver/decoder/realtime/streamline__decode_traces_realtime.sql
+++ models/streamline/silver/decoder/realtime/streamline__decode_traces_realtime.sql
@@ -1,15 +1,6 @@
 {{ config (
     materialized = "view",
-    post_hook = [fsc_utils.if_data_call_function_v2(
-        func = 'streamline.udf_bulk_decode_traces',
-        target = "{{this.schema}}.{{this.identifier}}",
-        params ={ "external_table" :"DECODED_TRACES", 
-        "sql_limit" :"20000000",
-        "producer_batch_size" :"500000",
-        "worker_batch_size" :"20000",
-        "sql_source" :"{{this.identifier}}" }
-    ),
-    fsc_utils.if_data_call_wait()],
+    post_hook = [if_data_call_function( func = "{{this.schema}}.udf_bulk_decode_traces(object_construct('sql_source', '{{this.identifier}}','producer_batch_size', 500000,'producer_limit_size', 20000000))", target = "{{this.schema}}.{{this.identifier}}" ),"call system$wait(" ~ var("WAIT", 400) ~ ")" ],
     tags = ['streamline_decoded_traces_realtime']
 ) }}
 {{ fsc_evm.streamline_decoded_traces_requests(

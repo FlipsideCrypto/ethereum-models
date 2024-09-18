14fff655dbb0c2b0a70b55d6a152f59b0041c2e9
mattromano <mattromano88@gmail.com>
2024-09-18 16:00:26 -0700

    update to decoded traces

--- models/silver/defi/lending/morpho/silver__morpho_liquidations.sql
+++ models/silver/defi/lending/morpho/silver__morpho_liquidations.sql
@@ -18,23 +18,16 @@ WITH traces AS (
             input,
             10
         ) AS function_sig,
-        len(input) AS segmented_input_len,
-        regexp_substr_all(SUBSTR(input, 11), '.{64}') AS segmented_input,
-        CONCAT('0x', SUBSTR(segmented_input [0] :: STRING, 25)) AS loan_token,
-        CONCAT('0x', SUBSTR(segmented_input [1] :: STRING, 25)) AS collateral_token,
-        CONCAT('0x', SUBSTR(segmented_input [2] :: STRING, 25)) AS oracle_address,
-        CONCAT('0x', SUBSTR(segmented_input [3] :: STRING, 25)) AS irm_address,
-        CONCAT('0x', SUBSTR(segmented_input [5] :: STRING, 25)) AS borrower,
-        TRY_TO_NUMBER(
-            utils.udf_hex_to_int(
-                segmented_input [6] :: STRING
-            )
-        ) AS amount,
-        COUNT(*) OVER (PARTITION BY tx_hash) AS tx_hash_count,
+        decoded_data:decoded_input_data:loanToken::STRING AS loan_token,
+        decoded_data:decoded_input_data:marketParams:collateralToken::STRING AS collateral_token,
+        decoded_data:decoded_input_data:marketParams:oracle::STRING AS oracle_address,
+        decoded_data:decoded_input_data:marketParams:irm::STRING AS irm_address,
+        decoded_data:decoded_input_data:borrower::STRING AS borrower,
+        decoded_data:decoded_output_data:output_1::INTEGER AS amount,
         _call_id,
         _inserted_timestamp
     FROM
-        {{ ref('silver__traces') }}
+        {{ ref('silver__decoded_traces') }}
     WHERE
         to_address = '0xbbbbbbbbbb9cc5e90e3b3af64bdaf62c37eeffcb' --Morpho Blue
         AND function_sig = '0xd8eabcb8'
@@ -123,47 +116,6 @@ FROM
     traces t
     INNER JOIN logs l
     ON l.tx_hash = t.tx_hash
-    LEFT JOIN {{ ref('silver__contracts') }}
-    c0
-    ON c0.address = t.loan_token
-    LEFT JOIN {{ ref('silver__contracts') }}
-    c1
-    ON c1.address = t.collateral_token
-WHERE t.tx_hash_count = 1
-UNION ALL
-SELECT
-    l.tx_hash,
-    l.block_number,
-    l.block_timestamp,
-    l.event_index,
-    l.origin_from_address,
-    l.origin_to_address,
-    l.origin_function_signature,
-    l.contract_address,
-    l.caller AS liquidator,
-    l.borrower,
-    t.loan_token AS debt_asset,
-    c0.symbol AS debt_asset_symbol,
-    l.repay_assets AS repayed_amount_unadj,
-    l.repay_assets / pow(
-        10,
-        c0.decimals
-    ) AS repayed_amount,
-    t.collateral_token AS collateral_asset,
-    c1.symbol AS collateral_asset_symbol,
-    l.seized_assets AS amount_unadj,
-    l.seized_assets / pow(
-        10,
-        c1.decimals
-    ) AS amount,
-    'Morpho Blue' AS platform,
-    'ethereum' AS blockchain,
-    t._call_id AS _id,
-    t._inserted_timestamp
-FROM
-    traces t
-    INNER JOIN logs l
-    ON l.tx_hash = t.tx_hash
     AND l.seized_assets = t.amount
     LEFT JOIN {{ ref('silver__contracts') }}
     c0
@@ -171,4 +123,3 @@ FROM
     LEFT JOIN {{ ref('silver__contracts') }}
     c1
     ON c1.address = t.collateral_token
-WHERE t.tx_hash_count >= 2

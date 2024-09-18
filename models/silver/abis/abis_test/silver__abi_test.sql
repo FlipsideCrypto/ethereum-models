87233cb441440e14ed0123d40883e17b8ddbeb97
sam <sam@flipsidecrypto.com>
2024-09-19 00:24:58 +0800

    test

---
+++ models/silver/abis/abis_test/silver__abi_test.sql
@@ -0,0 +1,12 @@
+-- depends_on: {{ ref('bronze__streamline_contract_abis') }}
+{{ config (
+    materialized = "incremental",
+    unique_key = "contract_address",
+    merge_update_columns = ["contract_address"],
+    post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION on equality(contract_address)",
+    tags = ['abis']
+) }}
+{{ fsc_evm.silver_bytecode_abis (
+    chain = 'ethereum',
+    is_ethereum = true
+) }}

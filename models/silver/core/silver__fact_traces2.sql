c103bde2466dc4a4a03dbd34a333c40db958bf49
Austin <austin@flipsidecrypto.com>
2024-09-23 10:43:55 -0400

    regular logic

--- models/silver/core/silver__fact_traces2.sql
+++ models/silver/core/silver__fact_traces2.sql
@@ -5,11 +5,10 @@
     incremental_predicates = [fsc_evm.standard_predicate()],
     cluster_by = "block_timestamp::date",
     post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION",
-    tags = ['traces_reload'],
+    tags = ['realtime'],
     full_refresh = false
 ) }}
 {{ fsc_evm.gold_traces_v1(
     full_reload_start_block = 3000000,
-    full_reload_blocks = 1000000,
-    full_reload_mode = true
+    full_reload_blocks = 1000000
 ) }}

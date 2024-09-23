217c389991763fb4b58dec59456039012d58d15b
Austin <93135983+austinFlipside@users.noreply.github.com>
2024-09-23 10:34:30 -0400

    reload gold traces (#957)

--- models/silver/core/silver__fact_traces2.sql
+++ models/silver/core/silver__fact_traces2.sql
@@ -1,15 +1,15 @@
--- depends_on: {{ ref('bronze__streamline_traces') }}
 {{ config (
     materialized = "incremental",
     incremental_strategy = 'delete+insert',
     unique_key = "block_number",
-    cluster_by = ['modified_timestamp::DATE','partition_key'],
+    incremental_predicates = [fsc_evm.standard_predicate()],
+    cluster_by = "block_timestamp::date",
     post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION",
-    full_refresh = false,
-    tags = ['realtime']
+    tags = ['traces_reload'],
+    full_refresh = false
 ) }}
-{{ fsc_evm.silver_traces_v1(
+{{ fsc_evm.gold_traces_v1(
     full_reload_start_block = 3000000,
     full_reload_blocks = 1000000,
-    use_partition_key = true
+    full_reload_mode = true
 ) }}

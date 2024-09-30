eeceb2fa2a3492a15ac26c5b4da35c78754d641c
drethereum <trevor.wenokur@gmail.com>
2024-09-30 11:53:05 -0600

    stats macros

--- models/gold/stats/stats__ez_core_metrics_hourly.sql
+++ models/gold/stats/stats__ez_core_metrics_hourly.sql
@@ -5,34 +5,6 @@
     meta ={ 'database_tags':{ 'table':{ 'PURPOSE': 'STATS, METRICS, CORE, HOURLY',
     } } }
 ) }}
-
-SELECT
-    block_timestamp_hour,
-    block_number_min,
-    block_number_max,
-    block_count,
-    transaction_count,
-    transaction_count_success,
-    transaction_count_failed,
-    unique_from_count,
-    unique_to_count,
-    total_fees AS total_fees_native,
-    ROUND(
-        total_fees * LAST_VALUE(
-            p.price ignore nulls
-        ) over (
-            ORDER BY
-                block_timestamp_hour rows unbounded preceding
-        ),
-        2
-    ) AS total_fees_usd,
-    core_metrics_hourly_id AS ez_core_metrics_hourly_id,
-    s.inserted_timestamp AS inserted_timestamp,
-    s.modified_timestamp AS modified_timestamp
-FROM
-    {{ ref('silver_stats__core_metrics_hourly') }}
-    s
-    LEFT JOIN {{ ref('price__ez_prices_hourly') }}
-    p
-    ON s.block_timestamp_hour = p.hour
-    AND p.token_address = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' --WETH
+{{ fsc_evm.stats_ez_core_metrics_hourly(
+    token_address = var('STATS_TOKEN_ADDRESS')
+) }}

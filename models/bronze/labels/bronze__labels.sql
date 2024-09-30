ddbc27bfc560bc77e69025fbf5686cee74a66d87
drethereum <trevor.wenokur@gmail.com>
2024-09-30 11:39:23 -0600

    labels macros

--- models/bronze/labels/bronze__labels.sql
+++ models/bronze/labels/bronze__labels.sql
@@ -1,25 +1,4 @@
 {{ config(
     materialized = 'view'
 ) }}
-
-SELECT
-    system_created_at,
-    insert_date,
-    blockchain,
-    address,
-    creator,
-    label_type,
-    label_subtype,
-    address_name,
-    project_name,
-    _is_deleted,
-    modified_timestamp,
-    labels_combined_id
-FROM
-    {{ source(
-        'crosschain_silver',
-        'labels_combined'
-    ) }}
-WHERE
-    blockchain = 'ethereum'
-    AND address LIKE '0x%'
+{{ fsc_evm.bronze_labels() }}
--- models/gold/core/core__dim_labels.sql
+++ models/gold/core/core__dim_labels.sql
@@ -3,17 +3,4 @@
     persist_docs ={ "relation": true,
     "columns": true }
 ) }}
-
-SELECT
-    blockchain,
-    creator,
-    address,
-    address_name,
-    label_type,
-    label_subtype,
-    project_name AS label,
-    labels_combined_id AS dim_labels_id,
-    inserted_timestamp,
-    modified_timestamp
-FROM
-    {{ ref('silver__labels') }}
+{{ fsc_evm.core_dim_labels() }}

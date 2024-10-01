a842e8d3774850e75e1e89ffd78200a55f1a0176
drethereum <trevor.wenokur@gmail.com>
2024-10-01 15:25:22 -0600

    stats macros

--- models/gold/stats/stats__ez_core_metrics_hourly.sql
+++ models/gold/stats/stats__ez_core_metrics_hourly.sql
@@ -1,10 +1,1 @@
-{{ config(
-    materialized = 'view',
-    persist_docs ={ "relation": true,
-    "columns": true },
-    meta ={ 'database_tags':{ 'table':{ 'PURPOSE': 'STATS, METRICS, CORE, HOURLY',
-    } } }
-) }}
-{{ fsc_evm.stats_ez_core_metrics_hourly(
-    token_address = var('STATS_TOKEN_ADDRESS')
-) }}
+{{ fsc_evm.stats_ez_core_metrics_hourly() }}
--- models/silver/stats/silver_stats__core_metrics_hourly.sql
+++ models/silver/stats/silver_stats__core_metrics_hourly.sql
@@ -1,8 +1,1 @@
-{{ config(
-    materialized = 'incremental',
-    incremental_strategy = 'delete+insert',
-    unique_key = "block_timestamp_hour",
-    cluster_by = ['block_timestamp_hour::DATE'],
-    tags = ['curated']
-) }}
 {{ fsc_evm.silver_stats_core_metrics_hourly() }}

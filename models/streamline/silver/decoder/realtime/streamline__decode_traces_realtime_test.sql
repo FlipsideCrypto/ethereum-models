38083e1d27b7528d5a044ee26f7ab2f9577498ed
sam <sam@flipsidecrypto.com>
2024-10-09 22:38:42 +0800

    .

---
+++ models/streamline/silver/decoder/realtime/streamline__decode_traces_realtime_test.sql
@@ -0,0 +1,17 @@
+{{ config (
+    materialized = "view",
+    post_hook = [fsc_utils.if_data_call_function_v2(
+        func = 'streamline.udf_bulk_decode_traces_v2',
+        target = "{{this.schema}}.{{this.identifier}}",
+        params ={ "external_table" :"DECODED_TRACES", 
+        "sql_limit" :"20000000",
+        "producer_batch_size" :"500000",
+        "worker_batch_size" :"20000",
+        "sql_source" :"{{this.identifier}}" }
+    ),
+    fsc_utils.if_data_call_wait()],
+    tags = ['streamline_decoded_traces_realtime']
+) }}
+{{ fsc_evm.streamline_decoded_traces_requests_test(
+    model_type = 'realtime'
+) }}

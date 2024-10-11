f1b5ba4e1c6fd3026ed2fe061eafdc67c2480215
drethereum <trevor.wenokur@gmail.com>
2024-10-11 14:21:45 -0600

    vars

--- models/streamline/bronze/balances/bronze__streamline_token_balances.sql
+++
@@ -1,8 +0,0 @@
-{{ config (
-    materialized = 'view'
-) }}
-{{ fsc_evm.streamline_external_table_query(
-    model = "token_balances_v2",
-    partition_function = "TO_NUMBER(SPLIT_PART(file_name, '/', 3))",
-    balances = true
-) }}
--- models/streamline/bronze/core/fr/bronze__streamline_fr_blocks_v1.sql
+++
@@ -1,9 +0,0 @@
-{{ config (
-    materialized = 'view'
-) }}
-{{ fsc_evm.streamline_external_table_fr_query(
-    model = "blocks",
-    partition_function = "CAST(SPLIT_PART(SPLIT_PART(file_name, '/', 3), '_', 1) AS INTEGER )",
-    partition_join_key = "_partition_by_block_id",
-    block_number = false
-) }}

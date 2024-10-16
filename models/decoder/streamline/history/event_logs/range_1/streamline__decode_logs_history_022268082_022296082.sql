4937c8ae5441299eab4dbab7552757fad199f2d8
drethereum <trevor.wenokur@gmail.com>
2024-10-16 16:04:00 -0600

    vars for decoder and history models

---
+++ models/decoder/streamline/history/event_logs/range_1/streamline__decode_logs_history_022268082_022296082.sql
@@ -0,0 +1,45 @@
+{# Set variables #}
+{%- set model_name = 'DECODED_LOGS' -%}
+{%- set model_type = 'HISTORY' -%}
+
+{%- set default_vars = fsc_evm.set_default_variables_streamline_decoder(model_name, model_type) -%}
+
+{# Set up parameters for the streamline process. These will come from the vars set in dbt_project.yml #}
+{%- set streamline_params = fsc_evm.set_streamline_parameters_decoder(
+    model_name=model_name,
+    model_type=model_type
+) -%}
+
+{%- set testing_limit = default_vars['testing_limit'] -%}
+
+{# Log configuration details #}
+{{ log_streamline_details(
+    model_name=model_name,
+    model_type=model_type,
+    testing_limit=testing_limit,
+    streamline_params=streamline_params
+) }}
+
+{# Set up dbt configuration #}
+{{ config (
+    materialized = "view",
+    post_hook = [fsc_utils.if_data_call_function_v2(
+        func = 'streamline.udf_bulk_decode_logs_v2',
+        target = "{{this.schema}}.{{this.identifier}}",
+        params = streamline_params
+    ),
+    fsc_utils.if_data_call_wait()],
+    tags = ['streamline_' ~ model_name.lower() ~ model_type.lower() ~ '_range_1']
+) }}
+
+{# Set up the range of blocks to decode #}
+{% set start = this.identifier.split("_") [-2] %}
+{% set stop = this.identifier.split("_") [-1] %}
+
+{# Main query starts here #}
+{{ fsc_evm.streamline_decoded_logs_requests(
+    start,
+    stop,
+    model_type = model_type.lower(),
+    testing_limit = testing_limit
+) }}
--- models/streamline/silver/decoder/realtime/streamline__decode_traces_realtime.sql
+++
@@ -1,8 +0,0 @@
-{{ config (
-    materialized = "view",
-    post_hook = [if_data_call_function( func = "{{this.schema}}.udf_bulk_decode_traces(object_construct('sql_source', '{{this.identifier}}','producer_batch_size', 500000,'producer_limit_size', 20000000))", target = "{{this.schema}}.{{this.identifier}}" ),"call system$wait(" ~ var("WAIT", 400) ~ ")" ],
-    tags = ['streamline_decoded_traces_realtime']
-) }}
-{{ fsc_evm.streamline_decoded_traces_requests(
-    model_type = 'realtime'
-) }}

f926485ef1c15227a8e6b36caf68eeb0415ff29b
Matt Romano <42412983+mattromano@users.noreply.github.com>
2024-10-31 10:52:31 -0700

    AN-5388/add-beacon-chain-dupe-test (#972)
    * add index dupe macro
    
    * move test window up
    
    * update threshold
    
    * revert and update ref

---
+++ macros/tests/dupe_index_days.sql
@@ -0,0 +1,36 @@
+{% test dupe_index_days(
+    model,
+    column_name
+) %}
+WITH daily_index_counts AS (
+    SELECT
+        DATE_TRUNC(
+            'day',
+            b.slot_timestamp
+        ) AS slot_date,
+        v.{{ column_name }},
+        COUNT(*) AS appearances_per_day
+    FROM
+        {{ model }}
+        v
+        LEFT JOIN {{ ref('silver__beacon_blocks') }} b
+        ON v.block_number = b.slot_number
+    WHERE
+        b.slot_timestamp :: DATE >= SYSDATE() - INTERVAL '7 day'
+    GROUP BY
+        1,
+        2
+    HAVING
+        COUNT(*) > 1
+)
+SELECT
+    slot_date,
+    {{ column_name }},
+    appearances_per_day
+FROM
+    daily_index_counts
+ORDER BY
+    slot_date,
+    {{ column_name }}
+
+    {% endtest %}

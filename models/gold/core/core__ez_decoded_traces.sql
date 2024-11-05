9edad517ecc01e5c955141058aa5c0d25300cfdc
drethereum <trevor.wenokur@gmail.com>
2024-11-05 10:07:32 -0700

    decoded traces updates

--- models/gold/core/core__ez_decoded_traces.sql
+++ models/gold/core/core__ez_decoded_traces.sql
@@ -6,33 +6,43 @@
 
 SELECT
     block_number,
-    tx_hash,
     block_timestamp,
-    tx_status,
+    tx_hash,
     tx_position,
-    trace_index,
+    trace_index,    
     from_address,
     c0.name AS from_address_name,
     to_address,
     c1.name AS to_address_name,
+    input,
+    output,
+    decoded_data :function_name :: STRING AS function_name,
+    decoded_data AS full_decoded_data, --new column
+    decoded_data :decoded_input_data AS decoded_input_data,
+    decoded_data :decoded_output_data AS decoded_output_data,
+    TYPE,
+    {# trace_address, --new column, requires FR on silver.decoded_traces #}
+    sub_traces,
     VALUE,
     value_precise_raw,
     value_precise,
     gas,
     gas_used,
-    TYPE,
-    identifier,
-    sub_traces,
+    CASE
+        WHEN trace_status = 'SUCCESS' THEN TRUE
+        ELSE FALSE
+    END AS trace_succeeded, --new column
     error_reason,
-    trace_status,
-    input,
-    output,
-    decoded_data :function_name :: STRING AS function_name,
-    decoded_data :decoded_input_data AS decoded_input_data,
-    decoded_data :decoded_output_data AS decoded_output_data,
+    CASE
+        WHEN tx_status = 'SUCCESS' THEN TRUE
+        ELSE FALSE
+    END AS tx_succeeded, --new column
     decoded_traces_id AS fact_decoded_traces_id,
     GREATEST(COALESCE(t.inserted_timestamp, '2000-01-01'), COALESCE(c0.inserted_timestamp, '2000-01-01'), COALESCE(c1.inserted_timestamp, '2000-01-01')) AS inserted_timestamp,
-    GREATEST(COALESCE(t.modified_timestamp, '2000-01-01'), COALESCE(c0.modified_timestamp, '2000-01-01'), COALESCE(c1.modified_timestamp, '2000-01-01')) AS modified_timestamp
+    GREATEST(COALESCE(t.modified_timestamp, '2000-01-01'), COALESCE(c0.modified_timestamp, '2000-01-01'), COALESCE(c1.modified_timestamp, '2000-01-01')) AS modified_timestamp,
+    identifier, --deprecate
+    trace_status, --deprecate
+    tx_status --deprecate
 FROM
     {{ ref('silver__decoded_traces') }}
     t

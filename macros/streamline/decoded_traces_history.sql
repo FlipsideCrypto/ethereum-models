28e0862583b11692bddbe7f06919cceb7b1bd5e6
sam <sam@flipsidecrypto.com>
2024-11-06 15:40:17 +0800

    add improvement

--- macros/streamline/decoded_traces_history.sql
+++ macros/streamline/decoded_traces_history.sql
@@ -22,89 +22,81 @@
         60
     ) %}
     {% set find_months_query %}
-    WITH exclusions AS (
+    WITH base AS (
         SELECT
-            _call_id
+            t.block_number,
+            DATE_TRUNC(
+                'month',
+                t.block_timestamp
+            ) :: DATE AS MONTH,
+            t._call_id
         FROM
-            {{ ref('streamline__complete_decoded_traces') }}
-    )
-SELECT
-    DISTINCT DATE_TRUNC(
-        'month',
-        b.block_timestamp
-    ) :: DATE AS MONTH
-FROM
-    {{ ref('core__fact_blocks') }}
-    b
-    INNER JOIN {{ ref('silver__traces') }}
-    t
-    ON t.block_number = b.block_number
-    INNER JOIN {{ ref('silver__flat_function_abis') }}
-    f
-    ON t.to_address = f.contract_address
-    AND LEFT(
-        input,
-        10
-    ) = LEFT(
-        f.function_signature,
-        10
-    )
-WHERE
-    NOT EXISTS (
-        SELECT
-            1
-        FROM
-            exclusions e
+            {{ ref('silver__traces') }}
+            t
+            INNER JOIN {{ ref('silver__flat_function_abis') }}
+            f
+            ON t.to_address = f.contract_address
+            AND LEFT(
+                input,
+                10
+            ) = LEFT(
+                f.function_signature,
+                10
+            )
         WHERE
-            t._call_id = e._call_id
-    ) {% if not backfill_mode %}
-        AND f._inserted_timestamp > DATEADD('day',- {{ params.lookback_days }}, SYSDATE())
-    {% endif %}
-ORDER BY
-    MONTH ASC {% endset %}
-    {% set results = run_query(find_months_query) %}
-    {% if execute %}
-        {% set months = results.columns [0].values() %}
-        {% for month in months %}
-            {% set view_name = 'decoded_traces_history_' ~ month.strftime('%Y_%m') %}
-            {% set create_view_query %}
-            CREATE
-            OR REPLACE VIEW streamline.{{ view_name }} AS (
-                WITH target_blocks AS (
-                    SELECT
-                        block_number
-                    FROM
-                        {{ ref('core__fact_blocks') }}
-                    WHERE
-                        DATE_TRUNC(
-                            'month',
-                            block_timestamp
-                        ) = '{{month}}' :: TIMESTAMP
-                ),
-                new_abis AS (
-                    SELECT
-                        contract_address,
-                        abi,
-                        function_signature,
-                        function_name
-                    FROM
-                        {{ ref('silver__flat_function_abis') }}
-
-                        {% if not backfill_mode %}
-                        WHERE
-                            _inserted_timestamp > DATEADD('day',- {{ params.lookback_days }}, SYSDATE())
-                        {% endif %}),
-                        abi_blocks AS (
+            1 = 1 {% if not backfill_mode %}
+                AND f._inserted_timestamp > DATEADD('day',- {{ params.lookback_days }}, SYSDATE())
+            {% endif %}),
+            ranges AS (
+                SELECT
+                    MIN(block_number) AS min_block_number,
+                    MAX(block_number) AS max_block_number
+                FROM
+                    base
+            ),
+            exclusions AS (
+                SELECT
+                    _call_id
+                FROM
+                    {{ ref('streamline__complete_decoded_traces') }}
+                    INNER JOIN ranges
+                WHERE
+                    block_number BETWEEN min_block_number
+                    AND max_block_number
+            )
+        SELECT
+            DISTINCT MONTH
+        FROM
+            base t
+        WHERE
+            NOT EXISTS (
+                SELECT
+                    1
+                FROM
+                    exclusions e
+                WHERE
+                    t._call_id = e._call_id
+            )
+        ORDER BY
+            MONTH ASC {% endset %}
+            {% set results = run_query(find_months_query) %}
+            {% if execute %}
+                {% set months = results.columns [0].values() %}
+                {% for month in months %}
+                    {% set view_name = 'decoded_traces_history_' ~ month.strftime('%Y_%m') %}
+                    {% set create_view_query %}
+                    CREATE
+                    OR REPLACE VIEW streamline.{{ view_name }} AS (
+                        WITH target_blocks AS (
                             SELECT
-                                DISTINCT block_number
+                                MIN(block_number) AS min_block_number,
+                                MAX(block_number) AS max_block_number
                             FROM
-                                ethereum.silver.traces t
-                                INNER JOIN new_abis n
-                                ON t.to_address = n.contract_address
+                                {{ ref('core__fact_blocks') }}
                             WHERE
                                 DATE_TRUNC(
                                     'month',
-                                    t.block_timestamp
+                                    block_timestamp
                                 ) = '{{month}}' :: TIMESTAMP
                         ),
                         existing_traces_to_exclude AS (
@@ -112,7 +104,10 @@ ORDER BY
                                 _call_id
                             FROM
                                 {{ ref('streamline__complete_decoded_traces') }}
-                                INNER JOIN target_blocks USING (block_number)
+                                INNER JOIN target_blocks
+                            WHERE
+                                block_number BETWEEN min_block_number
+                                AND max_block_number
                         ),
                         raw_traces AS (
                             SELECT
@@ -142,20 +137,14 @@ ORDER BY
                             FROM
                                 {{ ref('silver__traces') }}
                                 t
-                                INNER JOIN target_blocks USING (block_number)
+                                INNER JOIN target_blocks
                             WHERE
-                                DATE_TRUNC(
+                                block_number BETWEEN min_block_number
+                                AND max_block_number
+                                AND DATE_TRUNC(
                                     'month',
                                     t.block_timestamp
                                 ) = '{{month}}' :: TIMESTAMP
-                                AND EXISTS (
-                                    SELECT
-                                        1
-                                    FROM
-                                        abi_blocks b
-                                    WHERE
-                                        t.block_number = b.block_number
-                                )
                         ),
                         PARENT AS (
                             SELECT
@@ -220,91 +209,91 @@ ORDER BY
                                     input
                                 )
                         )
-                    SELECT
-                        t.block_number,
-                        t.tx_hash,
-                        t.trace_index,
-                        _call_id,
-                        f.abi AS abi,
-                        f.function_name,
-                        t.effective_contract_address AS abi_address,
-                        t.input,
-                        COALESCE(
-                            t.output,
-                            '0x'
-                        ) AS output
-                    FROM
-                        final_traces t
-                        LEFT JOIN {{ ref('silver__flat_function_abis') }}
-                        f
-                        ON t.effective_contract_address = f.contract_address
-                        AND LEFT(
+                        SELECT
+                            t.block_number,
+                            t.tx_hash,
+                            t.trace_index,
+                            _call_id,
+                            f.abi AS abi,
+                            f.function_name,
+                            t.effective_contract_address AS abi_address,
                             t.input,
-                            10
-                        ) = LEFT(
-                            f.function_signature,
-                            10
+                            COALESCE(
+                                t.output,
+                                '0x'
+                            ) AS output
+                        FROM
+                            final_traces t
+                            LEFT JOIN {{ ref('silver__flat_function_abis') }}
+                            f
+                            ON t.effective_contract_address = f.contract_address
+                            AND LEFT(
+                                t.input,
+                                10
+                            ) = LEFT(
+                                f.function_signature,
+                                10
+                            )
+                        WHERE
+                            f._inserted_timestamp > DATEADD('day',- {{ params.lookback_days }}, SYSDATE())
+                            AND f.abi IS NOT NULL
+                            AND NOT EXISTS (
+                                SELECT
+                                    1
+                                FROM
+                                    existing_traces_to_exclude e
+                                WHERE
+                                    e._call_id = t._call_id
+                            )
+                        LIMIT
+                            {{ params.sql_limit }}
+                    ) {% endset %}
+                    {# Create the view #}
+                    {% do run_query(create_view_query) %}
+                    {{ log(
+                        "Created view for month " ~ month.strftime('%Y-%m'),
+                        info = True
+                    ) }}
+
+                    {% if var(
+                            "STREAMLINE_INVOKE_STREAMS",
+                            false
+                        ) %}
+                        {# Invoke streamline, if rows exist to decode #}
+                        {% set decode_query %}
+                    SELECT
+                        streamline.udf_bulk_decode_traces_v2(
+                            PARSE_JSON(
+                                $${ "external_table": "decoded_traces",
+                                "producer_batch_size": {{ params.producer_batch_size }},
+                                "sql_limit": {{ params.sql_limit }},
+                                "sql_source": "{{view_name}}",
+                                "worker_batch_size": {{ params.worker_batch_size }} }$$
+                            )
                         )
-                        AND f._inserted_timestamp > DATEADD('day',- {{ params.lookback_days }}, SYSDATE())
                     WHERE
-                        NOT EXISTS (
+                        EXISTS(
                             SELECT
                                 1
                             FROM
-                                existing_traces_to_exclude e
-                            WHERE
-                                e._call_id = t._call_id
-                        )
-                        AND f.abi IS NOT NULL
-                    LIMIT
-                        {{ params.sql_limit }}
-                ) {% endset %}
-                {# Create the view #}
-                {% do run_query(create_view_query) %}
-                {{ log(
-                    "Created view for month " ~ month.strftime('%Y-%m'),
-                    info = True
-                ) }}
-
-                {% if var(
-                        "STREAMLINE_INVOKE_STREAMS",
-                        false
-                    ) %}
-                    {# Invoke streamline, if rows exist to decode #}
-                    {% set decode_query %}
-                SELECT
-                    streamline.udf_bulk_decode_traces_v2(
-                        PARSE_JSON(
-                            $${ "external_table": "decoded_traces",
-                            "producer_batch_size": {{ params.producer_batch_size }},
-                            "sql_limit": {{ params.sql_limit }},
-                            "sql_source": "{{view_name}}",
-                            "worker_batch_size": {{ params.worker_batch_size }} }$$
-                        )
-                    )
-                WHERE
-                    EXISTS(
-                        SELECT
-                            1
-                        FROM
-                            streamline.{{ view_name }}
-                        LIMIT
-                            1
-                    ) {% endset %}
-                    {% do run_query(decode_query) %}
-                    {{ log(
-                        "Triggered decoding for month " ~ month.strftime('%Y-%m'),
-                        info = True
-                    ) }}
-                    {# Call wait to avoid queueing up too many jobs #}
-                    {% do run_query(
-                        "call system$wait(" ~ wait_time ~ ")"
-                    ) %}
-                    {{ log(
-                        "Completed wait after decoding for month " ~ month.strftime('%Y-%m'),
-                        info = True
-                    ) }}
-                {% endif %}
+                                streamline.{{ view_name }}
+                            LIMIT
+                                1
+                        ) {% endset %}
+                        {% do run_query(decode_query) %}
+                        {{ log(
+                            "Triggered decoding for month " ~ month.strftime('%Y-%m'),
+                            info = True
+                        ) }}
+                        {# Call wait to avoid queueing up too many jobs #}
+                        {% do run_query(
+                            "call system$wait(" ~ wait_time ~ ")"
+                        ) %}
+                        {{ log(
+                            "Completed wait after decoding for month " ~ month.strftime('%Y-%m'),
+                            info = True
+                        ) }}
+                    {% endif %}
                 {% endfor %}
-                {% endif %}
+            {% endif %}
 {% endmacro %}

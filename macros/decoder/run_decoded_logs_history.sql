65feb9ff48998ae04875ca2b56f5e91e0ab6bb13
Austin <93135983+austinFlipside@users.noreply.github.com>
2024-11-19 21:18:07 -0500

    Lower decoder warehouse (#987)
    * warehouse
    
    * fix

--- macros/decoder/run_decoded_logs_history.sql
+++ macros/decoder/run_decoded_logs_history.sql
@@ -17,7 +17,7 @@
             SELECT
                 github_actions.workflow_dispatches(
                     'FlipsideCrypto',
-                    'arbitrum-models',
+                    'ethereum-models',
                     'dbt_run_streamline_decoded_logs_history.yml',
                     NULL
                 )

ab67d89469d7f20fd7427d8c0c77602403723441
Austin <austin@flipsidecrypto.com>
2024-11-22 13:39:23 -0500

    vuew

--- models/streamline/bronze/beacon/bronze__streamline_beacon_blocks.sql
+++ models/streamline/bronze/beacon/bronze__streamline_beacon_blocks.sql
@@ -1,8 +1,30 @@
 {{ config (
     materialized = 'view'
 ) }}
-{{ fsc_evm.streamline_external_table_query(
-    model = "beacon_blocks_v2",
-    partition_function = "CAST(SPLIT_PART(SPLIT_PART(file_name, '/', 4), '_', 1) AS INTEGER)",
-    block_number = false
-) }}
+
+WITH meta AS (
+    SELECT
+        job_created_time AS _inserted_timestamp,
+        file_name,
+        CAST(SPLIT_PART(SPLIT_PART(file_name, '/', 4), '_', 1) AS INTEGER) AS partition_key
+    FROM
+        TABLE(
+            information_schema.external_table_file_registration_history(
+                start_time => DATEADD('hour', -6, SYSDATE()),
+                table_name => '{{ source( "bronze_streamline", "beacon_blocks_v2") }}'
+            )
+        ) A
+)
+SELECT
+    s.*,
+    b.file_name,
+    b._inserted_timestamp
+    FROM
+        {{ source( "bronze_streamline", "beacon_blocks_v2") }} s
+    JOIN meta b
+    ON b.file_name = metadata$filename
+    AND b.partition_key = s.partition_key
+    WHERE
+        b.partition_key = s.partition_key
+        AND DATA :error IS NULL
+        AND DATA IS NOT NULL

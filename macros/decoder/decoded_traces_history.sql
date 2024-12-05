be96c3bc0a1a4a9ef51790355f411729c580691d
sam <sam@flipsidecrypto.com>
2024-12-05 18:10:01 +0800

    modify child logic

--- macros/decoder/decoded_traces_history.sql
+++ macros/decoder/decoded_traces_history.sql
@@ -135,7 +135,12 @@
                                     AND trace_address != 'ORIGIN' THEN trace_address || '_'
                                     ELSE NULL
                                 END AS parent_of,
-                                IFF(REGEXP_REPLACE(trace_address, '.$', '') = '', 'ORIGIN', REGEXP_REPLACE(trace_address, '.$', '')) AS child_of,
+                                IFF(REGEXP_REPLACE(trace_address, '[0-9]+$', '') = '', 'ORIGIN', REGEXP_REPLACE(trace_address, '[0-9]+$', '')) AS child_of_raw,
+                                IFF(
+                                    trace_address = 'ORIGIN',
+                                    'ORIGI',
+                                    child_of_raw
+                                ) AS child_of,
                                 input,
                                 output,
                                 concat_ws(

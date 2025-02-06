01b3809511c79f5901430b28159310a4cd43c49e
drethereum <trevor.wenokur@gmail.com>
2025-02-06 08:47:43 -0700

    missing_decoding macro

--- macros/tests/missing_decoding.sql
+++ macros/tests/missing_decoding.sql
@@ -1,21 +1,31 @@
 {% test missing_decoded_logs(model) %}
 SELECT
     l.block_number,
-    l._log_id
+    CONCAT(
+        l.tx_hash,
+        '-',
+        l.event_index
+    ) AS _log_id
 FROM
-    {{ ref('silver__logs') }}
+    {{ ref('core__fact_event_logs') }}
     l
     LEFT JOIN {{ model }}
     d
     ON l.block_number = d.block_number
-    AND l._log_id = d._log_id
+    AND CONCAT(
+        l.tx_hash,
+        '-',
+        l.event_index
+    ) = d._log_id
 WHERE
     l.contract_address = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' -- WETH
     AND l.topics [0] :: STRING = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef' -- Transfer
     AND l.block_timestamp BETWEEN DATEADD('hour', -48, SYSDATE())
     AND DATEADD('hour', -6, SYSDATE())
-    AND d._log_id IS NULL {% endtest %}
-    {% test missing_decoded_traces(model) %}
+    AND d._log_id IS NULL 
+{% endtest %}
+
+{% test missing_decoded_traces(model) %}
 SELECT
     t.block_number,
     t.tx_hash,
@@ -36,4 +46,5 @@ WHERE
     ) = '0xa9059cbb' -- transfer(address,uint256)
     AND t.block_timestamp BETWEEN DATEADD('hour', -48, SYSDATE())
     AND DATEADD('hour', -6, SYSDATE())
-    AND d.trace_index IS NULL {% endtest %}
+    AND d.trace_index IS NULL 
+{% endtest %}

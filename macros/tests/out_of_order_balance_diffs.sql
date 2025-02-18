66e7abf88675db71600593d7e710a7c26dc9aa1e
mattromano <mattromano88@gmail.com>
2025-02-18 09:30:15 -0800

    format macro

--- macros/tests/out_of_order_balance_diffs.sql
+++ macros/tests/out_of_order_balance_diffs.sql
@@ -1,46 +1,72 @@
 {% test out_of_order_balance_diffs(model) %}
-
 WITH ordered_balances AS (
-    SELECT 
+    SELECT
         t.block_number,
         t.block_timestamp,
         t.address,
-        {% if model.identifier == 'token_balance_diffs_recent' %}t.contract_address,{% endif %}
+        {% if model.identifier == 'token_balance_diffs_recent' %}
+            t.contract_address,
+        {% endif %}
+
         t.prev_bal_unadj,
         t.current_bal_unadj,
         t._inserted_timestamp,
         t.id,
-        COALESCE(LAG(t.current_bal_unadj) ignore nulls over(
-            PARTITION BY t.address{% if model.identifier == 'token_balance_diffs_recent' %}, t.contract_address{% endif %} 
-            ORDER BY t.block_number ASC), 0) AS actual_previous_balance,
-        CASE WHEN LAG(t.current_bal_unadj) over(
-            PARTITION BY t.address{% if model.identifier == 'token_balance_diffs_recent' %}, t.contract_address{% endif %}
-            ORDER BY t.block_number ASC) IS NULL THEN TRUE ELSE FALSE END AS is_first_record
-    FROM 
-        {{ model }} t
-)
+        COALESCE(LAG(t.current_bal_unadj) ignore nulls over(PARTITION BY t.address
+        {% if model.identifier == 'token_balance_diffs_recent' %}
+        , t.contract_address
+        {% endif %}
+    ORDER BY
+        t.block_number ASC), 0) AS actual_previous_balance,
+        CASE
+            WHEN LAG(
+                t.current_bal_unadj
+            ) over(
+                PARTITION BY t.address
 
-SELECT 
+                {% if model.identifier == 'token_balance_diffs_recent' %},
+                t.contract_address
+                {% endif %}
+                ORDER BY
+                    t.block_number ASC
+            ) IS NULL THEN TRUE
+            ELSE FALSE
+        END AS is_first_record
+    FROM
+        {{ model }}
+        t
+)
+SELECT
     block_number,
     block_timestamp,
     address,
-    {% if model.identifier == 'token_balance_diffs_recent' %}contract_address,{% endif %}
+    {% if model.identifier == 'token_balance_diffs_recent' %}
+        contract_address,
+    {% endif %}
+
     prev_bal_unadj,
     actual_previous_balance,
     current_bal_unadj,
     _inserted_timestamp,
     id,
-    ABS(prev_bal_unadj - actual_previous_balance) as difference,
+    ABS(
+        prev_bal_unadj - actual_previous_balance
+    ) AS difference,
     is_first_record
-FROM ordered_balances
-WHERE prev_bal_unadj != actual_previous_balance
-    AND actual_previous_balance IS NOT NULL 
-    AND is_first_record = false
+FROM
+    ordered_balances
+WHERE
+    prev_bal_unadj != actual_previous_balance
+    AND actual_previous_balance IS NOT NULL
+    AND is_first_record = FALSE
 GROUP BY
     block_number,
     block_timestamp,
     address,
-    {% if model.identifier == 'token_balance_diffs_recent' %}contract_address,{% endif %}
+    {% if model.identifier == 'token_balance_diffs_recent' %}
+        contract_address,
+    {% endif %}
+
     prev_bal_unadj,
     actual_previous_balance,
     current_bal_unadj,
@@ -48,6 +74,5 @@ GROUP BY
     id,
     difference,
     is_first_record
-HAVING COUNT(*) > 0
-
-{% endtest %}
+HAVING
+    COUNT(*) > 0 {% endtest %}

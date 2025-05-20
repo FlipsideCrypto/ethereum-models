49b889af6fbe79082488a4cc6fd66881cb446dc4
drethereum <71602799+drethereum@users.noreply.github.com>
2025-05-20 14:14:45 -0600

    abis v3 (#1062)

--- models/streamline/bronze/abis/bronze__contract_abis.sql
+++ models/streamline/bronze/abis/bronze__contract_abis.sql
@@ -4,7 +4,7 @@
 
 WITH base AS (
     {{ v0_streamline_external_table_query(
-        model = "contract_abis_v2",
+        model = "contract_abis_v3",
         partition_function = "CAST(SPLIT_PART(SPLIT_PART(file_name, '/', 4), '_', 1) AS INTEGER)"
     ) }}
 )

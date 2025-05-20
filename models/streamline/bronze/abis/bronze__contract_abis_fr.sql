0f9b34462af446d3600228b1c9bfd9f86b98c83a
drethereum <trevor.wenokur@gmail.com>
2025-05-20 14:11:15 -0600

    abis v3

--- models/streamline/bronze/abis/bronze__contract_abis_fr.sql
+++ models/streamline/bronze/abis/bronze__contract_abis_fr.sql
@@ -12,6 +12,18 @@ SELECT
     file_name,
     _inserted_timestamp
 FROM
+    {{ ref('bronze__contract_abis_fr_v3') }}
+UNION ALL
+SELECT
+    partition_key,
+    block_number,
+    VALUE :"CONTRACT_ADDRESS" :: STRING AS contract_address,
+    VALUE,
+    DATA,
+    metadata,
+    file_name,
+    _inserted_timestamp
+FROM
     {{ ref('bronze__contract_abis_fr_v2') }}
 UNION ALL
 SELECT

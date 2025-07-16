8d0f2ca557cf6ac5a0a35cb26f2facc6bab45a4f
Austin <austin@flipsidecrypto.com>
2025-07-16 08:48:20 -0400

    5k blocks

--- models/streamline/silver/beacon/realtime/streamline__beacon_blocks_realtime.sql
+++ models/streamline/silver/beacon/realtime/streamline__beacon_blocks_realtime.sql
@@ -4,7 +4,7 @@
         func = 'streamline.udf_bulk_rest_api_v2',
         target = "{{this.schema}}.{{this.identifier}}",
         params ={ "external_table" :"beacon_blocks_v2",
-        "sql_limit" :"2480",
+        "sql_limit" :"5000",
         "producer_batch_size" :"2480",
         "worker_batch_size" :"1240",
         "sql_source" :"{{this.identifier}}",

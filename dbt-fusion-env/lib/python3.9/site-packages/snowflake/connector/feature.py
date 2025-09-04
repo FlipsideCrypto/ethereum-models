87e48bab3d55e226038c34728dcb9d41fecf8f1b
mattromano <mattromano88@gmail.com>
2025-09-04 10:32:05 -0700

    remove fusion package

--- dbt-fusion-env/lib/python3.9/site-packages/snowflake/connector/feature.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-# Feature flags
-
-feature_use_pyopenssl = True  # use pyopenssl API or openssl command
--- dbt-fusion-env/lib/python3.9/site-packages/snowflake/connector/nanoarrow_cpp/ArrowIterator/Python/Common.cpp
+++
@@ -1,8 +0,0 @@
-#include "Common.hpp"
-
-namespace sf {
-
-namespace py {
-// this file will be deleted if it is not used in the future
-}
-}  // namespace sf

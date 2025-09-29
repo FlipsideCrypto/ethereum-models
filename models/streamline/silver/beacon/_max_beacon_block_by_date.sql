ffdcf2262cbd72931ca1f59f68fa137d54e2fb64
Austin <93135983+austinFlipside@users.noreply.github.com>
2025-09-29 11:11:53 -0400

    Fix/beacon slots view (#1145)
    * updates
    
    * filter

--- models/streamline/silver/beacon/_max_beacon_block_by_date.sql
+++ models/streamline/silver/beacon/_max_beacon_block_by_date.sql
@@ -9,7 +9,7 @@ WITH base AS (
         slot_timestamp :: DATE AS block_date,
         MAX(slot_number) block_number
     FROM
-        {{ ref("silver__beacon_blocks") }}
+        {{ ref("beacon_chain__fact_blocks") }}
     WHERE
         block_included
         AND TIME(slot_timestamp) = '23:59:59.000'
@@ -17,12 +17,12 @@ WITH base AS (
         slot_timestamp :: DATE
 )
 SELECT
-    slot_timestamp :: DATE AS block_date,
-    slot_number AS block_number,
-    state_root AS state_id
+    a.block_date,
+    a.block_number,
+    b.state_root AS state_id
 FROM
-    {{ ref("silver__beacon_blocks") }} A
-    JOIN base b
-    ON A.slot_number = b.block_number
+    base a
+    LEFT JOIN {{ ref("silver__beacon_blocks") }} b
+    ON a.block_number = b.slot_number
 WHERE
     block_date IS NOT NULL

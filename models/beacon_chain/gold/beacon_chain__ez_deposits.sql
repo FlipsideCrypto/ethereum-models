8bb3ad2b70fa00355daf60b2339bf06f5bd9ca13
sam <sam@flipsidecrypto.com>
2025-10-12 17:53:46 +0800

    add confirmed logic

--- models/beacon_chain/gold/beacon_chain__ez_deposits.sql
+++ models/beacon_chain/gold/beacon_chain__ez_deposits.sql
@@ -1,12 +1,49 @@
 {{ config(
-    materialized = 'view',
-    persist_docs ={ "relation": true,
-    "columns": true },
-    meta ={ 'database_tags':{ 'table':{ 'PURPOSE': 'DEFI' } } },
-    tags = ['gold','beacon','ez']
+    materialized = "incremental",
+    incremental_strategy = 'delete+insert',
+    unique_key = "ez_deposits_id",
+    cluster_by = "block_timestamp::date",
+    tags = ['gold','beacon']
 ) }}
 
+WITH blocks AS (
+
+    SELECT
+        slot_number,
+        block_number
+    FROM
+        {{ ref('beacon_chain__fact_blocks') }}
+
+{% if is_incremental() %}
+WHERE
+    modified_timestamp >= (
+        SELECT
+            MAX(modified_timestamp) - INTERVAL '24 hours'
+        FROM
+            {{ this }}
+    )
+{% endif %}
+),
+confirmed_deposits AS (
+    SELECT
+        processed_slot,
+        submit_slot_number AS slot_number,
+        pubkey
+    FROM
+        {{ ref('silver__confirmed_deposits') }}
+
+{% if is_incremental() %}
+WHERE
+    processed_slot >= (
+        SELECT
+            MAX(processed_slot) - 7200
+        FROM
+            {{ this }}
+    )
+{% endif %}
+)
 SELECT
+    slot_number,
     block_number,
     block_timestamp,
     tx_hash,
@@ -34,6 +71,19 @@ SELECT
     withdrawal_address,
     signature,
     deposit_index,
+    IFF(
+        processed_slot IS NULL,
+        NULL,
+        processed_slot
+    ) AS processed_slot,
+    CASE
+        -- min slot number for pending slot deposits
+        WHEN slot_number < 11649077
+        OR slot_number IS NULL THEN TRUE
+        WHEN slot_number >= 11649077
+        AND processed_slot IS NOT NULL THEN TRUE
+        ELSE FALSE
+    END AS processed,
     COALESCE (
         d.eth_staking_deposits_id,
         {{ dbt_utils.generate_surrogate_key(
@@ -63,6 +113,20 @@ SELECT
 FROM
     {{ ref('silver__eth_staking_deposits') }}
     d
+    LEFT JOIN blocks USING (block_number)
+    LEFT JOIN confirmed_deposits USING (
+        slot_number,
+        pubkey
+    )
     LEFT JOIN {{ ref('core__dim_labels') }}
     l
     ON d.platform_address = l.address
+
+{% if is_incremental() %}
+AND d.modified_timestamp >= (
+    SELECT
+        MAX(modified_timestamp) - INTERVAL '24 hours'
+    FROM
+        {{ this }}
+)
+{% endif %}

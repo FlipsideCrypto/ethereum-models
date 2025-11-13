7142427a530a343a95fd5aa9f0cbb44450fffb0a
drethereum <trevor.wenokur@gmail.com>
2025-11-13 10:22:53 -0700

    balances bronze cleanup

--- models/streamline/bronze/balances/bronze__streamline_fr_token_balances.sql
+++ models/streamline/bronze/balances/bronze__streamline_fr_token_balances.sql
@@ -15,17 +15,3 @@ SELECT
     _inserted_timestamp
 FROM
     {{ ref('bronze__streamline_fr_token_balances_v2') }}
-UNION ALL
-SELECT
-    _partition_by_block_id AS partition_key,
-    block_number,
-    address,
-    contract_address,
-    block_timestamp,
-    VALUE,
-    DATA,
-    metadata,
-    file_name,
-    _inserted_timestamp
-FROM
-    {{ ref('bronze__streamline_fr_token_balances_v1') }}

17a256f8a1b71477f01627936e514ade016aea8e
drethereum <trevor.wenokur@gmail.com>
2025-11-13 09:56:08 -0700

    abis, beacon, decoded traces, reads cleanup

--- models/streamline/bronze/abis/bronze__contract_abis_fr_v1.sql
+++
@@ -1,9 +0,0 @@
-{{ config (
-    materialized = 'view'
-) }}
-{{ v0_streamline_external_table_fr_query(
-    model = "contract_abis",
-    partition_function = "CAST(SPLIT_PART(SPLIT_PART(file_name, '/', 3), '_', 1) AS INTEGER)",
-    partition_join_key = "_partition_by_block_id",
-    block_number = false
-) }}
